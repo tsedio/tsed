b7f2c4700c617c924be39719606e377efd934e32
Romain Lenzotti <rom.lenzotti@gmail.com>
2016-12-09 08:27:22 +0100

    Remove FakeNextFn

--- test/helper/index.ts
+++ test/helper/index.ts
@@ -2,7 +2,6 @@
 
 export * from "./FakeApplication";
 export * from "./FakeClass";
-export * from "./FakeNextFn";
 export * from "./FakeRequest";
 export * from "./FakeResponse";
 export * from "./FakeServer";
