427de43a15599d92c1a11577d8632b87f37c40ba
vincent178 <vincent.007.cn@gmail.com>
2017-01-12 12:05:56 +0800

    add options prefix

--- src/models/model.ts
+++ src/models/model.ts
@@ -7,47 +7,101 @@ export class Model {
         return Metadata.get(TABLE, this);
     }
 
-    static columns(): string[] {
+    static columns(options?: any): string[] {
+
+        const defaultOptions = {
+            prefix: false
+        };
+
+        const _options = Object.assign({}, defaultOptions, options);
 
         return Object
             .getOwnPropertyNames(this.mappings())
-            .map(name => `${this.table()}.${this.mappings()[name]} AS '${this.column(name)}'`);
+            .map(name => `${this.table()}.${this.mappings()[name]} AS '${this.column(name, _options)}'`);
     }
 
-    static column(property: string): string {
-        return `${this.table()}.${property}`;
+    static column(property: string, options?: any): string {
+
+        const defaultOptions = {
+            prefix: false
+        };
+
+        const _options = Object.assign({}, defaultOptions, options);
+
+        const columnName = this.mappings()[property];
+
+        if (typeof columnName !== 'undefined') {
+
+            if (_options.prefix) {
+                return `${this.table()}.${columnName}`;
+            }
+
+            return columnName;
+        }
     }
 
+    static fromDB<T extends Model>(data: any, options?: any): T {
+
+        const defaultOptions = {
+            prefix: false
+        };
+
+        const _options = Object.assign({}, defaultOptions, options);
+
+        const instance = <T>new this();
+
+        for (let column in data) {
+
+            if (data.hasOwnProperty(column)) {
+
+                const value = data[column];
+
+                let key: string;
+
+                if (_options.prefix) {
+
+                    key = column.split(`${this.table()}.`)[1];
+
+                } else {
+
+                    key = column;
+                }
+
+                if (typeof key !== 'undefined') {
+
+                    const property = this.mappingsReverse()[key];
+
+                    if (typeof property !== 'undefined') {
+                        instance[property] = value;
+                    }
+                }
+
+            }
+
+        }
+
+        return instance;
+    }
+
+    /*
+     * @returns
+     *   "Class property" => "DB column"
+     */
     static mappings(): any {
 
         return Metadata.get(COLUMN, this);
     }
 
+    /*
+     * @returns
+     *   "DB column" => "Class property"
+     */
     static mappingsReverse(): any {
 
         return Object.getOwnPropertyNames(this.mappings()).reduce((ret: any, key: string) => {
             ret[this.mappings()[key]] = key;
             return ret;
         }, {});
-    }
-
-    static fromDB(data: any): Model {
-        const instance = new this();
-        for (let property in data) {
-            if (data.hasOwnProperty(property)) {
-
-                const value = data[property];
-                const key = property.split(`${this.table()}.`)[1];
-
-                if (typeof key !== 'undefined') {
-                    console.log(key);
-                    instance[key] = value;
-                }
-
-            }
-        }
-
-        return instance;
     }
 
 }
--- test/model.spec.ts
+++ test/model.spec.ts
@@ -5,30 +5,71 @@ const expect: Chai.ExpectStatic = Chai.expect;
 
 describe('Table/Column decorators and Model class: ', () => {
 
-    it('Comment should have table() static method', () => {
-        expect(Comment.table()).to.equal("comments");
+    describe('table() static method', () => {
+
+        it('should return table name', () => {
+            expect(Comment.table()).to.equal('comments');
+        })
+
     });
 
-    it('Comment should have mappings() static method', () => {
-        expect(Comment.mappings()).to.deep.equal({id: 'id', content: '_content'});
+    describe('mappings() static method', () => {
+
+        it('should return mappings', () => {
+            expect(Comment.mappings()).to.deep.equal({id: 'id', content: '_content'});
+        });
+
     });
 
-    it('Comment should have mappingsReverse() static method', () => {
-        expect(Comment.mappingsReverse()).to.deep.equal({id: 'id', _content: 'content'});
+    describe("mappingsReverse() static method", () => {
+
+        it('should return reversed mappings', () => {
+            expect(Comment.mappingsReverse()).to.deep.equal({id: 'id', _content: 'content'});
+        });
+
     });
 
-    it('Comment should have columns() static method', () => {
-        expect(Comment.columns()).to.deep.equal(["comments.id AS 'comments.id'", "comments._content AS 'comments.content'"]);
+    describe('columns() static method', () => {
+
+        it('should return columns without prefix', () => {
+            expect(Comment.columns()).to.deep.equal(["comments.id AS 'id'", "comments._content AS '_content'"]);
+        });
+
+        it('should return columns with prefix', () => {
+            expect(Comment.columns({prefix: true})).to.deep.equal(["comments.id AS 'comments.id'", "comments._content AS 'comments._content'"]);
+        });
     });
 
-    it('Comment should have column() static method', () => {
-        expect(Comment.column('content')).to.deep.equal('comments.content');
+
+    describe("column() static method", () => {
+
+        it('should return column name without prefix', () => {
+            expect(Comment.column('content')).to.deep.equal('_content');
+        });
+
+        it('should return column name with prefix', () => {
+            expect(Comment.column('content', {prefix: true})).to.deep.equal('comments._content');
+        });
+
     });
 
+    describe("fromDB() static method", () => {
 
-    it('Comment should have fromDB() static method', () => {
-        const data = {"comments.content": "<h1>Hello world</h1>", "comments.id": 1};
-        expect(Comment.fromDB(data)).to.have.property('content', data['comments.content']);
-        expect(Comment.fromDB(data)).to.have.property('id', data['comments.id']);
+        it('should handle without prefix data and return instance', () => {
+
+            const data = {_content: "<h1>Hello world</h1>", id: 1};
+            expect(Comment.fromDB(data)).to.have.property('content', data._content);
+            expect(Comment.fromDB(data)).to.have.property('id', data.id);
+
+        });
+
+        it('should return column name with prefix', () => {
+
+            const data = {"comments._content": "<h1>Hello world</h1>", "comments.id": 1};
+            expect(Comment.fromDB(data, {prefix: true})).to.have.property('content', data['comments._content']);
+            expect(Comment.fromDB(data, {prefix: true})).to.have.property('id', data['comments.id']);
+
+        });
+
     });
 });
