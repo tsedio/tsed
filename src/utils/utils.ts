749d2bec64f4dd7d0c3793679f0a7729049dafa8
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-01-21 11:30:35 +0100

    Refactoring InjectorService

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,53 +1,65 @@
-
+/**
+ * Get the class constructor if target is an instance.
+ * @param target
+ * @returns {*}
+ */
 export function getClass(target: any): any {
    return target.prototype ? target : target.constructor;
 }
 
 /**
- *
- * @param val
+ * Give the type of val.
+ * @param target
  * @param type
  * @returns {boolean}
  */
-export function isTargetType(val: any, type: "object" | "string"): boolean {
-    return typeof val === type;
+export function isTargetType(target: any, type: "object" | "string"): boolean {
+    return typeof target === type;
 }
 /**
- *
- * @param obj
+ * Return true if the given obj is a primitive.
+ * @param target
  * @returns {boolean}
  */
-export function isPrimitiveOrPrimitiveClass(obj: any): boolean {
+export function isPrimitiveOrPrimitiveClass(target: any): boolean {
 
-    const isPrimitive = ["string", "boolean", "number"].indexOf(typeof obj);
+    const isPrimitive = ["string", "boolean", "number"].indexOf(typeof target);
 
     if (isPrimitive > -1) {
         return true;
     }
 
-    return obj instanceof String
-        || obj === String
-        || obj instanceof Number
-        || obj === Number
-        || obj instanceof Boolean
-        || obj === Boolean;
+    return target instanceof String
+        || target === String
+        || target instanceof Number
+        || target === Number
+        || target instanceof Boolean
+        || target === Boolean;
 }
 /**
- *
- * @param clazz
+ * Return true if the clazz is an array.
+ * @param target
  * @returns {boolean}
  */
-export function isArrayOrArrayClass(clazz: Function): boolean {
-    if (clazz === Array) {
+export function isArrayOrArrayClass(target: Function): boolean {
+    if (target === Array) {
         return true;
     }
-    return Object.prototype.toString.call(clazz) === "[object Array]";
+    return Object.prototype.toString.call(target) === "[object Array]";
 }
-
+/**
+ * Return true if the target.
+ * @param target
+ * @returns {boolean}
+ */
 export function isCollection(target): boolean {
     return isArrayOrArrayClass(target) || target === Map || target === Set || target === WeakMap || target === WeakSet;
 }
-
+/**
+ * Return true if the value is an empty string, null or undefined.
+ * @param value
+ * @returns {boolean}
+ */
 export function isEmpty(value: any): boolean {
     return value === "" || value === null || value === undefined;
 }
