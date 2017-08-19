4ac42406af56e3d89ec3dacd98e8cd033106eb72
romain.lenzotti <rom.lenzotti@gmail.com>
2017-08-19 17:34:15 +0200

    chore: fix script

--- sh/finishfeat.sh
+++ sh/finishfeat.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+shopt -s nocasematch
 set -e
+set -o pipefail
 
 FEATURE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+MASTER_BRANCH="master"
+PRODUCTION_BRANCH="production"
 
 git fetch --all --prune --tags
 
-if [[${FEATURE_BRANCH} !=  "master"]]; then
+if [[ ${FEATURE_BRANCH} == ${PRODUCTION_BRANCH} ]]; then
+  exit 0
+fi
+
+if [[ ${FEATURE_BRANCH} != ${MASTER_BRANCH} ]]; then
 
    echo "Publish local feature"
    git rebase origin/master
@@ -16,20 +24,34 @@ fi
 git checkout master
 git reset --hard origin/master
 
-if [[${FEATURE_BRANCH} !=  "master"]]; then
+if [[ ${FEATURE_BRANCH} != ${MASTER_BRANCH} ]]; then
 
     git merge --no-ff -m "$(echo $FEATURE_BRANCH)" ${FEATURE_BRANCH}
 
 fi
 
-git branch -D production | echo 'existing locale branch production deleted'
-git checkout -b production origin/production
-git rebase origin/master
-git push origin production
+echo "Try to delete production"
 
-if [[${FEATURE_BRANCH} !=  "master"]]; then
+git branch -D production || echo "Local production not found"
+
+echo "Checkout production"
+
+git checkout -b production origin/production
+
+echo "Reset HEAD of production"
+
+git reset --hard origin/master
+
+echo "Push origin production"
+
+git push origin production -f
+
+if [[ ${FEATURE_BRANCH} != ${MASTER_BRANCH} ]]; then
     git push origin :${FEATURE_BRANCH}
     git branch -d ${FEATURE_BRANCH}
     echo ${FEATURE_BRANCH} ' FINISH DONE'
 fi
+
+git checkout master
+
 echo 'PUBLISH DONE'
