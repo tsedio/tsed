d114c074ae83bf12a2d6810c30c7614534ef4d96
Lasse Wallentin <lasse.wallentin@schneider-electric.com>
2017-09-19 09:08:26 +0200

    style(swagger): removed unused imports

--- src/swagger/decorators/operation.ts
+++ src/swagger/decorators/operation.ts
@@ -3,10 +3,8 @@
  */
 /** */
 import {Operation} from "swagger-schema-official";
-import {PropertyRegistry} from "../../converters/registries/PropertyRegistry";
 import {Store} from "../../core/class/Store";
 import {DecoratorParameters} from "../../core/interfaces";
-import {getDecoratorType} from "../../core/utils";
 
 export function Operation(operation: Operation|any) {
     return Store.decorate((store: Store, parameters: DecoratorParameters) => {
--- src/swagger/decorators/tag.ts
+++ src/swagger/decorators/tag.ts
@@ -3,10 +3,8 @@
  */
 /** */
 import {Tag} from "swagger-schema-official";
-import {PropertyRegistry} from "../../converters/registries/PropertyRegistry";
 import {Store} from "../../core/class/Store";
 import {DecoratorParameters} from "../../core/interfaces";
-import {getDecoratorType} from "../../core/utils";
 
 export function Tag(tag: Tag|any) {
     return Store.decorate((store: Store, parameters: DecoratorParameters) => {
