0e467c84603d8756da982e07dba26efbeeb65d99
romain.lenzotti <rom.lenzotti@gmail.com>
2017-10-09 10:15:49 +0200

    docs: add document

--- src/mvc/decorators/allow.ts
+++ src/mvc/decorators/allow.ts
@@ -1,14 +1,29 @@
-/**
- * @module common/mvc
- */
-/** */
-
 import {PropertyRegistry} from "../../converters/registries/PropertyRegistry";
 import {Type} from "../../core/interfaces";
 import {ParamRegistry} from "../registries/ParamRegistry";
 
 /**
- * Add allowed values when the property or parameters is required `@Required()`.
+ * Add allowed values when the property or parameters is required.
+ *
+ * #### Example on parameter:
+ *
+ * ```typescript
+ * @Post("/")
+ * async method(@Required() @Allow("") @BodyParams("field") field: string) {}
+ * ```
+ * > Required will throw a BadRequest when the given value is `null` or `undefined` but not for an empty string.
+ *
+ * #### Example on model:
+ *
+ * ```typescript
+ * class Model {
+ *   @JsonProperty()
+ *   @Required()
+ *   @Allow("")
+ *   field: string;
+ * }
+ * ```
+ *
  * @returns {Function}
  * @decorator
  */
--- src/mvc/decorators/required.ts
+++ src/mvc/decorators/required.ts
@@ -1,10 +1,45 @@
 import {PropertyRegistry} from "../../converters/registries/PropertyRegistry";
 import {Type} from "../../core/interfaces";
-import {EndpointRegistry} from "../registries/EndpointRegistry";
 import {ParamRegistry} from "../registries/ParamRegistry";
 
 /**
- * Add required annotation for a function argument .
+ * Add required annotation for a function argument.
+ *
+ * The @Required decorator can be used on two cases.
+ *
+ * To decorate a parameters:
+ *
+ * ```typescript
+ * @Post("/")
+ * async method(@Required() @BodyParams("field") field: string) {}
+ * ```
+ *
+ * To decorate a model:
+ *
+ * ```typescript
+ * class Model {
+ *   @JsonProperty()
+ *   @Required()
+ *   field: string;
+ * }
+ * ```
+ *
+ * > Required will throw a BadRequest when the given value is `null`, an empty string or `undefined`.
+ *
+ * ### Allow a values
+ *
+ * In some case, you didn't want trigger a BadRequest when the value is an empty string for example.
+ * The decorator `@Allow()`, allow you to configure a value list for which there will be no exception.
+ *
+ * ```typescript
+ * class Model {
+ *   @JsonProperty()
+ *   @Required()
+ *   @Allow("")
+ *   field: string;
+ * }
+ * ```
+ *
  * @returns {Function}
  * @decorator
  */
