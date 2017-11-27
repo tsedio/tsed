f287538270dcd60d24b60e6251cdff109ee70d19
romain.lenzotti <rom.lenzotti@gmail.com>
2017-11-27 10:27:34 +0100

    chore: remove some dependencies
    - Remove old sh script. Use gflow instead of.

--- sh/rebasefeat.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-git fetch --all --prune --tags
-git rebase origin/production
--- sh/startfeat.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-git fetch --all --prune --tags
-git checkout --no-track -b $1 origin/production
-echo 'NEW FEATURE ' $1 ' CREATION DONE from MASTER HEAD'
