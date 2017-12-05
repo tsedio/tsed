fd9abb8fa28d2adc484919af9b6ef13fd11d58ba
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-12-05 11:44:06 +0100

    chore: Update unit test
    Just update the unit test for the Example decorators

--- test/units/swagger/decorators/example.spec.ts
+++ test/units/swagger/decorators/example.spec.ts
@@ -1,5 +1,5 @@
 import {Store} from "../../../../src/core/class/Store";
-import { Example, ExampleAnyType } from '../../../../src/swagger/decorators/example';
+import { Example } from "../../../../src/swagger/decorators/example";
 import {expect} from "../../../tools";
 
 
@@ -10,35 +10,22 @@ class Test {
 }
 
 describe("Example()", () => {
-    before(() => {
-        Example("name", "description")(Test, "test");
-        this.store = Store.from(Test, "test");
-    });
-    it("should set the schema", () => {
-        expect(this.store.get("schema")).to.deep.eq({
-            "example": {
-                "name": "description"
-            }
-        });
-    });
-});
-
-describe("ExampleAnyType()", () => {
     describe("string type", () => {
         before(() => {
-            ExampleAnyType("stringTest")(Test, "test1");
-            this.store = Store.from(Test, "test1");
+            Example("name", "description")(Test, "test");
+            this.store = Store.from(Test, "test");
         });
         it("should set the schema", () => {
             expect(this.store.get("schema")).to.deep.eq({
-                "example": "stringTest"
+                "example": {
+                    "name": "description"
+                }
             });
         });
     });
-
     describe("array type", () => {
         before(() => {
-            ExampleAnyType(["s1", "s2"])(Test, "test2");
+            Example(["s1", "s2"])(Test, "test2");
             this.store = Store.from(Test, "test2");
         });
         it("should set the schema", () => {
@@ -47,10 +34,9 @@ describe("ExampleAnyType()", () => {
             });
         });
     });
-
     describe("object type", () => {
         before(() => {
-            ExampleAnyType({"k1": { "k2": "vaue" }})(Test, "test3");
+            Example({"k1": { "k2": "vaue" }})(Test, "test3");
             this.store = Store.from(Test, "test3");
         });
         it("should set the schema", () => {
