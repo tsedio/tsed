e71bff1f23b301cee493e48aa63067d7140512f7
Lasse Wallentin <lasse.wallentin@schneider-electric.com>
2018-01-05 13:52:28 +0100

    feat(ajv): ErrorFormatter and ajv settings
    Settings can now be given in ServerSettings for ajv.
    The format of the error message produced by AjvService can be altered

--- src/ajv/services/AjvService.ts
+++ src/ajv/services/AjvService.ts
@@ -4,20 +4,34 @@ import {BadRequest} from "ts-httpexceptions";
 import {OverrideService} from "../../di/decorators/overrideService";
 import {JsonSchemesService} from "../../jsonschema/services/JsonSchemesService";
 import {ValidationService} from "../../filters/services/ValidationService";
+import { ServerSettingsService } from "../../config/services/ServerSettingsService";
+import { IAjvSettings, IAjvOptions, ErrorFormatter } from "../interfaces/IAjvSettings";
+import { deepExtends } from "../../index";
+import { $log } from "ts-log-debug";
 
 @OverrideService(ValidationService)
 export class AjvService extends ValidationService {
-    constructor(private jsonSchemaService: JsonSchemesService) {
+
+    private errorFormatter: ErrorFormatter;
+    private options: IAjvOptions;
+
+    constructor(private jsonSchemaService: JsonSchemesService,
+                private serverSettingsService: ServerSettingsService) {
         super();
+
+        const ajvSettings = this.serverSettingsService.get<IAjvSettings>("ajv");
+        this.options = deepExtends({
+            verbose: false
+        }, ajvSettings && ajvSettings.options ? ajvSettings.options : {} );
+       const defaultFormatter =  (error: any) => `{{name}}${error.dataPath} ${error.message} (${error.keyword})`;
+       this.errorFormatter = ajvSettings && ajvSettings.errorFormat ? ajvSettings.errorFormat : defaultFormatter;
     }
 
     public validate(obj: any, targetType: any, baseType?: any): void {
-        const schema = this.jsonSchemaService.getSchemaDefinition(targetType);
-
+        let schema = <any>this.jsonSchemaService.getSchemaDefinition(targetType);
         if (schema) {
-            const ajv = new Ajv();
+            const ajv = new Ajv(this.options);
             const valid = ajv.validate(schema, obj);
-
             if (!valid) {
                 throw(this.buildErrors(ajv.errors!));
             }
@@ -25,11 +39,8 @@ export class AjvService extends ValidationService {
     }
 
     private buildErrors(errors: ErrorObject[]) {
-
-        const message = errors.map(error => {
-            return `{{name}}${error.dataPath} ${error.message} (${error.keyword})`;
-        }).join("\n");
-
+        $log.debug("AJV errors: ", errors);
+        const message = errors.map(this.errorFormatter).join("\n");
         return new BadRequest(message);
     }
 }
