96a7b2e795333150a56670a39fb0ad47d9d1f5b4
romain.lenzotti <rom.lenzotti@gmail.com>
2018-02-19 19:44:15 +0100

    fix: build version

--- tasks/write-pkg.js
+++ tasks/write-pkg.js
@@ -3,51 +3,58 @@ const replace = require('gulp-replace');
 const jeditor = require('gulp-json-editor');
 const findPackages = require('./utils/findPackages');
 const all = require('./utils/all');
+const readPackageJson = require('read-package-json');
 
 const writePackage = () => {
-  const pkg = require('../package.json');///
+  return new Promise((resolve, reject) => {
+    readPackageJson('./package.json', console.error, null, (er, data) => resolve(data));
+  })
+    .then((pkg) => {
+      fs.writeFileSync(`${__dirname}/../package.json`, JSON.stringify(require('../package.json'), null, 2), {});
 
-  fs.writeFileSync(`${__dirname}/../package.json`, JSON.stringify(pkg, null, 2), {});
+      const newPgk = Object.keys(pkg)
+        .filter(key => ['scripts', 'devDependencies'].indexOf(key) === -1)
+        .reduce((acc, key) => {
+          acc[key] = pkg[key];
+          return acc;
+        }, {});
 
-  const newPgk = Object.keys(pkg)
-    .filter(key => ['scripts', 'devDependencies'].indexOf(key) === -1)
-    .reduce((acc, key) => {
-      acc[key] = pkg[key];
-      return acc;
-    }, {});
+      newPgk.main = 'lib/index.js';
+      newPgk.typings = 'lib/index.d.ts';
+      newPgk.dependencies['@tsed/core'] = pkg.version;
+      newPgk.dependencies['@tsed/common'] = pkg.version;
 
-  newPgk.main = 'lib/index.js';
-  newPgk.typings = 'lib/index.d.ts';
-  newPgk.dependencies['@tsed/core'] = pkg.version;
-  newPgk.dependencies['@tsed/common'] = pkg.version;
+      fs.writeFileSync(`${__dirname}/../dist/package.json`, JSON.stringify(newPgk, null, 2), {
+        encode: 'utf8'
+      });
 
-  fs.writeFileSync(`${__dirname}/../dist/package.json`, JSON.stringify(newPgk, null, 2), {
-    encode: 'utf8'
-  });
-
-  return newPgk;
+      return newPgk;
+    });
 };
 
 module.exports = (gulp) => {
-  const version = require('../package.json').version;
-  const { repository, keywords, bugs, license, homepage, author } = writePackage();
 
-  const streams = findPackages().map((pkg) =>
-    gulp
-      .src([
-        'src/' + pkg + '/package.json'
-      ], { base: './' + 'src/' + pkg })
-      .pipe(replace('0.0.0-PLACEHOLDER', version))
-      .pipe(jeditor({
-        repository,
-        keywords,
-        bugs,
-        homepage,
-        author,
-        license
-      }))
-      .pipe(gulp.dest('dist/packages/' + pkg))
-  );
+  writePackage().then((pkg) => {
+    const { repository, keywords, bugs, license, homepage, author, version, gitHead } = pkg;
 
-  return all(...streams);
+    const streams = findPackages().map((pkg) =>
+      gulp
+        .src([
+          'src/' + pkg + '/package.json'
+        ], { base: './' + 'src/' + pkg })
+        .pipe(replace('0.0.0-PLACEHOLDER', version))
+        .pipe(jeditor({
+          repository,
+          keywords,
+          bugs,
+          homepage,
+          author,
+          license,
+          gitHead
+        }))
+        .pipe(gulp.dest('dist/packages/' + pkg))
+    );
+
+    return all(...streams);
+  });
 };
