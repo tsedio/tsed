0ecfb32348a01e6a3f64be2540000deb7ae4c717
milewski <rafael.milewski@gmail.com>
2018-03-30 13:05:46 +0800

    fix(mongoose): Fix issue #305

--- test/units/mongoose/decorators/indexed.spec.ts
+++ test/units/mongoose/decorators/indexed.spec.ts
@@ -2,6 +2,7 @@ import {Store} from "../../../../src/core/class/Store";
 import {descriptorOf} from "../../../../src/core/utils";
 import {Indexed} from "../../../../src/mongoose/decorators";
 import {expect} from "../../../tools";
+import {MONGOOSE_SCHEMA} from "../../../../src/mongoose/constants";
 
 describe("@Indexed()", () => {
     class Test {
@@ -13,7 +14,7 @@ describe("@Indexed()", () => {
     });
 
     it("should set metadata", () => {
-        expect(this.store.get("mongooseSchema")).to.deep.eq({
+        expect(this.store.get(MONGOOSE_SCHEMA)).to.deep.eq({
             index: true
         });
     });
