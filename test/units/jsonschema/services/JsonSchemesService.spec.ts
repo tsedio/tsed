e3d11a8e67b4713b45ddb1ba70f79a2438f727c4
romain.lenzotti <rom.lenzotti@gmail.com>
2018-04-08 07:49:57 +0200

    fix(jsonschema): Solve problem with circular ref in JsonSchema
    Closes: #316

--- test/units/jsonschema/services/JsonSchemesService.spec.ts
+++ test/units/jsonschema/services/JsonSchemesService.spec.ts
@@ -1,6 +1,6 @@
 import {JsonSchemesService} from "../../../../src/common/jsonschema/services/JsonSchemesService";
 import {inject} from "../../../../src/testing";
-import {JsonFoo2, Thingy} from "../../../helper/classes";
+import {Circular, IndirectCircular, JsonFoo2, Thingy} from "../../../helper/classes";
 import {expect} from "../../../tools";
 
 describe("JsonSchemesService", () => {
@@ -132,6 +132,66 @@ describe("JsonSchemesService", () => {
                 "properties": {
                     "stuff": {
                         "$ref": "#/definitions/Stuff"
+                    }
+                },
+                "type": "object"
+            });
+        });
+    });
+
+    describe("with circular reference", () => {
+        before(inject([JsonSchemesService], (service: JsonSchemesService) => {
+            this.result = service.getSchemaDefinition(Circular);
+        }));
+        it("should return a schema with his definitions", () => {
+            expect(this.result).to.deep.eq({
+                "definitions": {
+                    "Circular": {
+                        "properties": {
+                            "parent": {
+                                "$ref": "#/definitions/Circular"
+                            }
+                        },
+                        "type": "object"
+                    }
+                },
+                "properties": {
+                    "parent": {
+                        "$ref": "#/definitions/Circular"
+                    }
+                },
+                "type": "object"
+            });
+        });
+    });
+
+    describe("with indirect circular reference", () => {
+        before(inject([JsonSchemesService], (service: JsonSchemesService) => {
+            this.result = service.getSchemaDefinition(IndirectCircular);
+        }));
+        it("should return a schema with his definitions", () => {
+            expect(this.result).to.deep.eq({
+                "definitions": {
+                    "Dependency": {
+                        "properties": {
+                            "dep": {
+                                "$ref": "#/definitions/IndirectCircular"
+                            }
+                        },
+                        "type": "object"
+                    },
+                    "IndirectCircular": {
+                        "properties": {
+                            "parent": {
+                                "$ref": "#/definitions/Dependency"
+                            }
+                        },
+                        "type": "object"
+                    }
+                },
+                "properties": {
+                    "parent": {
+                        "$ref": "#/definitions/Dependency"
                     }
                 },
                 "type": "object"
