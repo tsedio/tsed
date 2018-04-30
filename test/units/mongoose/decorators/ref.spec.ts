a74a0f1e07a6b63f92f358d5377d5edb880e5ccb
romain.lenzotti <rom.lenzotti@gmail.com>
2018-04-30 15:10:05 +0200

    fix(mongoose): Ref decorator support string type for circular dependencies
    #Closes: 340

--- test/units/mongoose/decorators/ref.spec.ts
+++ test/units/mongoose/decorators/ref.spec.ts
@@ -6,22 +6,46 @@ import {Ref} from "../../../../src/mongoose/decorators";
 import {expect} from "../../../tools";
 
 describe("@Ref()", () => {
-    class Test {
-    }
 
-    class RefTest {
-    }
+    describe("type is a class", () => {
+        class Test {
+        }
 
-    before(() => {
-        Store.from(RefTest).set(MONGOOSE_MODEL_NAME, "RefTest");
-        Ref(RefTest)(Test, "test", descriptorOf(Test, "test"));
-        this.store = Store.from(Test, "test", descriptorOf(Test, "test"));
+        class RefTest {
+        }
+
+        before(() => {
+            Store.from(RefTest).set(MONGOOSE_MODEL_NAME, "RefTest");
+            Ref(RefTest)(Test, "test", descriptorOf(Test, "test"));
+            this.store = Store.from(Test, "test", descriptorOf(Test, "test"));
+        });
+
+        it("should set metadata", () => {
+            expect(this.store.get(MONGOOSE_SCHEMA)).to.deep.eq({
+                type: Schema.Types.ObjectId,
+                ref: "RefTest"
+            });
+        });
     });
 
-    it("should set metadata", () => {
-        expect(this.store.get(MONGOOSE_SCHEMA)).to.deep.eq({
-            type: Schema.Types.ObjectId,
-            ref: "RefTest"
+    describe("type is a string", () => {
+        class Test {
+        }
+
+        class RefTest {
+        }
+
+        before(() => {
+            Store.from(RefTest).set(MONGOOSE_MODEL_NAME, "RefTest");
+            Ref("RefTest")(Test, "test", descriptorOf(Test, "test"));
+            this.store = Store.from(Test, "test", descriptorOf(Test, "test"));
+        });
+
+        it("should set metadata", () => {
+            expect(this.store.get(MONGOOSE_SCHEMA)).to.deep.eq({
+                type: Schema.Types.ObjectId,
+                ref: "RefTest"
+            });
         });
     });
 });
