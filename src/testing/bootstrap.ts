7966650f1a45cfac3ce088d1d41f8e2044f94e8c
romain.lenzotti <rom.lenzotti@gmail.com>
2018-06-05 08:07:36 +0200

    fix: Restore done parameter on bootstrap method and remove after
    - after is specific to mocha library.
    - return Promise isn't supported by all unit test library.

--- src/testing/bootstrap.ts
+++ src/testing/bootstrap.ts
@@ -13,18 +13,17 @@ export function bootstrap(server: any, ...args: any[]) {
   $log.stop();
   process.env.NODE_ENV = process.env.NODE_ENV || "test";
 
-  if (after) {
-    after(() => {
-      this.$$injector = null;
-    });
-  }
-
-  return function before() {
+  return function before(done: any) {
     const instance = new server(...args);
 
     instance.startServers = () => Promise.resolve();
+
+    // TODO used by inject method
     this.$$injector = instance.injector;
 
-    return instance.start();
+    instance
+      .start()
+      .then(done)
+      .catch(done);
   };
 }
