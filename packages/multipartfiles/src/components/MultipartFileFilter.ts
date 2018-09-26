1a70bc95899a061a12c5ef76d81bd19a9b58c3bf
romain.lenzotti <rom.lenzotti@gmail.com>
2018-09-26 08:55:05 +0200

    fix(multipartfile): Fix request.files issue when file isn't required
    Closes: #462

--- packages/multipartfiles/src/components/MultipartFileFilter.ts
+++ packages/multipartfiles/src/components/MultipartFileFilter.ts
@@ -7,6 +7,6 @@ import {Filter, IFilter} from "@tsed/common";
 @Filter()
 export class MultipartFileFilter implements IFilter {
   transform(expression: string, request: any, response: any) {
-    return request["files"][0];
+    return request["files"] && request["files"][0];
   }
 }
