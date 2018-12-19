11ea201f3c5a70234094d88090415e6543bcad79
romain.lenzotti <rom.lenzotti@gmail.com>
2018-12-19 08:14:00 +0100

    fix(common): Change interface declaration for IServerSettings (Ambient merging interface declaration)

--- packages/ajv/src/index.ts
+++ packages/ajv/src/index.ts
@@ -1,2 +1,8 @@
-// decorators
 import "./services/AjvService";
+import {IAjvSettings} from "./interfaces/IAjvSettings";
+
+declare module "@tsed/common" {
+  interface IServerSettingsOptions {
+    ajv: IAjvSettings;
+  }
+}
--- packages/typeorm/src/index.ts
+++ packages/typeorm/src/index.ts
@@ -1,3 +1,10 @@
-export * from "./interfaces";
+import {ConnectionOptions} from "typeorm";
+
+declare module "@tsed/common" {
+  interface IServerSettingsOptions {
+    typeorm: {[connectionName: string]: ConnectionOptions};
+  }
+}
+
 export * from "./services/TypeORMService";
 export * from "./TypeORMModule";
