b9ff31d8a2e95a8d41dd6044615e7f1147ad5316
Nico Jansen <jansennico@gmail.com>
2018-12-19 13:45:26 +0100

    feat(testing): Make sure `injectbootstrap` can be called from strict context

--- packages/testing/src/bootstrap.ts
+++ packages/testing/src/bootstrap.ts
@@ -1,6 +1,5 @@
 import {$log} from "ts-log-debug";
-
-// export const servers: Map<any, any> = new Map();
+import {TestContext} from "./testContext";
 
 /**
  * Load the server silently without listening port and configure it on test profile.
@@ -18,12 +17,33 @@ export function bootstrap(server: any, ...args: any[]) {
 
     instance.startServers = () => Promise.resolve();
 
-    // TODO used by inject method
-    this.$$injector = instance.injector;
+    // used by inject method
+    TestContext.injector = instance.injector;
 
     instance
       .start()
       .then(done)
       .catch(done);
   };
+}
+
+/**
+ * Load the server silently without listening port and configure it on test profile.
+ * Same as `bootstrap`, but this returns a promise instead
+ * @example await bootstrapAsPromised(server);
+ * @decorator
+ * @param server
+ * @param args
+ * @returns {(done:Function)=>undefined}
+ */
+export function bootstrapAsPromised(server: any, ...args: any[]): Promise<void> {
+  return new Promise((res, rej) => {
+    bootstrap(server, ...args)((err: Error) => {
+      if (err) {
+        rej(err);
+      } else {
+        res();
+      }
+    });
+  });
 }
