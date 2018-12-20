09bdcb78ab2986f40aabc72709e497ed1daff504
Nico Jansen <jansennico@gmail.com>
2018-12-20 08:49:27 +0100

    feat(testing): Promisify the bootstrap function
    Make sure the bootstrap test setup function
    returns a `Promise` (instead of `(done) => void`).
    
    BREAKING CHANGE: `boostrap(MyServer)` now returns a `Promise`.
    
    Change `beforeEach(done => bootstrap(MyServer)(done))`
    to
    `beforeEach(async () => await bootstrap(MyServer))`
    
    (if your using  `beforeEach(bootstrap(MyServer))`, you're probably safe)

--- packages/testing/src/TestContext.ts
+++ packages/testing/src/TestContext.ts
@@ -1,11 +1,15 @@
 import {InjectorService} from "@tsed/di";
 
 export class TestContext {
-  static injector: InjectorService | null;
+  static injector: InjectorService | null = null;
+
+  /**
+   * Resets the test injector of the test context, so it won't pollute your next test. Call this in your `tearDown` logic.
+   */
   static reset() {
-    if (this.injector) {
-      this.injector.clear();
+    if (TestContext.injector && TestContext.injector.clear) {
+      TestContext.injector.clear();
     }
-    this.injector = null;
+    TestContext.injector = null;
   }
 }
--- packages/testing/src/bootstrap.ts
+++ packages/testing/src/bootstrap.ts
@@ -1,18 +1,17 @@
 import {$log} from "ts-log-debug";
-import {TestContext} from "./testContext";
+import {TestContext} from "./TestContext";
 
 /**
  * Load the server silently without listening port and configure it on test profile.
  * @decorator
  * @param server
  * @param args
- * @returns {(done:Function)=>undefined}
+ * @returns {Promise<void>}
  */
-export function bootstrap(server: any, ...args: any[]) {
+export function bootstrap(server: any, ...args: any[]): () => Promise<void> {
   $log.stop();
-  process.env.NODE_ENV = process.env.NODE_ENV || "test";
 
-  return function before(done: any) {
+  return function before() {
     const instance = new server(...args);
 
     instance.startServers = () => Promise.resolve();
@@ -20,30 +19,6 @@ export function bootstrap(server: any, ...args: any[]) {
     // used by inject method
     TestContext.injector = instance.injector;
 
-    instance
-      .start()
-      .then(done)
-      .catch(done);
+    return instance.start();
   };
-}
-
-/**
- * Load the server silently without listening port and configure it on test profile.
- * Same as `bootstrap`, but this returns a promise instead
- * @example await bootstrapAsPromised(server);
- * @decorator
- * @param server
- * @param args
- * @returns {(done:Function)=>undefined}
- */
-export function bootstrapAsPromised(server: any, ...args: any[]): Promise<void> {
-  return new Promise((res, rej) => {
-    bootstrap(server, ...args)((err: Error) => {
-      if (err) {
-        rej(err);
-      } else {
-        res();
-      }
-    });
-  });
 }
