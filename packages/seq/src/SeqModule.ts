4dc9a16a52b027e6b1c108cf7b2453474177a781
Oskar Lebuda <o.lebuda@gmail.com>
2019-05-07 23:30:45 +0200

    SEQ: add changes after Code Review

--- packages/seq/src/SeqModule.ts
+++ packages/seq/src/SeqModule.ts
@@ -1,21 +1,20 @@
-import {Inject, Service} from "@tsed/di";
+import {Constant, Service} from "@tsed/di";
 import {$log} from "ts-log-debug";
-import {AfterRoutesInit, ServerSettingsService} from "@tsed/common";
+import {AfterRoutesInit} from "@tsed/common";
 import {ISeqSettings} from "./interfaces/ISeqSettings";
 
 @Service()
 export class SeqModule implements AfterRoutesInit {
+  @Constant("seq", {
+    url: "http://localhost:5341"
+  })
   private config: ISeqSettings;
-
-  constructor(@Inject() private serverSettingsService: ServerSettingsService) {
-    this.config = serverSettingsService.get("seq") || {};
-  }
 
   $afterRoutesInit() {
     $log.appenders.set("seq", {
       type: "seq",
       levels: ["info", "debug", "trace", "fatal", "error", "warn"],
-      url: this.config.url ? this.config.url : "http://localhost:5341"
+      url: this.config.url
     });
   }
 }
