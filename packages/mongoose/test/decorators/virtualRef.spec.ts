6e120a1e7218feb9f44223fb88aa98cec9a092b1
Gustavo Mendonça <gustavo.oktagon@gmail.com>
2019-05-10 09:42:04 -0300

    feat(mongoose): Update VirtualRef decorator to support more options
    Closes: #549

--- packages/mongoose/test/decorators/virtualRef.spec.ts
+++ packages/mongoose/test/decorators/virtualRef.spec.ts
@@ -1,23 +1,75 @@
-import {Store} from "@tsed/core";
-import {descriptorOf} from "@tsed/core";
+import {descriptorOf, Store} from "@tsed/core";
 import {MONGOOSE_SCHEMA} from "../../src/constants";
 import {VirtualRef} from "../../src/decorators";
-import {expect} from "chai";
 
 describe("@VirtualRef()", () => {
-  class Test {}
-  class RefTest {}
+  describe("when type and foreign value are given", () => {
+    it("should set metadata", () => {
+      // GIVEN
+      class Test {
+      }
 
-  before(() => {
-    VirtualRef("RefTest", "foreign")(Test, "test", descriptorOf(Test, "test"));
-    this.store = Store.from(Test, "test", descriptorOf(Test, "test"));
+      const store = Store.from(Test, "test", descriptorOf(Test, "test"));
+      // WHEN
+      VirtualRef("RefTest", "foreign")(Test, "test", descriptorOf(Test, "test"));
+
+      // THEN
+      store.get(MONGOOSE_SCHEMA).should.deep.eq({
+        ref: "RefTest",
+        foreignField: "foreign",
+        localField: "test"
+      });
+    });
   });
 
-  it("should set metadata", () => {
-    expect(this.store.get(MONGOOSE_SCHEMA)).to.deep.eq({
-      ref: "RefTest",
-      foreignField: "foreign",
-      localField: "test"
+  describe("when options is given with minimal fields", () => {
+    it("should set metadata", () => {
+      // GIVEN
+      class Test {
+      }
+
+      const store = Store.from(Test, "test", descriptorOf(Test, "test"));
+      const options = {type: "RefTest", foreignField: "foreign"};
+      // WHEN
+      VirtualRef(options)(Test, "test", descriptorOf(Test, "test"));
+
+      // THEN
+      store.get(MONGOOSE_SCHEMA).should.deep.eq({
+        ref: "RefTest",
+        localField: "test",
+        foreignField: "foreign",
+        justOne: false,
+        options: undefined
+      });
+
+    });
+  });
+
+  describe("when options is given with all fields", () => {
+    it("should set metadata", () => {
+      // GIVEN
+      class Test {
+      }
+
+      const store = Store.from(Test, "test", descriptorOf(Test, "test"));
+      const options = {
+        type: "RefTest",
+        foreignField: "foreign",
+        localField: "test_2",
+        justOne: true,
+        options: {}
+      };
+      // WHEN
+      VirtualRef(options)(Test, "test", descriptorOf(Test, "test"));
+
+      // THEN
+      store.get(MONGOOSE_SCHEMA).should.deep.eq({
+        ref: "RefTest",
+        localField: "test_2",
+        foreignField: "foreign",
+        justOne: true,
+        options: {}
+      });
     });
   });
 });
