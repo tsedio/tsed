Usama-Tahir/ts-express-decorators

docs: Update documentation

docs/.vuepress/config.js
@@ -17,6 +17,7 @@ module.exports = {
       apiKey: "f8a038207e461aaac0e2fd16403c2b01",
       indexName: "ts_ed"
     },
+
     locales: {
       "/": {
         label: "English",
@@ -39,6 +40,34 @@ module.exports = {
             link: "/configuration.html"
           },
           {
+            text: "Docs",
+            items: [
+              {
+                text: "Overview",
+                items: [
+                  {link: "/docs/controllers.html", text: "Controllers", items: []},
+                  {link: "/docs/providers.html", text: "Providers"},
+                  {link: "/docs/model.html", text: "Models"},
+                  {link: "/docs/converters.html", text: "Converters"},
+                  {link: "/docs/middlewares.html", text: "Middlewares"},
+                  {link: "/docs/filters.html", text: "Filters"},
+                  {link: "/docs/interceptors.html", text: "Interceptors"},
+                  {link: "/docs/authentication.html", text: "Authentication"},
+                  {link: "/docs/server-loader.html", text: "ServerLoader"}
+                ]
+              },
+              {
+                text: "Advanced",
+                items: [
+                  {link: "/docs/injection-scopes.html", text: "Injection scopes"},
+                  {link: "/docs/customer-provider.html", text: "Custom Providers"},
+                  {link: "/docs/testing.html", text: "Testing"},
+                  {link: "/api.html", text: "Api Reference"}
+                ]
+              }
+            ]
+          },
+          {
             text: "Guide",
             type: "links",
             items: [
@@ -59,28 +88,6 @@ module.exports = {
               {link: "/tutorials/not-found-page.html", text: "Customize 404"},
               {link: "/tutorials/aws.html", text: "AWS project"}
             ]
-          },
-          {
-            text: "Documentation",
-            items: [
-              {link: "/docs/controllers.html", text: "Controllers"},
-              {link: "/docs/services.html", text: "Services"},
-              {link: "/docs/factory.html", text: "Factory"},
-              {link: "/docs/provider.html", text: "Provider"},
-              {link: "/docs/model.html", text: "Models"},
-              {link: "/docs/converters.html", text: "Converters"},
-              {link: "/docs/middlewares.html", text: "Middlewares"},
-              {link: "/docs/scope.html", text: "Scope"},
-              {link: "/docs/filters.html", text: "Filters"},
-              {link: "/docs/interceptors.html", text: "Interceptors"},
-              {link: "/docs/authentication.html", text: "Authentication"},
-              {link: "/docs/server-loader.html", text: "ServerLoader"},
-              {link: "/docs/testing.html", text: "Testing"}
-            ]
-          },
-          {
-            text: "Api Reference",
-            link: "/api.html"
           }
         ],
 
@@ -102,25 +109,32 @@ module.exports = {
             ]
           }],
 
-          "/docs/": [{
-            title: "Documentation",
-            collapsable: false,
-            children: [
-              "controllers",
-              "services",
-              "factory",
-              "provider",
-              "model",
-              "converters",
-              "middlewares",
-              "scope",
-              "filters",
-              "interceptors",
-              "authentication",
-              "server-loader",
-              "testing"
-            ]
-          }],
+          "/docs/": [
+            {
+              title: "Overview",
+              collapsable: false,
+              children: [
+                "controllers",
+                "providers",
+                "model",
+                "converters",
+                "middlewares",
+                "filters",
+                "interceptors",
+                "authentication",
+                "server-loader"
+              ]
+            },
+            {
+              title: "Advanced",
+              collapsable: false,
+              children: [
+                "injection-scopes",
+                "custom-providers",
+                "testing"
+              ]
+            }
+          ],
 
 
           "/tutorials/": [{
@@ -166,17 +180,16 @@ module.exports = {
           "/tutorials/aws",
           "/tutorials/seq",
           "/docs/controllers",
-          "/docs/services",
-          "/docs/factory",
-          "/docs/provider",
+          "/docs/providers",
           "/docs/model",
           "/docs/converters",
           "/docs/middlewares",
-          "/docs/scope",
           "/docs/filters",
           "/docs/interceptors",
           "/docs/authentication",
           "/docs/server-loader",
+          "/docs/injection-scopes",
+          "/docs/custom-providers",
           "/docs/testing"
         ],
 

tsedio/tsed

docs/.vuepress/config.js
@@ -17,6 +17,7 @@ module.exports = {
       apiKey: "f8a038207e461aaac0e2fd16403c2b01",
       indexName: "ts_ed"
     },
+
     locales: {
       "/": {
         label: "English",
@@ -39,6 +40,35 @@ module.exports = {
             link: "/configuration.html"
           },
           {
+            text: "Docs",
+            items: [
+              {
+                text: "Overview",
+                items: [
+                  {link: "/docs/controllers.html", text: "Controllers", items: []},
+                  {link: "/docs/providers.html", text: "Providers"},
+                  {link: "/docs/model.html", text: "Models"},
+                  {link: "/docs/converters.html", text: "Converters"},
+                  {link: "/docs/middlewares.html", text: "Middlewares"},
+                  {link: "/docs/filters.html", text: "Filters"},
+                  {link: "/docs/interceptors.html", text: "Interceptors"},
+                  {link: "/docs/authentication.html", text: "Authentication"},
+                  {link: "/docs/server-loader.html", text: "ServerLoader"}
+                ]
+              },
+              {
+                text: "Advanced",
+                items: [
+                  {link: "/docs/injection-scopes.html", text: "Injection scopes"},
+                  {link: "/docs/custom-providers.html", text: "Custom providers"},
+                  {link: "/docs/custom-endpoint-decorators.html", text: "Custom endpoint decorators"},
+                  {link: "/docs/testing.html", text: "Testing"},
+                  {link: "/api.html", text: "Api Reference"}
+                ]
+              }
+            ]
+          },
+          {
             text: "Guide",
             type: "links",
             items: [
@@ -59,28 +89,6 @@ module.exports = {
               {link: "/tutorials/not-found-page.html", text: "Customize 404"},
               {link: "/tutorials/aws.html", text: "AWS project"}
             ]
-          },
-          {
-            text: "Documentation",
-            items: [
-              {link: "/docs/controllers.html", text: "Controllers"},
-              {link: "/docs/services.html", text: "Services"},
-              {link: "/docs/factory.html", text: "Factory"},
-              {link: "/docs/provider.html", text: "Provider"},
-              {link: "/docs/model.html", text: "Models"},
-              {link: "/docs/converters.html", text: "Converters"},
-              {link: "/docs/middlewares.html", text: "Middlewares"},
-              {link: "/docs/scope.html", text: "Scope"},
-              {link: "/docs/filters.html", text: "Filters"},
-              {link: "/docs/interceptors.html", text: "Interceptors"},
-              {link: "/docs/authentication.html", text: "Authentication"},
-              {link: "/docs/server-loader.html", text: "ServerLoader"},
-              {link: "/docs/testing.html", text: "Testing"}
-            ]
-          },
-          {
-            text: "Api Reference",
-            link: "/api.html"
           }
         ],
 
@@ -89,10 +97,6 @@ module.exports = {
             title: "Middlewares",
             collapsable: false,
             children: [
-              "global-middleware",
-              "global-error-middleware",
-              "endpoint-middleware",
-              "endpoint-error-middleware",
               "call-sequence",
               "override-middleware",
               "override/authentication",
@@ -102,25 +106,33 @@ module.exports = {
             ]
           }],
 
-          "/docs/": [{
-            title: "Documentation",
-            collapsable: false,
-            children: [
-              "controllers",
-              "services",
-              "factory",
-              "provider",
-              "model",
-              "converters",
-              "middlewares",
-              "scope",
-              "filters",
-              "interceptors",
-              "authentication",
-              "server-loader",
-              "testing"
-            ]
-          }],
+          "/docs/": [
+            {
+              title: "Overview",
+              collapsable: false,
+              children: [
+                "controllers",
+                "providers",
+                "model",
+                "converters",
+                "middlewares",
+                "filters",
+                "interceptors",
+                "authentication",
+                "server-loader"
+              ]
+            },
+            {
+              title: "Advanced",
+              collapsable: false,
+              children: [
+                "injection-scopes",
+                "custom-providers",
+                "custom-endpoint-decorators",
+                "testing"
+              ]
+            }
+          ],
 
 
           "/tutorials/": [{
@@ -166,17 +178,17 @@ module.exports = {
           "/tutorials/aws",
           "/tutorials/seq",
           "/docs/controllers",
-          "/docs/services",
-          "/docs/factory",
-          "/docs/provider",
+          "/docs/providers",
           "/docs/model",
           "/docs/converters",
           "/docs/middlewares",
-          "/docs/scope",
           "/docs/filters",
           "/docs/interceptors",
           "/docs/authentication",
           "/docs/server-loader",
+          "/docs/injection-scopes",
+          "/docs/custom-providers",
+          "/docs/custom-endpoint-decorators",
           "/docs/testing"
         ],
 