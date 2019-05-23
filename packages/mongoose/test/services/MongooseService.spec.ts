38f4bd6f8e5acc546a50d14146bc7c09c00f31e7
romain.lenzotti <rom.lenzotti@gmail.com>
2019-05-23 14:25:32 +0200

    chore: Add unit test

--- packages/mongoose/test/services/MongooseService.spec.ts
+++ packages/mongoose/test/services/MongooseService.spec.ts
@@ -1,5 +1,5 @@
 import {ServerSettingsService} from "@tsed/common";
-import {inject, TestContext} from "@tsed/testing";
+import {inject} from "@tsed/testing";
 import {expect} from "chai";
 import * as Mongoose from "mongoose";
 import * as Sinon from "sinon";
@@ -22,6 +22,10 @@ describe("MongooseService", () => {
         (Mongoose.connect as any).restore();
       })
     );
+
+    afterEach(() => {
+      (Mongoose.connect as any).reset();
+    });
 
     it("should call mongoose.connect", inject([MongooseService], async (mongooseService: MongooseService) => {
       // GIVEN
@@ -46,6 +50,52 @@ describe("MongooseService", () => {
       (Mongoose.connect as any).should.have.been.calledOnce.and.calledWithExactly("mongodb://test", {options: "options"});
       expect(mongooseService.get()).to.eq(undefined);
       expect(mongooseService.has()).to.eq(false);
+    }));
+
+    it("should close connection (1)", inject([MongooseService], async (mongooseService: MongooseService) => {
+      // GIVEN
+      const instance = {
+        connection: {
+          readyState: 1
+        },
+        disconnect: Sinon.stub()
+      };
+      (Mongoose.connect as any).resolves(instance);
+
+      // WHEN
+      await mongooseService
+        .connect(
+          "key1",
+          "mongodb://test",
+          {options: "options"} as any
+        );
+      await mongooseService.closeConnections();
+
+      // THEN
+      instance.disconnect.should.have.been.calledWithExactly();
+    }));
+
+    it("should close connection (2)", inject([MongooseService], async (mongooseService: MongooseService) => {
+      // GIVEN
+      const instance = {
+        connection: {
+          readyState: 2
+        },
+        disconnect: Sinon.stub()
+      };
+      (Mongoose.connect as any).resolves(instance);
+
+      // WHEN
+      await mongooseService
+        .connect(
+          "key2",
+          "mongodb://test",
+          {options: "options"} as any
+        );
+      await mongooseService.closeConnections();
+
+      // THEN
+      instance.disconnect.should.have.been.calledWithExactly();
     }));
   });
 });
