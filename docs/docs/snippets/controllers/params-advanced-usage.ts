9ca6826f822826d8b99ff29026bcc8d54d65404b
romain.lenzotti <rom.lenzotti@gmail.com>
2019-07-26 15:45:28 +0200

    chore: Fix controller documentation

--- docs/docs/snippets/controllers/params-advanced-usage.ts
+++ docs/docs/snippets/controllers/params-advanced-usage.ts
@@ -1,9 +1,6 @@
 import {BodyParams, Post} from "@tsed/common";
-import {Type} from "@tsed/core";
 
-abstract class BaseCRUDCtrl<T> {
-  protected abstract $model: Type<T>;
-
+class MyController {
   @Post()
   async create(@BodyParams({expression: "user", useConverter: false}) body: T): Promise<T> {
     console.log("payload", body);
