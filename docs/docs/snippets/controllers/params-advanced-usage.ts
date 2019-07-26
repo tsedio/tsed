97870c31ed48017b3fbf8ef49b3415b9feedafdb
romain.lenzotti <rom.lenzotti@gmail.com>
2019-07-26 15:44:30 +0200

    chore: Fix controller documentation

--- docs/docs/snippets/controllers/params-advanced-usage.ts
+++ docs/docs/snippets/controllers/params-advanced-usage.ts
@@ -5,7 +5,7 @@ abstract class BaseCRUDCtrl<T> {
   protected abstract $model: Type<T>;
 
   @Post()
-  async create(@BodyParams({expression: "user", useType: () => new this.$model}) body: T): Promise<T> {
+  async create(@BodyParams({expression: "user", useConverter: false}) body: T): Promise<T> {
     console.log("payload", body);
 
     return body;
