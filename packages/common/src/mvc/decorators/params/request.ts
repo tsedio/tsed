5ec5b92e13a81f7a711f3037b01144bc87db96e9
romain.lenzotti <rom.lenzotti@gmail.com>
2019-08-11 20:54:54 +0200

    feat(mvc): Request accept expression to get property from Request

--- packages/common/src/mvc/decorators/params/request.ts
+++ packages/common/src/mvc/decorators/params/request.ts
@@ -1,6 +1,9 @@
+import {Type} from "@tsed/core";
 import * as Express from "express";
-import {EXPRESS_REQUEST} from "../../../filters/constants";
+import {ParamTypes} from "../../models/ParamTypes";
 import {UseFilter} from "./useFilter";
+import {mapParamsOptions} from "./utils/mapParamsOptions";
+import {IParamOptions} from "../../interfaces/IParamOptions";
 
 export type Request = Express.Request;
 export type Req = Express.Request;
@@ -10,8 +13,14 @@ export type Req = Express.Request;
  * @returns {function(Function, (string|symbol), number): void}
  * @decorator
  */
-export function Request(): ParameterDecorator {
-  return Req();
+export function Request(expression: string, useType: Type<any>): ParameterDecorator;
+export function Request(expression: string): ParameterDecorator;
+export function Request(useType: Type<any>): ParameterDecorator;
+export function Request(options: IParamOptions<any>): ParameterDecorator;
+export function Request(): ParameterDecorator;
+export function Request(...args: any[]): ParameterDecorator {
+  // @ts-ignore
+  return Req(...args);
 }
 
 /**
@@ -20,6 +29,18 @@ export function Request(): ParameterDecorator {
  * @decorator
  * @alias Request
  */
-export function Req(): ParameterDecorator {
-  return UseFilter(EXPRESS_REQUEST);
+export function Req(expression: string, useType: Type<any>): ParameterDecorator;
+export function Req(expression: string): ParameterDecorator;
+export function Req(useType: Type<any>): ParameterDecorator;
+export function Req(options: IParamOptions<any>): ParameterDecorator;
+export function Req(): ParameterDecorator;
+export function Req(...args: any[]): ParameterDecorator {
+  const {expression, useType, useConverter = false, useValidation = false} = mapParamsOptions(args);
+
+  return UseFilter(ParamTypes.REQUEST, {
+    expression,
+    useType,
+    useConverter,
+    useValidation
+  });
 }
