7176bb85305841aa08642391faf48ffca190bdc0
Michael Pirouet <michael@clouud.xyz>
2019-08-27 20:29:18 +0100

    Rather than remove the output, only output when debug

--- packages/common/src/server/components/ServerLoader.ts
+++ packages/common/src/server/components/ServerLoader.ts
@@ -398,6 +398,10 @@ export abstract class ServerLoader implements IServerLifecycle {
     await this.callHook("$beforeInit");
     await this.callHook("$onInit");
 
+    this.settings.forEach((value, key) => {
+      this.injector.logger.debug(`settings.${key} =>`, value);
+    });
+
     this.injector.logger.info("Build providers");
 
     await loadInjector(this.injector);
