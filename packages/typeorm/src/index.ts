aa15e34349ff6cb2fd1aefe3e2d43b61ef52743b
romain.lenzotti <rom.lenzotti@gmail.com>
2019-09-02 19:36:06 +0200

    fix(typeorm): Fix typeorm configuration interface

--- packages/typeorm/src/index.ts
+++ packages/typeorm/src/index.ts
@@ -3,7 +3,7 @@ import {ConnectionOptions} from "typeorm";
 
 declare module "@tsed/common" {
   interface IServerSettingsOptions {
-    typeorm: {[connectionName: string]: ConnectionOptions};
+    typeorm: ConnectionOptions[];
   }
 }
 
