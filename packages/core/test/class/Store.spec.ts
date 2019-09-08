5614aa65dc1616043366af98bf39a4d35edf20c1
romain.lenzotti <rom.lenzotti@gmail.com>
2019-09-08 11:35:17 +0200

    feat(core): Add support to create Store over Symbol

--- packages/core/test/class/Store.spec.ts
+++ packages/core/test/class/Store.spec.ts
@@ -20,37 +20,39 @@ class SuperFake extends FakeMetadata {
 describe("Store", () => {
   describe("constructor", () => {
     describe("when metadata should be store on class", () => {
+      let spyGet: any, store: any, store2: any, store3: any;
       before(() => {
-        this.spyGet = Sinon.spy(Metadata, "getOwn");
-        this.store = new Store([FakeMetadata]);
-        this.store2 = new Store([FakeMetadata]);
-        this.store3 = new Store([class {
+        spyGet = Sinon.spy(Metadata, "getOwn");
+        store = new Store([FakeMetadata]);
+        store2 = new Store([FakeMetadata]);
+        store3 = new Store([class {
         }]);
 
-        this.store.set("keyTest", {test: "2"});
+        store.set("keyTest", {test: "2"});
       });
       after(() => {
-        this.spyGet.restore();
-        this.store.clear();
+        spyGet.restore();
+        store.clear();
       });
 
       it("should have been called the Metadata.get()", () => {
-        this.spyGet.should.have.been.calledWithExactly(CLASS_STORE, FakeMetadata);
+        spyGet.should.have.been.calledWithExactly(CLASS_STORE, FakeMetadata);
       });
 
       it("should share the same StoreMap when the signature is equals", () => {
-        expect(this.store.get("keyTest")).to.eq(this.store2.get("keyTest"));
+        expect(store.get("keyTest")).to.eq(store2.get("keyTest"));
       });
 
       it("should not share the same StoreMap when the signature is not equals", () => {
-        expect(this.store.get("keyTest")).not.to.eq(this.store3.get("keyTest"));
+        expect(store.get("keyTest")).not.to.eq(store3.get("keyTest"));
       });
     });
 
     describe("when metadata should be store on method", () => {
+      let spyGet: any, store: any;
       before(() => {
-        this.spyGet = Sinon.spy(Metadata, "getOwn");
-        this.store = new Store([
+        spyGet = Sinon.spy(Metadata, "getOwn");
+        store = new Store([
           FakeMetadata,
           "get",
           {
@@ -60,32 +62,34 @@ describe("Store", () => {
         ]);
       });
       after(() => {
-        this.spyGet.restore();
+        spyGet.restore();
       });
 
       it("should have been called the Metadata.get()", () => {
-        this.spyGet.should.have.been.calledWithExactly(METHOD_STORE, FakeMetadata, "get");
+        spyGet.should.have.been.calledWithExactly(METHOD_STORE, FakeMetadata, "get");
       });
     });
 
     describe("when metadata should be store on property (1)", () => {
+      let spyGet: any, store: any;
       before(() => {
-        this.spyGet = Sinon.spy(Metadata, "getOwn");
-        this.store = new Store([FakeMetadata, "get"]);
+        spyGet = Sinon.spy(Metadata, "getOwn");
+        store = new Store([FakeMetadata, "get"]);
       });
       after(() => {
-        this.spyGet.restore();
+        spyGet.restore();
       });
 
       it("should have been called the Metadata.get()", () => {
-        this.spyGet.should.have.been.calledWithExactly(PROPERTY_STORE, FakeMetadata, "get");
+        spyGet.should.have.been.calledWithExactly(PROPERTY_STORE, FakeMetadata, "get");
       });
     });
 
     describe("when metadata should be store on property (2)", () => {
+      let spyGet: any, store: any;
       before(() => {
-        this.spyGet = Sinon.spy(Metadata, "getOwn");
-        this.store = new Store([
+        spyGet = Sinon.spy(Metadata, "getOwn");
+        store = new Store([
           FakeMetadata,
           "get",
           {
@@ -95,18 +99,19 @@ describe("Store", () => {
         ]);
       });
       after(() => {
-        this.spyGet.restore();
+        spyGet.restore();
       });
 
       it("should have been called the Metadata.get()", () => {
-        this.spyGet.should.have.been.calledWithExactly(PROPERTY_STORE, FakeMetadata, "get");
+        spyGet.should.have.been.calledWithExactly(PROPERTY_STORE, FakeMetadata, "get");
       });
     });
 
     describe("when metadata should be store on property (3)", () => {
+      let spyGet: any, store: any;
       before(() => {
-        this.spyGet = Sinon.spy(Metadata, "getOwn");
-        this.store = new Store([
+        spyGet = Sinon.spy(Metadata, "getOwn");
+        store = new Store([
           FakeMetadata,
           "get",
           {
@@ -116,202 +121,218 @@ describe("Store", () => {
         ]);
       });
       after(() => {
-        this.spyGet.restore();
+        spyGet.restore();
       });
 
       it("should have been called the Metadata.get()", () => {
-        this.spyGet.should.have.been.calledWithExactly(PROPERTY_STORE, FakeMetadata, "get");
+        spyGet.should.have.been.calledWithExactly(PROPERTY_STORE, FakeMetadata, "get");
       });
     });
 
     describe("when metadata should be store on parameters", () => {
+      let spyGet: any, store: any;
       before(() => {
-        this.spyGet = Sinon.spy(Metadata, "getOwn");
-        this.store = new Store([FakeMetadata, "get", 0]);
+        spyGet = Sinon.spy(Metadata, "getOwn");
+        store = new Store([FakeMetadata, "get", 0]);
       });
       after(() => {
-        this.spyGet.restore();
+        spyGet.restore();
       });
 
       it("should have been called the Metadata.get()", () => {
-        this.spyGet.should.have.been.calledWithExactly(PARAM_STORE, FakeMetadata, "get");
+        spyGet.should.have.been.calledWithExactly(PARAM_STORE, FakeMetadata, "get");
       });
     });
   });
 
   describe("set()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
-      this.store.set("key", {});
+      store = new Store([FakeMetadata]);
+      store.set("key", {});
     });
     it("should add a metadata", () => {
-      expect(this.store.size).to.equal(1);
+      expect(store.size).to.equal(1);
     });
   });
 
   describe("has()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
+      store = new Store([FakeMetadata]);
     });
     it("should add a metadata", () => {
-      this.store.set("key", {});
-      expect(this.store.size).to.equal(1);
+      store.set("key", {});
+      expect(store.size).to.equal(1);
     });
     it("should return true if class is known", () => {
-      expect(this.store.has("key")).to.be.true;
+      expect(store.has("key")).to.be.true;
     });
     it("should return false if class is unknown", () => {
-      expect(this.store.has("key2")).to.be.false;
+      expect(store.has("key2")).to.be.false;
     });
   });
 
   describe("get()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
+      store = new Store([FakeMetadata]);
     });
     it("should add a metadata", () => {
-      this.store.set("key", {test: true});
-      expect(this.store.size).to.equal(1);
+      store.set("key", {test: true});
+      expect(store.size).to.equal(1);
     });
     it("should get metadata", () => {
-      expect(this.store.get("key").test).to.be.true;
+      expect(store.get("key").test).to.be.true;
     });
   });
 
   describe("entries()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
+      store = new Store([FakeMetadata]);
     });
     it("should add a metadata", () => {
-      this.store.set("key", {test: true});
-      expect(this.store.size).to.equal(1);
+      store.set("key", {test: true});
+      expect(store.size).to.equal(1);
     });
 
     it("should return entries", () => {
-      expect(typeof this.store.entries()).to.equal("object");
+      expect(typeof store.entries()).to.equal("object");
     });
   });
 
   describe("keys()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
-      this.store.set("test", {test: true});
+      store = new Store([FakeMetadata]);
+      store.set("test", {test: true});
     });
 
     it("should return the keys", () => {
-      expect(typeof this.store.keys()).to.equal("object");
+      expect(typeof store.keys()).to.equal("object");
     });
   });
 
   describe("clear()", () => {
+    let store: any;
     before(() => {
-      this.store.set("key", {test: true});
-      this.store.clear();
+      store = new Store([FakeMetadata]);
+      store.set("key", {test: true});
+      store.clear();
     });
 
     it("should remove all keys", () => {
-      expect(this.store.size).to.equal(0);
+      expect(store.size).to.equal(0);
     });
   });
 
   describe("delete()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
+      store = new Store([FakeMetadata]);
     });
     it("should add a metadata", () => {
-      this.store.set("key", {test: true});
-      expect(this.store.size).to.equal(1);
+      store.set("key", {test: true});
+      expect(store.size).to.equal(1);
     });
 
     it("should remove all keys", () => {
-      this.store.delete("key");
-      expect(this.store.size).to.equal(0);
+      store.delete("key");
+      expect(store.size).to.equal(0);
     });
   });
 
   describe("forEach()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
+      store = new Store([FakeMetadata]);
     });
     it("should add a metadata", () => {
-      this.store.set("key", {test: true});
-      expect(this.store.size).to.equal(1);
+      store.set("key", {test: true});
+      expect(store.size).to.equal(1);
     });
 
     it("should loop for each item stored in registry", () => {
       const o: any = [];
-      this.store.forEach((e: any) => o.push(e));
+      store.forEach((e: any) => o.push(e));
       expect(o.length).to.equal(1);
     });
   });
 
   describe("values()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
+      store = new Store([FakeMetadata]);
     });
     it("should add a metadata", () => {
-      this.store.set("key", {test: true});
-      expect(this.store.size).to.equal(1);
+      store.set("key", {test: true});
+      expect(store.size).to.equal(1);
     });
 
     it("should return the values", () => {
-      expect(typeof this.store.values()).to.equal("object");
+      expect(typeof store.values()).to.equal("object");
     });
   });
 
   describe("merge()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
-      this.store.merge("key3", {attr1: 1});
-      this.store.merge("key3", {attr2: 2});
+      store = new Store([FakeMetadata]);
+      store.merge("key3", {attr1: 1});
+      store.merge("key3", {attr2: 2});
     });
 
     it("should merge metadata", () => {
-      expect(this.store.get("key3")).to.deep.equal({attr1: 1, attr2: 2});
+      expect(store.get("key3")).to.deep.equal({attr1: 1, attr2: 2});
     });
   });
 
   describe("storeValues()", () => {
+    let store: any;
     before(() => {
-      this.store = new Store([FakeMetadata]);
-      this.store.storeValues({content: "json", content2: "json2"});
+      store = new Store([FakeMetadata]);
+      store.storeValues({content: "json", content2: "json2"});
     });
 
     it("should store data", () => {
-      expect(this.store.get("content")).to.eq("json");
+      expect(store.get("content")).to.eq("json");
     });
     it("should store data", () => {
-      expect(this.store.get("content2")).to.eq("json2");
+      expect(store.get("content2")).to.eq("json2");
     });
   });
 
-  // describe("decorate()", () => {
-  //   before(() => {
-  //     this.parameters = [FakeMetadata, "test", descriptorOf(FakeMetadata, "test")];
-  //     this.cbStub = Sinon.stub();
-  //     this.fnStub = Sinon.stub().returns(this.cbStub);
-  //     StoreFn(this.fnStub)(...this.parameters);
-  //   });
-  //
-  //   it("should have been called the function", () => {
-  //     this.fnStub.should.be.calledOnce.and.calledWithExactly(Sinon.match.instanceOf(Store), this.parameters);
-  //   });
-  //
-  //   it("should have been called the return function with the parameters", () => {
-  //     this.cbStub.should.be.calledOnce.and.calledWithExactly(...this.parameters);
-  //   });
-  // });
-
   describe("inheritance", () => {
+    let r1: any, r2: any;
     before(() => {
       Store.from(FakeMetadata).set("sc", {test: "test"});
       Store.from(SuperFake).set("sc", {test: "test2"});
-      this.r1 = Store.from(SuperFake).get("sc");
-      this.r2 = Store.from(FakeMetadata).get("sc");
+      r1 = Store.from(SuperFake).get("sc");
+      r2 = Store.from(FakeMetadata).get("sc");
     });
 
     it("should haven't the same sc", () => {
-      expect(this.r1).to.not.deep.equal(this.r2);
+      expect(r1).to.not.deep.equal(r2);
+    });
+  });
+
+  describe("from()", () => {
+    describe("from Symbol", () => {
+      it("should create a store from Symbol", () => {
+        // GIVEN
+        const TOKEN = Symbol.for("token");
+        const TOKEN2 = Symbol.for("token2");
+        const store1 = Store.from(TOKEN);
+        const store2 = Store.from(TOKEN);
+        const store3 = Store.from(TOKEN2);
+
+        // WHEN
+        store1.set("test", "value");
+
+        expect(store2.get("test")).to.eq("value");
+        expect(store3.get("test")).to.eq(undefined);
+      });
     });
   });
 });
