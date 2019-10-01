95004cea16b7e8602db416794facf6f86a17408f
9KwHCUDP <54501060+9KwHCUDP@users.noreply.github.com>
2019-10-01 12:58:16 +0300

    fix(seq): Remove bold wraps

--- packages/seq/src/appenders/SeqAppender.ts
+++ packages/seq/src/appenders/SeqAppender.ts
@@ -52,7 +52,11 @@ export class SeqAppender extends BaseAppender {
     const level = loggingEvent.level.toString().toLowerCase();
 
     if (level !== "OFF" && this.logger[level]) {
-      this.logger[level](...loggingEvent.data);
+      const data = loggingEvent.data;
+      if (typeof data[0] === "string") {
+        data[0] = data[0].replace(/\[1m|\[22m/g, "");
+      }
+      this.logger[level](...data);
     }
   }
 }
