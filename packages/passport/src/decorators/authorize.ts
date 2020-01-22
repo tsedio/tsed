1efa680aeaea18b29ec3564ebb9cd054c5886e8f
romain.lenzotti <rom.lenzotti@gmail.com>
2020-01-22 08:13:25 +0100

    fix(passport): Add AuthenticateOption on decorator signature

--- packages/passport/src/decorators/authorize.ts
+++ packages/passport/src/decorators/authorize.ts
@@ -1,7 +1,8 @@
 import {UseAuth} from "@tsed/common";
+import {AuthenticateOptions} from "passport";
 import {PassportMiddleware} from "../middlewares/PassportMiddleware";
 
-export function Authorize(protocol: string | string[], options: any = {}): Function {
+export function Authorize(protocol: string | string[], options: AuthenticateOptions & {security?: any} = {}): Function {
   return UseAuth(PassportMiddleware, {
     protocol,
     method: "authorize",
