0fbbd430944bbf1ade0fddc5bac15a48c5696431
Giray <giray@storyofams.com>
2020-02-13 21:28:54 +0100

    feat(mongoose): Trim decorator

---
+++ packages/mongoose/test/decorators/trim.spec.ts
@@ -0,0 +1,20 @@
+import {Store} from "@tsed/core";
+import {descriptorOf} from "@tsed/core";
+import {Trim} from "../../src/decorators";
+import {expect} from "chai";
+import {MONGOOSE_SCHEMA} from "../../src/constants";
+
+describe("@Trim()", () => {
+  class Test {}
+
+  before(() => {
+    Trim()(Test, "test", descriptorOf(Test, "test"));
+    this.store = Store.from(Test, "test", descriptorOf(Test, "test"));
+  });
+
+  it("should set metadata", () => {
+    expect(this.store.get(MONGOOSE_SCHEMA)).to.deep.eq({
+      trim: true
+    });
+  });
+});
