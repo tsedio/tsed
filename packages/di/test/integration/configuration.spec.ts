37a534978353b9820e59cc9985bcc7be0c7b8992
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-05-27 16:47:48 +0200

    feat(di): Merge module configuration with the server configuration.

---
+++ packages/di/test/integration/configuration.spec.ts
@@ -0,0 +1,47 @@
+import {Controller, createInjector, Get, Module} from "@tsed/common/src";
+import {expect} from "chai";
+
+describe("DI", () => {
+  it("should merge DI configuration", async () => {
+    @Controller("/global")
+    class ControllerGlobal {
+      @Get("/")
+      get() {
+      }
+    }
+
+    @Controller("/module")
+    class ControllerModule {
+      @Get("/")
+      get() {
+      }
+    }
+
+    @Module({
+      mount: {
+        "/rest": [
+          ControllerModule
+        ]
+      }
+    })
+    class MyModule {
+    }
+
+    const injector = createInjector({
+      mount: {
+        "/rest": [ControllerGlobal]
+      },
+      imports: [MyModule]
+    });
+    injector.add(MyModule);
+
+    injector.resolveConfiguration();
+
+    expect(injector.settings.mount).to.deep.eq({
+      "/rest": [
+        ControllerModule,
+        ControllerGlobal
+      ]
+    });
+  });
+});
