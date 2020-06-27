a479d0ad930194b3a1d307db426032cb1952f1e7
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-06-27 09:10:05 +0200

    chore: add integration test

---
+++ packages/mongoose/test/helpers/models/UserWorkspace.ts
@@ -0,0 +1,30 @@
+import {Property, PropertyType} from "@tsed/common";
+import {Model, MongooseModel, ObjectID, Ref, Schema} from "@tsed/mongoose";
+import {Types} from "mongoose";
+
+@Model()
+export class Workspace {
+  @ObjectID("id")
+  _id: string;
+
+  @Property()
+  name: string;
+}
+
+@Schema()
+export class MyWorkspace {
+  @Ref(Workspace)
+  workspaceId: Ref<Workspace>;
+
+  @Property()
+  title: string;
+}
+
+@Model()
+export class User {
+  @PropertyType(MyWorkspace)
+  workspaces: MyWorkspace[];
+}
+
+export type UserModel = MongooseModel<User & {workspaces: Types.Array<MyWorkspace>}>;
+export type WorkspaceModel = MongooseModel<Workspace>;
