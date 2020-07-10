2fca119e46bb302b2aa1556b899d351d82ab876c
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-07-10 19:58:35 +0200

    chore: Remove unused files

--- tasks/gulp/utils/all.js
+++
@@ -1,9 +0,0 @@
-exports.all = (...args) => {
-  return Promise.all(
-    args.map((stream) =>
-      new Promise((resolve, reject) =>
-        stream.on('error', reject).on('end', resolve)
-      )
-    )
-  );
-};
