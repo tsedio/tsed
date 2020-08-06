1e4b14aa3b3b41a921b37a32f6799d67e0cfb77a
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-08-06 09:07:03 +0200

    fix(swagger): Add depreciation note on BaseParameter, Responses, Operation decorator

--- packages/swagger/src/decorators/responses.ts
+++ packages/swagger/src/decorators/responses.ts
@@ -8,6 +8,7 @@ import {Response} from "swagger-schema-official";
  * @returns {Function}
  * @decorator
  * @swagger
+ * @deprecated Use @Returns from @tsed/schema. Will be removed in v6.
  */
 export function Responses(status: string | number, response: Response): Function {
   return (...args: DecoratorParameters) => {
