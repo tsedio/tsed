50ed9819b109b7053ead1aa8cec49ea1e50f67a2
Romain Lenzotti <romain.lenzotti@gmail.com>
2020-08-17 15:34:26 +0200

    chore: Remove @tsed/testing and ts-express-decorators
    Testing utils are now embed in @tsed/common module.
    
    BREAKING CHANGE: Remove @tsed/testing and ts-express-decorators packages

--- packages/common/src/mvc/services/ParseService.spec.ts
+++ packages/common/src/mvc/services/ParseService.spec.ts
@@ -1,6 +1,5 @@
-import {inject} from "@tsed/testing";
 import {expect} from "chai";
-import {ParseService} from "../../../src";
+import {ParseService, PlatformTest} from "../../../src";
 
 describe("ParseService :", () => {
   it("should clone object", () => {
@@ -9,52 +8,67 @@ describe("ParseService :", () => {
     expect(ParseService.clone(source)).not.to.be.equal(source);
   });
 
-  it("should not clone object", inject([ParseService], (parserService: ParseService) => {
-    const source = {test: {}};
+  it(
+    "should not clone object",
+    PlatformTest.inject([ParseService], (parserService: ParseService) => {
+      const source = {test: {}};
 
-    expect(parserService.eval(undefined!, source, false)).to.equal(source);
-    expect(parserService.eval("test", source, false)).to.equal(source.test);
-  }));
+      expect(parserService.eval(undefined!, source, false)).to.equal(source);
+      expect(parserService.eval("test", source, false)).to.equal(source.test);
+    })
+  );
 
-  it("should eval expression with a scope and return value", inject([ParseService], (parserService: ParseService) => {
-    expect(
-      parserService.eval(undefined!, {
-        test: "yes"
-      }).test
-    ).to.equal("yes");
+  it(
+    "should eval expression with a scope and return value",
+    PlatformTest.inject([ParseService], (parserService: ParseService) => {
+      expect(
+        parserService.eval(undefined!, {
+          test: "yes"
+        }).test
+      ).to.equal("yes");
 
-    expect(parserService.eval(undefined!, "test")).to.equal("test");
-  }));
+      expect(parserService.eval(undefined!, "test")).to.equal("test");
+    })
+  );
 
-  it("should eval expression with a scope and return value", inject([ParseService], (parserService: ParseService) => {
-    expect(
-      parserService.eval("test", {
-        test: "yes"
-      })
-    ).to.equal("yes");
-  }));
+  it(
+    "should eval expression with a scope and return value",
+    PlatformTest.inject([ParseService], (parserService: ParseService) => {
+      expect(
+        parserService.eval("test", {
+          test: "yes"
+        })
+      ).to.equal("yes");
+    })
+  );
 
-  it("should eval expression with a scope and return value", inject([ParseService], (parserService: ParseService) => {
-    expect(
-      parserService.eval("test.foo", {
-        test: "yes"
-      })
-    ).to.equal(undefined);
-  }));
+  it(
+    "should eval expression with a scope and return value",
+    PlatformTest.inject([ParseService], (parserService: ParseService) => {
+      expect(
+        parserService.eval("test.foo", {
+          test: "yes"
+        })
+      ).to.equal(undefined);
+    })
+  );
 
-  it("should eval expression with a scope and return a new object", inject([ParseService], (parserService: ParseService) => {
-    const scope = {
-      test: {
-        foo: "yes"
-      }
-    };
+  it(
+    "should eval expression with a scope and return a new object",
+    PlatformTest.inject([ParseService], (parserService: ParseService) => {
+      const scope = {
+        test: {
+          foo: "yes"
+        }
+      };
 
-    const value = parserService.eval("test", scope);
+      const value = parserService.eval("test", scope);
 
-    expect(value.foo).to.equal("yes");
+      expect(value.foo).to.equal("yes");
 
-    value.foo = "test";
+      value.foo = "test";
 
-    expect(value.foo).to.not.equal(scope.test.foo); // New instance
-  }));
+      expect(value.foo).to.not.equal(scope.test.foo); // New instance
+    })
+  );
 });
