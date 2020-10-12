f5cd3b91a823d1aafaf699f88fdfcecc896d0a99
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-10-12 15:00:22 +0200

    fix(platform-express): Fix issue on Express typings inference

--- packages/platform-express/src/services/PlatformExpressResponse.ts
+++ packages/platform-express/src/services/PlatformExpressResponse.ts
@@ -2,9 +2,6 @@ import {PlatformResponse} from "@tsed/common";
 import type * as Express from "express";
 
 declare global {
-  namespace Express {
-    export interface Response {}
-  }
   namespace TsED {
     export interface Response extends Express.Response {}
   }
