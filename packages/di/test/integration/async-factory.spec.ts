6480e31397bd0558c007d3895789e52cd6787235
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-11-10 10:28:35 +0100

    fix(di): Fix asyncFactory when the bean is injected with @Inject on server
    Closes: 1065

---
+++ packages/di/test/integration/async-factory.spec.ts
@@ -0,0 +1,44 @@
+import {isPromise} from "@tsed/core";
+import {expect} from "chai";
+import {Container, GlobalProviders, Inject, Injectable, InjectorService, registerProvider} from "../../src";
+
+describe("DI", () => {
+  describe("create new injector", () => {
+    const ASYNC_FACTORY = Symbol.for("ASYNC_FACTORY");
+
+    registerProvider({
+      provide: ASYNC_FACTORY,
+      useAsyncFactory() {
+        return Promise.resolve({connection: true});
+      }
+    });
+
+    @Injectable()
+    class Server {
+      @Inject(ASYNC_FACTORY)
+      asyncFactory: any;
+    }
+
+    after(() => {
+      GlobalProviders.delete(Server);
+      GlobalProviders.delete(ASYNC_FACTORY);
+    });
+
+    it("should load all providers with the SINGLETON scope only", async () => {
+      // GIVEN
+      const injector = new InjectorService();
+      const container = new Container();
+      container.add(ASYNC_FACTORY);
+      container.add(Server);
+
+      const server = injector.invoke<any>(Server);
+      expect(isPromise(server.asyncFactory)).to.eq(true);
+
+      // WHEN
+      await injector.load(container);
+
+      expect(isPromise(server.asyncFactory)).to.eq(false);
+      expect(server.asyncFactory).to.deep.eq({connection: true});
+    });
+  });
+});
