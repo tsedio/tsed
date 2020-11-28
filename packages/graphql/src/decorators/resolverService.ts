a1491289a710b219c2186b974f0b685c66538eef
Gustavo Mendonça <gustavohm.mendonca@gmail.com>
2020-11-28 12:34:54 -0300

    fix(graphql): fixed ResolverService decorator using wrong options type

--- packages/graphql/src/decorators/resolverService.ts
+++ packages/graphql/src/decorators/resolverService.ts
@@ -1,11 +1,11 @@
 import {ClassType, Resolver} from "type-graphql";
-import {ClassTypeResolver, ResolverClassOptions} from "type-graphql/dist/decorators/types";
+import {ClassTypeResolver, AbstractClassOptions} from "type-graphql/dist/decorators/types";
 import {registerResolverService} from "../registries/ResolverServiceRegistry";
 
 export function ResolverService(): ClassDecorator;
-export function ResolverService(options: ResolverClassOptions): ClassDecorator;
-export function ResolverService(typeFunc: ClassTypeResolver, options?: ResolverClassOptions): ClassDecorator;
-export function ResolverService(objectType: ClassType, options?: ResolverClassOptions): ClassDecorator;
+export function ResolverService(options: AbstractClassOptions): ClassDecorator;
+export function ResolverService(typeFunc: ClassTypeResolver, options?: AbstractClassOptions): ClassDecorator;
+export function ResolverService(objectType: ClassType, options?: AbstractClassOptions): ClassDecorator;
 export function ResolverService(...args: any[]): ClassDecorator {
   return <TFunction extends Function>(target: TFunction): void => {
     Resolver.apply(this, args)(target);
