0904fd70b4ebe9149f07ff020e0680f7eba4dd4f
Luís Sousa <luis@filipe.xyz>
2020-12-27 22:30:14 -0300

    fix(ajv): Add missing keyword exports

--- packages/ajv/src/index.ts
+++ packages/ajv/src/index.ts
@@ -3,6 +3,8 @@ export * from "./decorators/useSchema";
 export * from "./pipes/AjvValidationPipe";
 export * from "./pipes/AjvErrorFormatterPipe";
 export * from "./services/Ajv";
+export * from "./decorators/keyword";
+export * from "./interfaces/KeywordMethods";
 
 import {IAjvSettings} from "./interfaces/IAjvSettings";
 
