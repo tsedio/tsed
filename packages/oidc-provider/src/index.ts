6fc6e3e285f5c51147571c02f91cfb0661e6a418
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-01-04 18:54:20 +0100

    fix(oidc): Add missing import on oidc-provider module

--- packages/oidc-provider/src/index.ts
+++ packages/oidc-provider/src/index.ts
@@ -1,8 +1,15 @@
 export * from "./decorators";
+
 export * from "./domain";
+
+export * from "./middlewares/OidcInteractionMiddleware";
+export * from "./middlewares/OidcNoCacheMiddleware";
+export * from "./middlewares/OidcSecureMiddleware";
+
 export * from "./services/OidcAdapters";
 export * from "./services/OidcInteractions";
 export * from "./services/OidcInteractionContext";
 export * from "./services/OidcJwks";
 export * from "./services/OidcProvider";
+
 export * from "./OidcModule";
