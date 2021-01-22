741d165e101d9be75a877cbb5f738c5b36b87975
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-01-22 19:23:32 +0100

    chore(common): Add complementary unit test
    Closes: #1198

--- packages/platform-test-utils/src/tests/testMulter.ts
+++ packages/platform-test-utils/src/tests/testMulter.ts
@@ -1,6 +1,8 @@
 import {BodyParams, Controller, MulterOptions, MultipartFile, PlatformMulterFile, PlatformTest, Post} from "@tsed/common";
 import {CollectionOf, Property, Required, Status} from "@tsed/schema";
 import {expect} from "chai";
+import {Request} from "express";
+import multer, {FileFilterCallback} from "multer";
 import SuperTest from "supertest";
 import {PlatformTestOptions} from "../interfaces";
 
@@ -21,6 +23,20 @@ export class Event {
   tasks: Task[];
 }
 
+function getFileConfig(): any {
+  return {
+    storage: multer.diskStorage({
+      destination: (req: Express.Request, _fileItem, cb) => {
+        const path = `${__dirname}/../.tmp`;
+        cb(null, path);
+      }
+    }),
+    fileFilter: (req: Request, file: PlatformMulterFile, callback: FileFilterCallback) => {
+      callback(null, true);
+    }
+  };
+}
+
 @Controller("/multer")
 export class TestMulterController {
   @Post("/scenario-1")
@@ -33,15 +49,19 @@ export class TestMulterController {
   @Post("/scenario-2")
   @Status(201)
   @MulterOptions({dest: `${__dirname}/../.tmp`})
-  uploadWithPayload(
-    @MultipartFile("media") media: PlatformMulterFile,
-    @BodyParams("form_id") formId: string,
-    @BodyParams("event") event: Event
-  ) {
+  uploadWithPayload(@MultipartFile("media") media: PlatformMulterFile, @BodyParams("form_id") formId: string) {
     return {
       file: media.originalname,
-      formId,
-      event
+      formId
+    };
+  }
+
+  @Post("/scenario-3")
+  @Status(201)
+  @MulterOptions(getFileConfig())
+  uploadWithOptions(@MultipartFile("media") media: PlatformMulterFile) {
+    return {
+      file: media.originalname
     };
   }
 }
@@ -85,6 +105,28 @@ export function testMulter(options: PlatformTestOptions) {
             schemaPath: "#/required"
           }
         ]
+      });
+    });
+  });
+  describe("Scenario 2: POST /rest/multer/scenario-2", () => {
+    it("should upload file with multer", async () => {
+      const result = await request
+        .post("/rest/multer/scenario-2")
+        .attach("media", `${__dirname}/../data/file.txt`)
+        .field({
+          form_id: "form_id"
+        })
+        .expect(201);
+
+      expect(result.body).to.deep.eq({file: "file.txt", formId: "form_id"});
+    });
+  });
+  describe("Scenario 3: POST /rest/multer/scenario-3", () => {
+    it("should upload file with multer", async () => {
+      const result = await request.post("/rest/multer/scenario-3").attach("media", `${__dirname}/../data/file.txt`).expect(201);
+
+      expect(result.body).to.deep.eq({
+        file: "file.txt"
       });
     });
   });
