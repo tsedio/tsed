a37bf809c042cd40fc9f07050ad09c8a5f230868
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-02-05 22:15:51 +0100

    docs: Update documentation

---
+++ docs/docs/snippets/controllers/params-post-enum.ts
@@ -0,0 +1,26 @@
+import {Controller, Post, QueryParams} from "@tsed/common";
+import {Enum} from "@tsed/schema";
+
+export enum SearchType {
+  PARTIAL = "partial",
+  EXTENDED = "extended",
+}
+
+@Controller("/enums")
+export class AnyCtrl {
+  // BREAKING CHANGE with v5
+  @Post()
+  updatePayload1(@QueryParams("type") type = SearchType.PARTIAL): any {
+    // type = SearchType.PARTIAL will considered by typescript as any => Object
+  }
+
+  // WORKS
+  @Post()
+  updatePayload2(@QueryParams("type") type: SearchType = SearchType.PARTIAL): any {
+  }
+
+  // Add validation
+  @Post()
+  updatePayload3(@QueryParams("type") @Enum(SearchType) type: SearchType = SearchType.PARTIAL): any {
+  }
+}
