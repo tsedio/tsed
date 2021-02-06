934afa1fae0ef2618600c5c0632ca5241ecddbc2
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-02-06 20:30:32 +0100

    fix(json-mapper): Fix mixed type deserialization on DateMapper
    Closes: #1231

--- packages/json-mapper/src/components/DateMapper.spec.ts
+++ packages/json-mapper/src/components/DateMapper.spec.ts
@@ -3,13 +3,32 @@ import {DateMapper} from "./DateMapper";
 
 describe("DateMapper", () => {
   describe("deserialize()", () => {
-    it("should return value", () => {
+    it("should return a Date when the data is a string", () => {
       const date = new Date();
       const mapper = new DateMapper();
 
       const value = mapper.deserialize(date.toISOString());
 
       expect(value).to.deep.eq(date);
+    });
+
+    it("should return a Date when the data is a number", () => {
+      const date = new Date();
+      const mapper = new DateMapper();
+
+      const value = mapper.deserialize(date.getTime());
+
+      expect(value).to.deep.eq(date);
+    });
+
+    it("should return value when the data is a boolean/null/undefined", () => {
+      const date = new Date();
+      const mapper = new DateMapper();
+
+      expect(mapper.deserialize(false)).to.eq(false);
+      expect(mapper.deserialize(true)).to.eq(true);
+      expect(mapper.deserialize(null)).to.eq(null);
+      expect(mapper.deserialize(undefined)).to.eq(undefined);
     });
   });
   describe("serialize()", () => {
--- packages/json-mapper/src/components/DateMapper.ts
+++ packages/json-mapper/src/components/DateMapper.ts
@@ -1,3 +1,4 @@
+import {isBoolean} from "@tsed/core";
 import {JsonMapper} from "../decorators/jsonMapper";
 import {JsonMapperMethods} from "../interfaces/JsonMapperMethods";
 
@@ -8,7 +9,16 @@ import {JsonMapperMethods} from "../interfaces/JsonMapperMethods";
  */
 @JsonMapper(Date)
 export class DateMapper implements JsonMapperMethods {
-  deserialize(data: string): Date {
+  deserialize(data: string | number): Date;
+  deserialize(data: boolean | null | undefined): boolean | null | undefined;
+  deserialize(data: any): any {
+    // don't convert unexpected data. In normal case, Ajv reject unexpected data.
+    // But by default, we have to skip data deserialization and let user to apply
+    // the right mapping
+    if (isBoolean(data) || data === null || data === undefined) {
+      return data;
+    }
+
     return new Date(data);
   }
 
