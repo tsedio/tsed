b128751891f09be490f638b19ecd70ddfe0995fe
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-02-08 09:41:23 +0100

    fix(schema): Add @CustomKeys

--- packages/schema/src/decorators/common/customKey.ts
+++ packages/schema/src/decorators/common/customKey.ts
@@ -18,3 +18,22 @@ export function CustomKey(key: string, value: any) {
     store.itemSchema.customKey(key, value);
   });
 }
+/**
+ * Set a group of custom keys on JsonSchema that is not a part of the official spec.
+ *
+ * This custom key can only be displayed if the @@getJsonSchema@@ is called with `{customKeys: true}`.
+ *
+ * @returns {Function}
+ * @decorator
+ * @validation
+ * @property
+ * @parameter
+ * @schema
+ */
+export function CustomKeys(obj: Record<string, any>) {
+  return JsonEntityFn((store: JsonEntityStore) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      store.itemSchema.customKey(key, value);
+    });
+  });
+}
