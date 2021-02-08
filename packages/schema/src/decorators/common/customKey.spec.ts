bd759e5439db1807551d6db8b4a3ff7319758ea5
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-02-08 09:41:23 +0100

    fix(schema): Add @CustomKeys

--- packages/schema/src/decorators/common/customKey.spec.ts
+++ packages/schema/src/decorators/common/customKey.spec.ts
@@ -1,4 +1,4 @@
-import {getJsonSchema, getSpec, OperationPath, Path, Returns} from "@tsed/schema";
+import {CustomKeys, getJsonSchema, getSpec, OperationPath, Path, Returns} from "@tsed/schema";
 import {expect} from "chai";
 import {CustomKey} from "./customKey";
 
@@ -75,6 +75,27 @@ describe("@CustomKey", () => {
           name: "MyController"
         }
       ]
+    });
+  });
+
+  it("should declare customKey field (keys)", () => {
+    // WHEN
+    class Model {
+      @CustomKeys({range: [1, 2]})
+      num: number;
+    }
+
+    // THEN
+    const schema = getJsonSchema(Model, {customKeys: true});
+
+    expect(schema).to.deep.equal({
+      properties: {
+        num: {
+          type: "number",
+          range: [1, 2]
+        }
+      },
+      type: "object"
     });
   });
 });
