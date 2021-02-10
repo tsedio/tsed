fe54fe3edfccc632373092843abee931f8f432b9
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-02-10 09:55:45 +0100

    docs: Update example to import controller

--- docs/docs/snippets/providers/getting-started-serverloader.ts
+++ docs/docs/snippets/providers/getting-started-serverloader.ts
@@ -1,14 +1,10 @@
 import {Configuration} from "@tsed/common";
 import {CalendarsCtrl} from "./controllers/CalendarsCtrl";
-import {CalendarsService} from "./services/CalendarsService";
 
 @Configuration({
   mount: {
     "/rest": [CalendarsCtrl]
-  },
-  componentsScan: [
-    CalendarsService
-  ]
+  }
 })
 export class Server {
 }
