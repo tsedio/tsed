3a24d860f07dd55c8c41a3b296c7d1d3d1bccd23
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-04-09 07:19:52 +0200

    docs: Update vuepress-theme-tsed version

--- packages/socketio/src/services/SocketIOServer.ts
+++ packages/socketio/src/services/SocketIOServer.ts
@@ -6,8 +6,10 @@ export type SocketIOServer = Server;
 // tslint:disable-next-line: variable-name
 export const SocketIOServer = Server;
 
+export {Server};
+
 registerFactory({
-  token: SocketIOServer,
+  provide: Server,
   useFactory() {
     return new Server();
   }
