9c3e7567e7c56b63c046a04620610cd4b2c6a899
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-05-02 10:12:20 +0200

    feat(mongoose): Add Uppercase decorator

---
+++ packages/mongoose/src/decorators/uppercase.ts
@@ -0,0 +1,24 @@
+import {MongooseSchema} from "./schema";
+
+/**
+ * If truthy, Mongoose will add a custom setter that uppercases this string using JavaScript's built-in String#toUpperCase().
+ *
+ * ### Example
+ *
+ * ```typescript
+ * @Model()
+ * export class EventModel {
+ *   @Uppercase()
+ *   field: string;
+ * }
+ * ```
+ *
+ * @param {boolean} uppercase
+ * @returns {Function}
+ * @decorator
+ * @mongoose
+ * @property
+ */
+export function Uppercase(uppercase: boolean = true): PropertyDecorator {
+  return MongooseSchema({uppercase} as any);
+}
