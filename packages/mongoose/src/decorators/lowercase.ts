c433833fd938bffaa94bacf12608371605cab25d
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-05-02 10:10:26 +0200

    feat(mongoose): Add Lowercase decorator

---
+++ packages/mongoose/src/decorators/lowercase.ts
@@ -0,0 +1,24 @@
+import {MongooseSchema} from "./schema";
+
+/**
+ * If truthy, Mongoose will add a custom setter that lowercases this string using JavaScript's built-in String#toLowerCase().
+ *
+ * ### Example
+ *
+ * ```typescript
+ * @Model()
+ * export class EventModel {
+ *   @Lowercase()
+ *   field: string;
+ * }
+ * ```
+ *
+ * @param {boolean} lowercase
+ * @returns {Function}
+ * @decorator
+ * @mongoose
+ * @property
+ */
+export function Lowercase(lowercase: boolean = true): PropertyDecorator {
+  return MongooseSchema({lowercase} as any);
+}
