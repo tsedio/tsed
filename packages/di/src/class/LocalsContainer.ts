1f5d38653929ae6ee66f3dbce19b41da0df93799
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-05-14 11:44:09 +0200

    fix: Improve performance on LocalsContainer

--- packages/di/src/class/LocalsContainer.ts
+++ packages/di/src/class/LocalsContainer.ts
@@ -1,6 +1,8 @@
 import {TokenProvider} from "../interfaces/TokenProvider";
 
 export class LocalsContainer<V> extends Map<TokenProvider, V> {
+  #events: Map<string, any> = new Map();
+
   /**
    * Emit an event to all service. See service [lifecycle hooks](/docs/services.md#lifecycle-hooks).
    * @param eventName The event name to emit at all services.
@@ -8,34 +10,22 @@ export class LocalsContainer<V> extends Map<TokenProvider, V> {
    * @returns {Promise<any[]>} A list of promises.
    */
   public async emit(eventName: string, ...args: any[]) {
-    const instances: any[] = this.toArray();
-
-    for (const instance of instances) {
-      if (typeof instance === "object" && instance && eventName in instance) {
-        await instance[eventName](...args);
-      }
+    for (const handler of this.getListeners(eventName)) {
+      await handler(...args);
     }
   }
 
   public alter<T = any>(eventName: string, value: any, ...args: any[]): T {
-    const instances: any[] = this.toArray();
-
-    for (const instance of instances) {
-      if (typeof instance === "object" && instance && eventName in instance) {
-        value = instance[eventName](value, ...args);
-      }
+    for (const handler of this.getListeners(eventName)) {
+      value = handler(value, ...args);
     }
 
     return value;
   }
 
   public async alterAsync<T = any>(eventName: string, value: any, ...args: any[]): Promise<T> {
-    const instances: any[] = this.toArray();
-
-    for (const instance of instances) {
-      if (typeof instance === "object" && instance && eventName in instance) {
-        value = await instance[eventName](value, ...args);
-      }
+    for (const handler of this.getListeners(eventName)) {
+      value = handler(value, ...args);
     }
 
     return value;
@@ -48,5 +38,21 @@ export class LocalsContainer<V> extends Map<TokenProvider, V> {
   async destroy() {
     await this.emit("$onDestroy");
     this.clear();
+  }
+
+  protected getListeners(event: string): any[] {
+    if (!this.#events.has(event)) {
+      const listeners: any = [];
+
+      this.toArray().forEach((instance: any) => {
+        if (typeof instance === "object" && instance && event in instance) {
+          listeners.push(instance[event].bind(instance));
+        }
+      });
+
+      this.#events.set(event, listeners);
+    }
+
+    return this.#events.get(event);
   }
 }
