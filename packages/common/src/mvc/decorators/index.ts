040813bc5fb11690a2634a8740f4707c39f08d1a
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-05-15 18:22:44 +0200

    chore: Remove deprecated decorators ContentType & Header from @tsed/common

--- packages/common/src/mvc/decorators/index.ts
+++ packages/common/src/mvc/decorators/index.ts
@@ -10,13 +10,9 @@ export * from "./method/location";
 export * from "./method/redirect";
 export * from "./method/status";
 export * from "./method/authOptions";
-export * from "./method/contentType";
 export * from "./method/returnType";
 export * from "./method/view";
 export * from "./method/endpointFn";
-
-// Shared
-export * from "./method/header";
 
 // class
 export * from "./class/controller";
