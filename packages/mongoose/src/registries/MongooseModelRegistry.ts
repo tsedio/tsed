f029cdfbd650c65bdf7bbad9aecabdc232b8e5b6
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-05-15 18:05:29 +0200

    chore: refactoring mongoose models registry

--- packages/mongoose/src/registries/MongooseModelRegistry.ts
+++
@@ -1,9 +0,0 @@
-import {GlobalProviders, Provider, TypedProvidersRegistry} from "@tsed/di";
-
-export const PROVIDER_TYPE_MONGOOSE_MODEL = "mongooseModel";
-// tslint:disable-next-line: variable-name
-export const MongooseModelRegistry: TypedProvidersRegistry = GlobalProviders.createRegistry(PROVIDER_TYPE_MONGOOSE_MODEL, Provider, {
-  injectable: true
-});
-
-export const registerModel = GlobalProviders.createRegisterFn(PROVIDER_TYPE_MONGOOSE_MODEL);
