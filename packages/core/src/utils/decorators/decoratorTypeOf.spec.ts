858d3ecd286501b363005fb7d666c092dde19c34
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-05-15 18:35:42 +0200

    chore: Remove deprecated getDecoratorType

--- packages/core/src/utils/decorators/decoratorTypeOf.spec.ts
+++ packages/core/src/utils/decorators/decoratorTypeOf.spec.ts
@@ -1,83 +1,9 @@
 import {expect} from "chai";
-import {decoratorTypeOf, getDecoratorType} from "./decoratorTypeOf";
+import {decoratorTypeOf} from "./decoratorTypeOf";
 
 class Test {}
 
 describe("decoratorTypeOf()", () => {
-  describe("when short type", () => {
-    it("should return class", () => {
-      expect(getDecoratorType([Test])).to.equal("class");
-    });
-
-    it("should return property (static)", () => {
-      expect(getDecoratorType([Test, "props"])).to.equal("property");
-    });
-
-    it("should return property (instance)", () => {
-      expect(getDecoratorType([Test.prototype, "props"])).to.equal("property");
-    });
-
-    it("should return method (instance, getter)", () => {
-      expect(
-        decoratorTypeOf([
-          Test.prototype,
-          "props",
-          {
-            get: () => {}
-          }
-        ])
-      ).to.equal("property");
-    });
-
-    it("should return method (instance, setter)", () => {
-      expect(
-        decoratorTypeOf([
-          Test.prototype,
-          "props",
-          {
-            set: () => {}
-          }
-        ])
-      ).to.equal("property");
-    });
-
-    it("should return method (static)", () => {
-      expect(
-        decoratorTypeOf([
-          Test,
-          "props",
-          {
-            value: () => {}
-          }
-        ])
-      ).to.equal("method.static");
-    });
-
-    it("should return method (instance)", () => {
-      expect(
-        decoratorTypeOf([
-          Test.prototype,
-          "props",
-          {
-            value: () => {}
-          }
-        ])
-      ).to.equal("method");
-    });
-
-    it("should return params (static)", () => {
-      expect(decoratorTypeOf([Test, "props", 0])).to.equal("parameter.static");
-    });
-
-    it("should return params (instance)", () => {
-      expect(decoratorTypeOf([Test.prototype, "props", 0])).to.equal("parameter");
-    });
-
-    it("should return params (constructor)", () => {
-      expect(decoratorTypeOf([Test.prototype, undefined, 0])).to.equal("parameter.constructor");
-    });
-  });
-
   it("should return class", () => {
     expect(decoratorTypeOf([Test])).to.equal("class");
   });
--- packages/core/src/utils/decorators/decoratorTypeOf.ts
+++ packages/core/src/utils/decorators/decoratorTypeOf.ts
@@ -1,15 +1,6 @@
 import {DecoratorTypes} from "../../domain/DecoratorTypes";
 import {classOf} from "../objects/classOf";
 
-/**
- * @deprecated Since 2020-11-28. Use decoratorTypeOf function.
- */
-export function getDecoratorType(args: any[], longType = false): DecoratorTypes {
-  const type = decoratorTypeOf(args);
-
-  return longType ? type : ((type as string).split(".")[0] as any);
-}
-
 export function decoratorTypeOf(args: any[]): DecoratorTypes {
   const [target, propertyKey, descriptor] = args;
 
