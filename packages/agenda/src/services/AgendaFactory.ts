859bc4038cb9858f7cd3c0e0f7d95570b017294f
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-05-26 07:48:50 +0200

    fix: Add Alias AgendaService

--- packages/agenda/src/services/AgendaFactory.ts
+++ packages/agenda/src/services/AgendaFactory.ts
@@ -1,6 +1,9 @@
 import {Configuration, registerProvider} from "@tsed/di";
 import {Agenda, AgendaConfig} from "agenda";
 
+// create Alias to avoid confusing import between Agenda from "agenda" and Agenda decorator
+export const AgendaService: Agenda = Agenda;
+
 registerProvider({
   provide: Agenda,
   deps: [Configuration],
