83fbb6a1912d4f822ceea5dd3315b7d351503e74
Oliver Christen <christen@app-logik.de>
2021-05-27 22:37:43 +0200

    fix(agenda): Fix integration tests

--- packages/agenda/test/integration.spec.ts
+++ packages/agenda/test/integration.spec.ts
@@ -1,11 +1,10 @@
+import {expect} from "chai";
 import {PlatformTest} from "@tsed/common";
 import {TestMongooseContext} from "@tsed/testing-mongoose";
-import {Agenda} from "agenda";
-import {expect} from "chai";
-import {Agenda as AgendaDecorator, Every, Define} from "../src/";
+import {Agenda, AgendaService, Every, Define} from "../src/";
 import {Server} from "./helpers/Server";
 
-@AgendaDecorator({namespace: "test-nsp"})
+@Agenda({namespace: "test-nsp"})
 class Test {
   @Every("60 seconds")
   test() {
@@ -18,7 +17,7 @@ class Test {
   }
 }
 
-@AgendaDecorator()
+@Agenda()
 class TestTwo {
   @Every("* * * * *")
   test3() {
@@ -27,39 +26,63 @@ class TestTwo {
 }
 
 describe("Agenda integration", () => {
-  beforeEach(async () => {
-    await TestMongooseContext.install();
-    const {url} = await TestMongooseContext.getMongooseOptions();
-    const bstrp = PlatformTest.bootstrap(Server, {
-      agenda: {
-        db: {
-          address: url
+  describe("enabled", () => {
+    beforeEach(async () => {
+      await TestMongooseContext.install();
+      const {url} = await TestMongooseContext.getMongooseOptions();
+      const bstrp = PlatformTest.bootstrap(Server, {
+        agenda: {
+          enabled: true,
+          db: {
+            address: url
+          }
         }
-      }
+      });
+  
+      await bstrp();
+    });
+    afterEach(TestMongooseContext.reset);
+  
+    it("should have job definitions", async () => {
+      const agenda = PlatformTest.injector.get<AgendaService>(AgendaService)!;
+      expect(agenda._definitions).to.contain.keys("test-nsp.test", "test-nsp.customName", "test3");
+    });
+  
+    it("should schedule cron-like jobs", async () => {
+      const agenda = PlatformTest.injector.get(AgendaService)!;
+      const jobs = await agenda.jobs();
+  
+      expect(jobs).to.have.length(4);
+      expect(jobs[0].attrs.name).to.be.deep.eq("test-nsp.test");
+      expect(jobs[0].attrs.repeatInterval).to.be.eq("60 seconds");
+  
+      expect(jobs[1].attrs.name).to.be.deep.eq("test3");
+      expect(jobs[1].attrs.repeatInterval).to.be.eq("* * * * *");
+    });
+  })
+  
+  describe("disabled", () => {
+
+    beforeEach(async () => {
+      await TestMongooseContext.install();
+      const {url} = await TestMongooseContext.getMongooseOptions();
+      const bstrp = PlatformTest.bootstrap(Server, {
+        agenda: {
+          db: {
+            address: url
+          }
+        }
+      });
+  
+      await bstrp();
+    });
+    afterEach(TestMongooseContext.reset);
+
+    it("should not have job definitions", async () => {
+      const agenda = PlatformTest.injector.get(AgendaService)!;
+      expect(agenda._definitions).to.be.empty
     });
 
-    await bstrp();
   });
-  afterEach(TestMongooseContext.reset);
-
-  it("should have job definitions", async () => {
-    const agenda = PlatformTest.injector.get<Agenda>(Agenda)!;
-    expect(agenda._definitions).to.contain.keys("test-nsp.test", "test-nsp.customName", "test3");
-  });
-
-  it("should schedule cron-like jobs", async () => {
-    const agenda = PlatformTest.injector.get<Agenda>(Agenda)!;
-    await agenda.start();
-    const jobs = await agenda.jobs();
-    await agenda.stop();
-
-    expect(jobs).to.have.length(4); 
-    expect(jobs[0].attrs.name).to.be.deep.eq("test-nsp.test");
-    expect(jobs[0].attrs.repeatInterval).to.be.eq("60 seconds");
-
-    expect(jobs[1].attrs.name).to.be.deep.eq("test3");
-    expect(jobs[1].attrs.repeatInterval).to.be.eq("* * * * *");
-  });
-
 
 });
