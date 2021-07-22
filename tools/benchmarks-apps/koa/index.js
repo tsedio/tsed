8dcd4ad047008255db506a632fb9556acbc2b8e7
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-07-22 03:25:14 +0200

    wip

--- tools/benchmarks-apps/koa/index.js
+++ tools/benchmarks-apps/koa/index.js
@@ -1,11 +1,29 @@
 const Koa = require("koa");
+const Router = require("@koa/router");
 const {printMemory} = require("../printMemory");
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-  printMemory("nest");
+router.get("/", async (ctx) => {
+  printMemory("koa");
   ctx.body = "Hello world!";
 });
 
+router.get("/rest/path", async (ctx) => {
+  printMemory("koa");
+  ctx.body = "Hello world 2!";
+});
+
+app
+  .use(async (ctx, next) => {
+    await next();
+    const status = ctx.status || 404;
+
+    if (status === 404) {
+      ctx.body = "NOT FOUND ?";
+    }
+  })
+  .use(router.routes())
+  .use(router.allowedMethods());
 app.listen(process.env.PORT || 3000);
-printMemory("nest");
+printMemory("koa");
