98ab1bbc6e4205822e5702dfa9337d2ec3475d34
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-07-26 20:49:59 +0200

    feat(core): Fix deepMerge function when key contains a dot
    Closes: #1471

--- packages/schema/src/utils/mergeSpec.spec.ts
+++ packages/schema/src/utils/mergeSpec.spec.ts
@@ -414,4 +414,120 @@ describe("mergeSpec", () => {
       });
     });
   });
+  describe("merge two specs", () => {
+    it("should merge the spec", () => {
+      const spec1 = {
+        paths: {
+          "/rest/api/1.0/test": {
+            get: {
+              operationId: "helloWorldControllerGet",
+              responses: {"200": {description: "Success"}},
+              parameters: [],
+              tags: ["HelloWorldController"]
+            }
+          },
+          "/rest/api/1.0/test/{id}": {
+            get: {
+              operationId: "helloWorldControllerGetById",
+              responses: {"200": {description: "Success"}},
+              parameters: [{in: "path", name: "id", required: true, schema: {type: "string"}}],
+              tags: ["HelloWorldController"]
+            }
+          },
+          "/rest/api/2.0/test": {
+            get: {
+              operationId: "helloWorldController2Get",
+              responses: {"200": {description: "Success"}},
+              parameters: [],
+              tags: ["HelloWorldController2"]
+            }
+          },
+          "/rest/api/2.0/test/{id}": {
+            get: {
+              operationId: "helloWorldController2GetById",
+              responses: {"200": {description: "Success"}},
+              parameters: [{in: "path", name: "id", required: true, schema: {type: "string"}}],
+              tags: ["HelloWorldController2"]
+            }
+          }
+        }
+      };
+
+      const spec2 = {
+        paths: {
+          "/rest/api/1.0/test": {
+            get: {
+              operationId: "helloWorldControllerGet",
+              responses: {"200": {description: "Success"}},
+              parameters: [],
+              tags: ["HelloWorldController"]
+            }
+          },
+          "/rest/api/1.0/test/{id}": {
+            get: {
+              operationId: "helloWorldControllerGetById",
+              responses: {"200": {description: "Success"}},
+              parameters: [{in: "path", name: "id", required: true, schema: {type: "string"}}],
+              tags: ["HelloWorldController"]
+            }
+          },
+          "/rest/api/2.0/test": {
+            get: {
+              operationId: "helloWorldController2Get",
+              responses: {"200": {description: "Success"}},
+              parameters: [],
+              tags: ["HelloWorldController2"]
+            }
+          },
+          "/rest/api/2.0/test/{id}": {
+            get: {
+              operationId: "helloWorldController2GetById",
+              responses: {"200": {description: "Success"}},
+              parameters: [{in: "path", name: "id", required: true, schema: {type: "string"}}],
+              tags: ["HelloWorldController2"]
+            }
+          }
+        }
+      };
+
+      const spec = mergeSpec(spec1, spec2);
+      console.log(JSON.stringify(spec));
+      expect(spec).to.deep.eq({
+        paths: {
+          "/rest/api/1.0/test": {
+            get: {
+              operationId: "helloWorldControllerGet",
+              responses: {"200": {description: "Success"}},
+              parameters: [],
+              tags: ["HelloWorldController"]
+            }
+          },
+          "/rest/api/1.0/test/{id}": {
+            get: {
+              operationId: "helloWorldControllerGetById",
+              responses: {"200": {description: "Success"}},
+              parameters: [{in: "path", name: "id", required: true, schema: {type: "string"}}],
+              tags: ["HelloWorldController"]
+            }
+          },
+          "/rest/api/2.0/test": {
+            get: {
+              operationId: "helloWorldController2Get",
+              responses: {"200": {description: "Success"}},
+              parameters: [],
+              tags: ["HelloWorldController2"]
+            }
+          },
+          "/rest/api/2.0/test/{id}": {
+            get: {
+              operationId: "helloWorldController2GetById",
+              responses: {"200": {description: "Success"}},
+              parameters: [{in: "path", name: "id", required: true, schema: {type: "string"}}],
+              tags: ["HelloWorldController2"]
+            }
+          }
+        }
+      });
+    });
+  });
 });
