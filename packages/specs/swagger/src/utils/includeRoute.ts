7a2e640ec7dc8de84973d20cd16b539ae9cb036e
Tero Kujala <tero.kujala@cuuma.com>
2021-09-15 15:29:27 +0300

    fix(swagger): Fix how swagger generation handles pathPatterns, hidden and doc
    Hidden decorator on controller will now always hide controller from swagger. Controller will now included to swagger if either doc from swagger options matches to Docs decorator on controller or pathPatterns matches to controller route. Also fixes issue when not matching doc is needed to get negative pathPatterns option to work.

---
+++ packages/specs/swagger/src/utils/includeRoute.ts
@@ -0,0 +1,21 @@
+import {ControllerProvider} from "@tsed/common";
+import {matchPath} from ".";
+import {SwaggerSettings} from "..";
+
+export function includeRoute(route: string, provider: ControllerProvider, conf: SwaggerSettings): boolean {
+  const hidden = provider.store.get("hidden");
+  const docs = provider.store.get("docs") || [];
+  const {doc, pathPatterns} = conf;
+  const inDoc = doc && docs.indexOf(doc) > -1;
+  const pathPatternMatch = pathPatterns && matchPath(route, pathPatterns);
+
+  if (hidden) {
+    return false;
+  }
+
+  if (!doc && !pathPatterns) {
+    return true;
+  }
+
+  return !!(inDoc || pathPatternMatch);
+}
