d741ea5c666606db2d08c90ece38b5e77246833c
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-09-26 11:31:29 +0200

    chore(platform-middlewares): Remove IMiddleware interface.
    BREAKING CHANGE: Remove IMiddleware. Use MiddlewareMethods instead.

--- packages/platform/platform-middlewares/src/domain/MiddlewareMethods.ts
+++ packages/platform/platform-middlewares/src/domain/MiddlewareMethods.ts
@@ -1,8 +1,3 @@
 export interface MiddlewareMethods {
   use(...args: any[]): void | any | Promise<any>;
 }
-
-/**
- * @deprecated Since v6. Use MiddlewareMethods instead
- */
-export interface IMiddleware extends MiddlewareMethods {}
