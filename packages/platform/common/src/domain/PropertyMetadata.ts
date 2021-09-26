1d97ae21bcb2097802175abeb360147b4fbdc5c4
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-09-26 11:49:05 +0200

    chore(common): Remove PropertyMetadata.getProperties
    BREAKING CHANGE: Use getProperties from @tsed/schema instead

--- packages/platform/common/src/domain/PropertyMetadata.ts
+++ packages/platform/common/src/domain/PropertyMetadata.ts
@@ -1,16 +1,9 @@
 import {DecoratorTypes, prototypeOf, Type} from "@tsed/core";
-import {getProperties, JsonEntityComponent, JsonEntityStore} from "@tsed/schema";
+import {JsonEntityComponent, JsonEntityStore} from "@tsed/schema";
 
 @JsonEntityComponent(DecoratorTypes.PROP)
 export class PropertyMetadata extends JsonEntityStore {
   static get(target: Type<any>, propertyKey: string | symbol) {
     return JsonEntityStore.from<PropertyMetadata>(prototypeOf(target), propertyKey);
-  }
-
-  /**
-   * @deprecated Since 2020-11-11. Use getProperties from @tsed/schema
-   */
-  static getProperties(target: Type<any>, options: Partial<{withIgnoredProps: boolean}> = {}) {
-    return getProperties(target, options);
   }
 }
