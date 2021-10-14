bc93fd2ca7824d8abafd0aff524975e99524ebe5
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-10-14 07:45:47 +0200

    fix(typeorm): Add AbstractRepository to whitelisted injectable services
    Closes: #1577

--- packages/orm/typeorm/src/TypeORMModule.ts
+++ packages/orm/typeorm/src/TypeORMModule.ts
@@ -1,7 +1,9 @@
-import {ancestorsOf, isClass} from "@tsed/core";
+import {ancestorsOf, isClass, Type} from "@tsed/core";
 import {Configuration, InjectorService, OnDestroy, registerProvider} from "@tsed/di";
-import {ConnectionOptions, ContainedType, getCustomRepository, Repository, useContainer} from "typeorm";
+import {AbstractRepository, ConnectionOptions, ContainedType, getCustomRepository, Repository, useContainer} from "typeorm";
 import {TypeORMService} from "./services/TypeORMService";
+
+const WHITELIST = [Repository, AbstractRepository];
 
 export class TypeORMModule implements OnDestroy {
   private settings: ConnectionOptions[];
@@ -21,6 +23,17 @@ export class TypeORMModule implements OnDestroy {
   }
 }
 
+function isRepository(type: Type<any>) {
+  const ancestors = ancestorsOf(type);
+
+  return (
+    isClass(type) &&
+    ancestors.find((ancestor) => {
+      return WHITELIST.includes(ancestor);
+    })
+  );
+}
+
 registerProvider({
   provide: TypeORMModule,
   deps: [Configuration, TypeORMService, InjectorService],
@@ -28,7 +41,7 @@ registerProvider({
     {
       deps: [TypeORMModule],
       get(type, options: any) {
-        if (isClass(type) && ancestorsOf(type).includes(Repository)) {
+        if (isRepository(type)) {
           try {
             return getCustomRepository(type, options.connection || "default");
           } catch (er) {
