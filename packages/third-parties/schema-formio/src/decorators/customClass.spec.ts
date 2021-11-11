57ea49019299954de650bdff9293112da97184bc
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-11-11 10:49:13 +0100

    feat(schema-formio): Add `@CustomClass` decorator

---
+++ packages/third-parties/schema-formio/src/decorators/customClass.spec.ts
@@ -0,0 +1,35 @@
+import {getFormioSchema} from "@tsed/schema-formio";
+import {CustomClass} from "./customClass";
+
+describe("@CustomClass", () => {
+  it("should add a custom css class on field", async () => {
+    class Model {
+      @CustomClass("css-class")
+      test: string;
+    }
+
+    expect(await getFormioSchema(Model)).toEqual({
+      access: [],
+      components: [
+        {
+          disabled: false,
+          input: true,
+          key: "test",
+          label: "Test",
+          customClass: "css-class",
+          type: "textfield",
+          validate: {
+            required: false
+          }
+        }
+      ],
+      display: "form",
+      machineName: "model",
+      name: "model",
+      submissionAccess: [],
+      tags: [],
+      title: "Model",
+      type: "form"
+    });
+  });
+});
