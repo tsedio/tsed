fba303e93fc5143876b16da3c15aecbc97f938d8
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-11-11 10:48:59 +0100

    feat(schema-formio): Add `@Mask` and `@Masks` decorators

---
+++ packages/third-parties/schema-formio/src/decorators/mask.spec.ts
@@ -0,0 +1,104 @@
+import {getFormioSchema} from "@tsed/schema-formio";
+import {Mask, Masks} from "./mask";
+
+describe("@Mask", () => {
+  it("should add a mask on field (1)", async () => {
+    class Model {
+      @Mask("(99999)", "____")
+      test: string;
+    }
+
+    expect(await getFormioSchema(Model)).toEqual({
+      access: [],
+      components: [
+        {
+          disabled: false,
+          input: true,
+          key: "test",
+          label: "Test",
+          type: "textfield",
+          inputMask: "(99999)",
+          inputMaskPlaceholderChar: "____",
+          validate: {
+            required: false
+          }
+        }
+      ],
+      display: "form",
+      machineName: "model",
+      name: "model",
+      submissionAccess: [],
+      tags: [],
+      title: "Model",
+      type: "form"
+    });
+  });
+  it("should add a mask on field (2)", async () => {
+    class Model {
+      @Mask("(99999)")
+      test: string;
+    }
+
+    expect(await getFormioSchema(Model)).toEqual({
+      access: [],
+      components: [
+        {
+          disabled: false,
+          input: true,
+          key: "test",
+          label: "Test",
+          type: "textfield",
+          inputMask: "(99999)",
+          validate: {
+            required: false
+          }
+        }
+      ],
+      display: "form",
+      machineName: "model",
+      name: "model",
+      submissionAccess: [],
+      tags: [],
+      title: "Model",
+      type: "form"
+    });
+  });
+});
+
+describe("@Masks", () => {
+  it("should add masks on field", async () => {
+    class Model {
+      @Masks({label: "label", mask: "mask"})
+      test: string;
+    }
+
+    expect(await getFormioSchema(Model)).toEqual({
+      access: [],
+      components: [
+        {
+          disabled: false,
+          input: true,
+          key: "test",
+          label: "Test",
+          inputMasks: [
+            {
+              label: "label",
+              mask: "mask"
+            }
+          ],
+          type: "textfield",
+          validate: {
+            required: false
+          }
+        }
+      ],
+      display: "form",
+      machineName: "model",
+      name: "model",
+      submissionAccess: [],
+      tags: [],
+      title: "Model",
+      type: "form"
+    });
+  });
+});
