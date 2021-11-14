b081a9dcb8df12bd910c22d94b33a4edb7b41b96
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-11-14 13:25:18 +0100

    feat(core): Add importPackage function

---
+++ packages/core/src/utils/imports/importPackage.spec.ts
@@ -0,0 +1,29 @@
+import {importPackage} from "./importPackage";
+import {expect} from "chai";
+import {catchAsyncError} from "@tsed/core";
+
+describe("importPackage", () => {
+  it("should load package util", async () => {
+    const util = await importPackage("util");
+
+    expect(!!util).to.be.true;
+  });
+
+  it("should load package from loaderFn", async () => {
+    const util = await importPackage("util", () => import("util"));
+
+    expect(!!util).to.be.true;
+  });
+
+  it("should throw error", async () => {
+    const error = await catchAsyncError(() => importPackage("@tsed/nowhere"));
+
+    expect(error?.message).to.eq('The "@tsed/nowhere" package is missing.');
+  });
+
+  it("should ignore missing package", async () => {
+    const error = await importPackage("@tsed/nowhere", undefined, true);
+
+    expect(error).to.deep.eq({});
+  });
+});
