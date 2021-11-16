e142e1e4d645664db8de432f5fd761328171fb40
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-11-16 15:55:53 +0100

    feat(schema-formio): Add @OpenWhenEmpty decorator to open editgrid by default

--- packages/third-parties/schema-formio/test/editgrid.integration.spec.ts
+++ packages/third-parties/schema-formio/test/editgrid.integration.spec.ts
@@ -1,5 +1,6 @@
 import {CollectionOf, Property} from "@tsed/schema";
-import {Currency, getFormioSchema} from "../src";
+import {Currency, getFormioSchema, Component} from "../src";
+import {OpenWhenEmpty} from "../src/decorators/openWhenEmpty";
 
 
 describe('EditGrid integration', () => {
@@ -9,7 +10,6 @@ describe('EditGrid integration', () => {
       id: string;
     }
     class Model {
-      @Currency()
       @CollectionOf(Nested)
       test: Nested[];
     }
@@ -35,6 +35,102 @@ describe('EditGrid integration', () => {
           label: "Test",
           rowDrafts: false,
           type: "editgrid",
+          validate: {
+            required: false
+          }
+        }
+      ],
+      display: "form",
+      machineName: "model",
+      name: "model",
+      title: "Model",
+      type: "form",
+      submissionAccess: [],
+      access: [],
+      tags: [],
+    });
+  });
+  it("should generate form with openWhenEmpty", async () => {
+    class Nested {
+      @Property()
+      id: string;
+    }
+    class Model {
+      @CollectionOf(Nested)
+      @OpenWhenEmpty()
+      test: Nested[];
+    }
+
+    expect(await getFormioSchema(Model)).toEqual({
+      components: [
+        {
+          components: [
+            {
+              disabled: false,
+              input: true,
+              key: "id",
+              label: "Id",
+              type: "textfield",
+              validate: {
+                required: false
+              }
+            }
+          ],
+          disabled: false,
+          input: true,
+          key: "test",
+          label: "Test",
+          rowDrafts: false,
+          type: "editgrid",
+          openWhenEmpty: true,
+          validate: {
+            required: false
+          }
+        }
+      ],
+      display: "form",
+      machineName: "model",
+      name: "model",
+      title: "Model",
+      type: "form",
+      submissionAccess: [],
+      access: [],
+      tags: [],
+    });
+  });
+  it("should generate form with openWhenEmpty = false", async () => {
+    class Nested {
+      @Property()
+      id: string;
+    }
+    class Model {
+      @CollectionOf(Nested)
+      @OpenWhenEmpty(false)
+      test: Nested[];
+    }
+
+    expect(await getFormioSchema(Model)).toEqual({
+      components: [
+        {
+          components: [
+            {
+              disabled: false,
+              input: true,
+              key: "id",
+              label: "Id",
+              type: "textfield",
+              validate: {
+                required: false
+              }
+            }
+          ],
+          disabled: false,
+          input: true,
+          key: "test",
+          label: "Test",
+          rowDrafts: false,
+          type: "editgrid",
+          openWhenEmpty: false,
           validate: {
             required: false
           }
