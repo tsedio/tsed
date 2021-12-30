b3f5a5c34af89e51ad17560a5842a95de85badd9
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2021-12-30 14:22:44 +0300

    feat(di): allow using a custom token with the `@Injectable` decorator (#1698)
    closes #1695

---
+++ docs/docs/snippets/providers/override-injection-token.ts
@@ -0,0 +1,25 @@
+import {Inject, Injectable} from '@tsed/di';
+
+export interface RetryPolicy {
+  retry<T extends (...args: unknown[]) => unknown>(
+    task: T
+  ): Promise<ReturnType<T>>;
+}
+
+export const RetryPolicy: unique symbol = Symbol('RetryPolicy');
+
+@Injectable({ provide: RetryPolicy })
+export class TokenBucket implements RetryPolicy {
+  public retry<T extends (...args: unknown[]) => unknown>(
+    task: T
+  ): Promise<ReturnType<T>> {
+    // ...
+  }
+}
+
+@Injectable()
+export class MyService {
+  constructor(@Inject(RetryPolicy) private readonly retryPolicy: RetryPolicy) { // an instance of `TokenBucket`
+
+  }
+}
