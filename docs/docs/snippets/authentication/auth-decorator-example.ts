63a0232ea21385d185da2deb25a1579ad5da4d22
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-12-31 11:26:14 +0100

    docs: Update documentation about auth

--- docs/docs/snippets/authentication/auth-decorator-example.ts
+++ docs/docs/snippets/authentication/auth-decorator-example.ts
@@ -1,14 +1,14 @@
-import {IAuthOptions, UseAuth} from "@tsed/platform-middlewares";
+import {UseAuth} from "@tsed/platform-middlewares";
 import {useDecorators} from "@tsed/core";
 import {Security, Returns} from "@tsed/schema";
 import {CustomAuthMiddleware} from "../guards/CustomAuthMiddleware";
 
-export interface ICustomAuthOptions extends IAuthOptions {
+export interface AuthOpts extends Record<string, unknown> {
   role?: string;
   scopes?: string[];
 }
 
-export function CustomAuth(options: ICustomAuthOptions = {}): Function {
+export function CustomAuth(options: AuthOpts = {}): Function {
   return useDecorators(
     UseAuth(CustomAuthMiddleware, options),
     Security("oauth", ...(options.scopes || [])),
