68337c45b153bf26910d204187d5647f99b65fd5
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-01-02 10:01:42 +0100

    docs: Update documentation

--- docs/docs/snippets/pipes/async-transformer-pipe-with-options.ts
+++ docs/docs/snippets/pipes/async-transformer-pipe-with-options.ts
@@ -1,6 +1,6 @@
-import {ParamMetadata, PipeMethods} from "@tsed/platform-params";
 import {Inject, Injectable} from "@tsed/di";
 import {NotFound} from "@tsed/exceptions";
+import {JsonParameterStore, PipeMethods} from "@tsed/schema";
 import {IUsePersonParamOptions} from "../decorators/UsePersonParam";
 import {PersonModel} from "../models/PersonModel";
 import {PersonsService} from "../models/PersonsService";
@@ -10,7 +10,7 @@ export class PersonPipe implements PipeMethods<string, Promise<PersonModel>> {
   @Inject()
   personsService: PersonsService;
 
-  async transform(id: string, metadata: ParamMetadata): Promise<PersonModel> {
+  async transform(id: string, metadata: JsonParameterStore): Promise<PersonModel> {
     const person = await this.personsService.findOne(id);
     const options = metadata.store.get<IUsePersonParamOptions>(PersonPipe);
 
--- docs/docs/snippets/validation/validator-pipe.ts
+++ docs/docs/snippets/validation/validator-pipe.ts
@@ -1,11 +1,12 @@
-import {PipeMethods, ParamMetadata, ValidationError, ValidationPipe} from "@tsed/platform-params";
+import {ValidationError, ValidationPipe} from "@tsed/platform-params";
+import {JsonParameterStore, PipeMethods} from "@tsed/schema";
 import {OverrideProvider} from "@tsed/di";
 import {getJsonSchema} from "@tsed/schema";
 import {validate} from "./validate";
 
 @OverrideProvider(ValidationPipe)
 export class CustomValidationPipe extends ValidationPipe implements PipeMethods {
-  public transform(obj: any, metadata: ParamMetadata): void {
+  public transform(obj: any, metadata: JsonParameterStore): void {
     // JSON service contain tool to build the Schema definition of a model.
     const schema = getJsonSchema(metadata.type);
 
