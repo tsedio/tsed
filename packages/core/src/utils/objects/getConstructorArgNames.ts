52a1a2bd763f889e2ea926a639fb6aaf72fd7330
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-02-19 18:15:41 +0100

    fix: add missing module prop in package.json

--- packages/core/src/utils/objects/getConstructorArgNames.ts
+++ packages/core/src/utils/objects/getConstructorArgNames.ts
@@ -1,5 +1,10 @@
 import {toStringConstructor} from "./toStringConstructor";
 
 export function getConstructorArgNames(target: any) {
-  return toStringConstructor(target).replace("constructor(", "").replace(")", "").split(", ").filter(Boolean);
+  return toStringConstructor(target)
+    .split("constructor(")[1]
+    .split(")")[0]
+    .split(", ")
+    .filter(Boolean)
+    .map((s) => s.trim());
 }
