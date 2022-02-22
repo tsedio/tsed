49baa8d2442bd0849aceafab78dfff7163ca9670
Nino Bergen <n-bergen@hotmail.de>
2022-02-22 22:32:12 +0100

    test(keycloak): add jest config in keyclok package

---
+++ packages/third-parties/keycloak/jest.config.js
@@ -0,0 +1,14 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  ...require("@tsed/jest-config")(__dirname, "keycloak"),
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 99.19,
+      functions: 100,
+      lines: 100
+    }
+  }
+};
