4677c2aeb5381fe89305b8363a1637ac1df5adcb
Nino Bergen <n-bergen@hotmail.de>
2022-02-22 09:03:30 +0100

    refactor(keycloak): add new exports to index

--- packages/third-parties/keycloak/src/index.ts
+++ packages/third-parties/keycloak/src/index.ts
@@ -1,4 +1,7 @@
+export * from "./KeycloakModule";
 export * from "./services/KeycloakFactory";
 export * from "./interfaces/KeycloakAuthOptions";
+export * from "./interfaces/KeycloakAuthOptions";
+export * from "./interfaces/KeycloakMiddlewareOptions";
 export * from "./middlewares/KeycloakMiddleware";
 export * from "./decorators/UseKeycloakAuthDecorator";
