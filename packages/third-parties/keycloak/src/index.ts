57dbc9d424bb716072a3082215347729b7104b74
Nino Bergen <n-bergen@hotmail.de>
2022-02-22 08:45:09 +0100

    refactor(keycloak): remove declare global from index

--- packages/third-parties/keycloak/src/index.ts
+++ packages/third-parties/keycloak/src/index.ts
@@ -1,22 +1,3 @@
-import {KeycloakConfig, KeycloakOptions} from "keycloak-connect";
-
-declare global {
-  namespace TsED {
-    interface Configuration {
-      keycloak?: {
-        /**
-         * Keycloak options for web session store, scopes and cookies. Default empty object.
-         */
-        options?: KeycloakOptions;
-        /**
-         * Keycloak configuration or the path to the configuration file.
-         */
-        config: KeycloakConfig | string;
-      };
-    }
-  }
-}
-
 export * from "./services/KeycloakFactory";
 export * from "./interfaces/KeycloakAuthOptions";
 export * from "./middlewares/KeycloakMiddleware";
