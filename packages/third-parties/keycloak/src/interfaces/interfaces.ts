071fb120c8dcd8b4ba93a3be98fb613fffeeb630
Nino Bergen <n-bergen@hotmail.de>
2022-02-22 08:47:25 +0100

    refactor(keycloak): move declare global to interfaces

---
+++ packages/third-parties/keycloak/src/interfaces/interfaces.ts
@@ -0,0 +1,25 @@
+import {KeycloakConfig, KeycloakOptions} from "keycloak-connect";
+import {KeycloakMiddlewareOptions} from "./KeycloakMiddlewareOptions";
+
+declare global {
+  namespace TsED {
+    interface Configuration {
+      keycloak?: {
+        /**
+         * Keycloak options for web session store, scopes and cookies. Default empty object.
+         */
+        options?: KeycloakOptions;
+        /**
+         * Keycloak configuration or the path to the configuration file.
+         */
+        config: KeycloakConfig | string;
+        /**
+         * Provide additional options that are passed to middleware:
+         * `logout` URL for logging a user out. Defaults to `/logout`.
+         * `admin` Root URL for Keycloak admin callbacks.  Defaults to `/`.
+         */
+        middlewareOptions: KeycloakMiddlewareOptions;
+      };
+    }
+  }
+}
