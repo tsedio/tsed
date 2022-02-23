de8bd354632965b9be76f71ec2bc5d417cbdce3c
Nino Bergen <n-bergen@hotmail.de>
2022-02-23 17:02:03 +0100

    refactor(keycloak): update use keycloak auth decorator

--- packages/third-parties/keycloak/src/decorators/UseKeycloakAuthDecorator.ts
+++ packages/third-parties/keycloak/src/decorators/UseKeycloakAuthDecorator.ts
@@ -1,9 +1,9 @@
 import {UseAuth} from "@tsed/common";
-import {useDecorators} from "@tsed/core";
+import {Type, useDecorators} from "@tsed/core";
 import {Returns, Security} from "@tsed/schema";
 import {KeycloakAuthOptions} from "../interfaces/KeycloakAuthOptions";
 import {KeycloakMiddleware} from "../middlewares/KeycloakMiddleware";
 
-export function UseKeycloakAuth(options?: KeycloakAuthOptions): MethodDecorator {
-  return useDecorators(UseAuth(KeycloakMiddleware, options), Security("oauth2"), Returns(403));
+export function UseKeycloakAuth(options?: KeycloakAuthOptions, middleware: Type<any> = KeycloakMiddleware): MethodDecorator {
+  return useDecorators(UseAuth(middleware, options), Security("oauth2"), Returns(403));
 }
