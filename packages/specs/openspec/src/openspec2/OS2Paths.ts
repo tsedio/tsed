6c5de0a00b70591be03ef7d8453ce9c90fcee8cc
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-03-14 18:16:20 +0100

    fix(openspec): remove depreciation annotation

--- packages/specs/openspec/src/openspec2/OS2Paths.ts
+++ packages/specs/openspec/src/openspec2/OS2Paths.ts
@@ -3,9 +3,6 @@ import {OpenSpecRef} from "../common/OpenSpecRef";
 import {OS2Operation} from "./OS2Operation";
 import {OS2Parameter} from "./OS2Parameter";
 
-/**
- * @deprecated
- */
 export interface OS2Paths extends OpenSpecPath<OS2Operation> {
   parameters?: (OS2Parameter | OpenSpecRef)[];
 }
--- packages/specs/openspec/src/openspec2/OS2Security.ts
+++ packages/specs/openspec/src/openspec2/OS2Security.ts
@@ -1,8 +1,5 @@
 import {OpenSpecHash} from "../common/OpenSpecHash";
 
-/**
- * @deprecated
- */
 export interface OS2SecurityBase {
   /**
    * The type of the security scheme
@@ -14,25 +11,16 @@ export interface OS2SecurityBase {
   description?: string;
 }
 
-/**
- * @deprecated
- */
 export interface OS2SecurityBasic extends OS2SecurityBase {
   type: "basic";
 }
 
-/**
- * @deprecated
- */
 export interface OS2SecurityApiKey extends OS2SecurityBase {
   type: "apiKey";
   name: string;
   in: "query" | "header";
 }
 
-/**
- * @deprecated
- */
 export interface OS2SecurityOAuth extends OS2SecurityBase {
   type: "oauth2";
   flow: "accessCode" | "application" | "implicit" | "password";
@@ -42,36 +30,24 @@ export interface OS2SecurityOAuth extends OS2SecurityBase {
   scopes: OpenSpecHash<string>;
 }
 
-/**
- * @deprecated
- */
 export interface OS2FlowImplicit extends OS2SecurityOAuth {
   type: "oauth2";
   flow: "implicit";
   authorizationUrl: string;
 }
 
-/**
- * @deprecated
- */
 export interface OS2FlowPassword extends OS2SecurityOAuth {
   type: "oauth2";
   flow: "password";
   tokenUrl: string;
 }
 
-/**
- * @deprecated
- */
 export interface OS2FlowApplication extends OS2SecurityOAuth {
   type: "oauth2";
   flow: "application";
   tokenUrl: string;
 }
 
-/**
- * @deprecated
- */
 export interface OS2FlowAccessCode extends OS2SecurityOAuth {
   type: "oauth2";
   flow: "accessCode";
@@ -79,7 +55,4 @@ export interface OS2FlowAccessCode extends OS2SecurityOAuth {
   authorizationUrl: string;
 }
 
-/**
- * @deprecated
- */
 export type OS2Security = OS2SecurityBasic | OS2FlowAccessCode | OS2FlowApplication | OS2FlowImplicit | OS2FlowPassword | OS2SecurityApiKey;
