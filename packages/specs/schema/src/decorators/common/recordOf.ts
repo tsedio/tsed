9bbcda98c4d8fcebe990edfeadaf804520f6550d
Roberto Geuke <roberto@digitalenterprises.nl>
2022-03-15 10:05:57 +0100

    feat(schema): add RecordOf decorator

---
+++ packages/specs/schema/src/decorators/common/recordOf.ts
@@ -0,0 +1,59 @@
+import {Schema} from "./schema";
+
+/**
+ * Constructs a json schema object type whose property keys are keys and whose property values are type.
+ *
+ * ## Example
+ *
+ * ```typescript
+ * type keys = 'one' | 'two';
+ *
+ * class Part {
+ *   prop: string;
+ * }
+ *
+ * type Parts = Record<keys, Part>;
+ *
+ * class Test {
+ *   @RecordOf(Part, 'one', 'two')
+ *   parts: Parts;
+ * }
+ * ```
+ *
+ * Will produce:
+ *
+ * ```json
+ * {
+ *     "type": "object",
+ *     "properties": {
+ *         "one": {
+ *             "$ref": "#/components/schemas/Part"
+ *         },
+ *         "two": {
+ *             "$ref": "#/components/schemas/Part"
+ *         },
+ *     },
+ * }
+ * ```
+ *
+ * @param model
+ * @param keys
+ * @decorator
+ * @validation
+ * @swagger
+ * @schema
+ * @classDecorator
+ * @input
+ */
+export function RecordOf(model: any, ...keys: string[]) {
+  return Schema({
+    properties: {
+      ...keys.reduce((initial, key) => {
+        return {
+          ...initial,
+          [key]: model
+        };
+      }, {})
+    }
+  });
+}
