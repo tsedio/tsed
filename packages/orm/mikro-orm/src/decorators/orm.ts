42590a22a7a16522759dc0c5b112e25257b37a10
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-04-13 18:51:08 +0200

    fix(mikroOrm): remove @Connection decorator
    BREAKING CHANGE: remove `@Connection` decorator. Use `@Orm` instead.

--- packages/orm/mikro-orm/src/decorators/orm.ts
+++ packages/orm/mikro-orm/src/decorators/orm.ts
@@ -7,10 +7,3 @@ import {Inject} from "@tsed/di";
  */
 export const Orm = (contextName?: string): PropertyDecorator =>
   Inject(MikroOrmRegistry, (registry: MikroOrmRegistry) => registry.get(contextName)) as PropertyDecorator;
-
-/**
- * Get the ORM for the given context name.
- * @param {String} contextName
- * @deprecated Since 2022-02-01. Use {@link Orm} instead
- */
-export const Connection = Orm;
