a7f327cfb034219956d907fae197d7ce12c4213c
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-04-13 19:18:33 +0200

    fix(common): remove HttpsServer decorator
    BREAKING CHANGE: remove `HttpsServer`. Use `Https.Server` as injectable type instead

--- packages/platform/common/src/utils/createHttpsServer.ts
+++ packages/platform/common/src/utils/createHttpsServer.ts
@@ -2,7 +2,6 @@ import {InjectorService, ProviderScope} from "@tsed/di";
 import {getHostInfoFromPort} from "@tsed/core";
 import Https from "https";
 import Http from "http";
-import {HttpsServer} from "../decorators/httpsServer";
 import {listenServer} from "./listenServer";
 
 export function createHttpsServer(injector: InjectorService, requestListener?: Http.RequestListener) {
@@ -12,17 +11,11 @@ export function createHttpsServer(injector: InjectorService, requestListener?: H
 
   const server = httpsPort !== false ? Https.createServer(httpsOptions, requestListener) : null;
 
-  injector.addProvider(HttpsServer, {
-    scope: ProviderScope.SINGLETON,
-    useValue: server
-  });
-
   injector.addProvider(Https.Server, {
     scope: ProviderScope.SINGLETON,
     useValue: server
   });
 
-  injector.invoke(HttpsServer);
   injector.invoke(Https.Server);
 
   if (server) {
