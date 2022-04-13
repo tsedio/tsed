8f308a9cdc139d9c297cce55fd5f996dfda48ed9
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-04-13 19:14:06 +0200

    fix(passport): remove IProtocol interface
    BREAKING CHANGE: remove `IProtocol`. Use `ProtocolMethods` instead

--- packages/security/passport/src/interfaces/ProtocolMethods.ts
+++ packages/security/passport/src/interfaces/ProtocolMethods.ts
@@ -2,8 +2,4 @@ import {OnInstall} from "./OnInstall";
 import {OnVerify} from "./OnVerify";
 import {BeforeInstall} from "./BeforeInstall";
 
-/**
- * @deprecated Use ProtocolMethods instead
- */
-export interface IProtocol<Settings = any> extends OnVerify, OnInstall, BeforeInstall<Settings> {}
 export interface ProtocolMethods<Settings = any> extends OnVerify, OnInstall, BeforeInstall<Settings> {}
