a85031bb2dcd255cc973731c3032d90dd3310697
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-07-25 18:39:41 +0200

    chore: remove useCtxHandler function
    BREAKING CHANGE: remove useCtxHandler function. Use useContextHandler instead

--- packages/platform/common/src/utils/useCtxHandler.ts
+++
@@ -1,8 +0,0 @@
-import {useContextHandler} from "@tsed/platform-router";
-
-/**
- * Create Ts.ED context handler
- * @param fn
- * @deprecated Use useContextHandler from "@tsed/platform-router"
- */
-export const useCtxHandler = useContextHandler;
