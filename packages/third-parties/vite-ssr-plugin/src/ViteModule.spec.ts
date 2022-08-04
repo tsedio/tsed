160047e658911bc868915e76487bb530171f8b98
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-04 14:00:01 +0200

    feat(vite): add @tsed/vite-ssr-plugin package

---
+++ packages/third-parties/vite-ssr-plugin/src/ViteModule.spec.ts
@@ -0,0 +1,95 @@
+import {PlatformApplication, PlatformTest} from "@tsed/common";
+import {PlatformViews} from "@tsed/platform-views";
+
+import {ViteRendererMiddleware} from "./middlewares/ViteRendererMiddleware";
+import {VITE_SERVER} from "./services/ViteServer";
+import {ViteService} from "./services/ViteService";
+import {ViteModule} from "./ViteModule";
+
+jest.mock("vite-plugin-ssr");
+jest.mock("vite");
+
+async function getModuleFixture() {
+  const platformViews = {
+    registerEngine: jest.fn()
+  };
+  const viteService = {
+    render: jest.fn()
+  };
+  const platformApplication = {
+    use: jest.fn(),
+    get: jest.fn()
+  };
+  const viteServer = {
+    middlewares: "middlewares"
+  };
+  const service = await PlatformTest.invoke<ViteModule>(ViteModule, [
+    {
+      token: ViteService,
+      use: viteService
+    },
+    {
+      token: PlatformApplication,
+      use: platformApplication
+    },
+    {
+      token: PlatformViews,
+      use: platformViews
+    },
+    {
+      token: VITE_SERVER,
+      use: viteServer
+    }
+  ]);
+
+  return {service, viteService, platformApplication, platformViews, viteServer};
+}
+
+describe("ViteModule", () => {
+  beforeEach(async () => PlatformTest.create());
+  afterEach(async () => PlatformTest.reset());
+
+  describe("$onInit()", () => {
+    it("should render html", async () => {
+      const {viteService, platformViews} = await getModuleFixture();
+      viteService.render.mockResolvedValue("html");
+      expect(platformViews.registerEngine).toHaveBeenCalledWith("vite", {options: {}, render: expect.any(Function)});
+
+      const result = await platformViews.registerEngine.mock.calls[0][1].render("path.vite", {$ctx: {}});
+
+      expect(result).toEqual("html");
+      expect(viteService.render).toHaveBeenCalledWith("path.vite", {});
+    });
+
+    it("should render empty content", async () => {
+      const {viteService, platformViews} = await getModuleFixture();
+
+      viteService.render.mockResolvedValue(undefined);
+
+      expect(platformViews.registerEngine).toHaveBeenCalledWith("vite", {options: {}, render: expect.any(Function)});
+
+      const result = await platformViews.registerEngine.mock.calls[0][1].render("path.vite", {$ctx: {}});
+
+      expect(result).toEqual("");
+      expect(viteService.render).toHaveBeenCalledWith("path.vite", {});
+    });
+  });
+  describe("$afterInit()", () => {
+    it("should add the middlewares", async () => {
+      const {service, platformApplication} = await getModuleFixture();
+
+      service.$afterInit();
+
+      expect(platformApplication.use).toHaveBeenCalledWith(expect.any(Function), "middlewares");
+    });
+  });
+  describe("$afterRoutesInit()", () => {
+    it("should handle all GET routes", async () => {
+      const {service, platformApplication} = await getModuleFixture();
+
+      service.$afterRoutesInit();
+
+      expect(platformApplication.get).toHaveBeenCalledWith("*", ViteRendererMiddleware);
+    });
+  });
+});
---
+++ packages/third-parties/vite-ssr-plugin/src/services/ViteServer.spec.ts
@@ -0,0 +1,125 @@
+import {PlatformTest} from "@tsed/common";
+import {Env} from "@tsed/core";
+import sirv from "sirv";
+import {createServer} from "vite";
+
+import {VITE_SERVER} from "./ViteServer";
+
+jest.mock("vite-plugin-ssr");
+jest.mock("vite");
+jest.mock("sirv");
+
+describe("ViteServer", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    (createServer as jest.Mock).mockResolvedValue({middlewares: "middlewares"});
+    (sirv as jest.Mock).mockReturnValue("sirv");
+  });
+
+  describe("with default options - dev mode", () => {
+    beforeEach(async () =>
+      PlatformTest.create({
+        vite: {}
+      })
+    );
+    afterEach(async () => PlatformTest.reset());
+
+    it("should load vite dev server", async () => {
+      const viteDevServer = PlatformTest.get<VITE_SERVER>(VITE_SERVER);
+
+      expect(viteDevServer.middlewares).toEqual("middlewares");
+      expect(createServer).toHaveBeenCalledWith({
+        logLevel: "off",
+        server: {
+          middlewareMode: true
+        }
+      });
+
+      // @ts-ignore
+      await viteDevServer.$onDestroy();
+    });
+    it("should load and close server", async () => {
+      const viteDevServer = PlatformTest.get<VITE_SERVER>(VITE_SERVER);
+      viteDevServer.close = jest.fn();
+
+      expect(viteDevServer.middlewares).toEqual("middlewares");
+      expect(createServer).toHaveBeenCalledWith({
+        logLevel: "off",
+        server: {
+          middlewareMode: true
+        }
+      });
+
+      // @ts-ignore
+      await viteDevServer.$onDestroy();
+      expect(viteDevServer.close).toHaveBeenCalledWith();
+    });
+  });
+  describe("with defined options - dev mode", () => {
+    beforeEach(async () =>
+      PlatformTest.create({
+        vite: {
+          logLevel: "error",
+          root: "/root",
+          server: {
+            middlewareMode: false
+          }
+        }
+      })
+    );
+    afterEach(async () => PlatformTest.reset());
+
+    it("should load vite dev server", () => {
+      const viteDevServer = PlatformTest.get<VITE_SERVER>(VITE_SERVER);
+
+      expect(viteDevServer.middlewares).toEqual("middlewares");
+      expect(createServer).toHaveBeenCalledWith({
+        logLevel: "error",
+        root: "/root",
+        server: {
+          middlewareMode: true
+        }
+      });
+    });
+  });
+  describe("with default options - prod mode", () => {
+    beforeEach(async () =>
+      PlatformTest.create({
+        env: Env.PROD,
+        vite: {},
+        logger: {
+          level: "off"
+        }
+      })
+    );
+    afterEach(async () => PlatformTest.reset());
+
+    it("should load sirv to expose statics files", () => {
+      const viteDevServer = PlatformTest.get<VITE_SERVER>(VITE_SERVER);
+
+      expect(viteDevServer.middlewares).toEqual("sirv");
+      expect(sirv).toHaveBeenCalledWith("/dist/client");
+    });
+  });
+  describe("with defined options - prod mode", () => {
+    beforeEach(async () =>
+      PlatformTest.create({
+        env: Env.PROD,
+        logger: {
+          level: "off"
+        },
+        vite: {
+          root: "/root"
+        }
+      })
+    );
+    afterEach(async () => PlatformTest.reset());
+
+    it("should load sirv to expose statics files", () => {
+      const viteDevServer = PlatformTest.get<VITE_SERVER>(VITE_SERVER);
+
+      expect(viteDevServer.middlewares).toEqual("sirv");
+      expect(sirv).toHaveBeenCalledWith("/root/dist/client");
+    });
+  });
+});
