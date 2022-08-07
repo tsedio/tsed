321cb4d0f1373032e1b7c021560c3e816d0048f6
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-07 22:51:16 +0200

    fix(schema): optimize concat path algorithm

---
+++ packages/specs/schema/src/utils/concatPath.spec.ts
@@ -0,0 +1,23 @@
+import {concatPath} from "./concatPath";
+
+describe("concatPath", () => {
+  it("should return path", () => {
+    expect(concatPath("/controllers/", "/path")).toEqual("/controllers/path");
+    expect(concatPath("/controllers", "/path")).toEqual("/controllers/path");
+    expect(concatPath("/controllers/", "path")).toEqual("/controllers/path");
+    expect(concatPath(undefined, "/path")).toEqual("/path");
+    expect(concatPath("/controllers/", undefined)).toEqual("/controllers");
+    expect(concatPath("/", "/:id")).toEqual("/:id");
+    expect(concatPath("/", "/")).toEqual("/");
+    expect(concatPath("/", undefined)).toEqual("/");
+    expect(concatPath(undefined, "/")).toEqual("/");
+    expect(concatPath("tests/", "/test")).toEqual("tests/test");
+  });
+  it("should concat regexp", () => {
+    expect(concatPath(/\/(.*)/, /\/test(.*)/).source).toEqual("\\/(.*)\\/test(.*)");
+    expect(concatPath(undefined, /\/test(.*)/).source).toEqual("\\/test(.*)");
+    expect(concatPath(/\/(.*)/, undefined).source).toEqual("\\/(.*)");
+    expect(concatPath(/\/(.*)/, "/test").source).toEqual("\\/(.*)\\/test");
+    expect(concatPath("/test", /\/test(.*)/).source).toEqual("\\/test\\/test(.*)");
+  });
+});
