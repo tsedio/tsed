9f546c9312c969aacb31d1a9d3087df7804cfc40
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-09 10:47:56 +0200

    fix(json-mapper): don't assign value on readOnly field
    Closes: #2031

---
+++ packages/orm/mongoose/test/readonly.integration.spec.ts
@@ -0,0 +1,64 @@
+import {Inject, Injectable, PlatformTest} from "@tsed/common";
+import {getJsonSchema, Groups, Name, Property, ReadOnly, Required} from "@tsed/schema";
+import {TestMongooseContext} from "@tsed/testing-mongoose";
+import {Immutable, Model, MongooseModel, ObjectID} from "../src/index";
+
+class BaseModel {
+  @ObjectID("id")
+  @Required()
+  @Groups("!create")
+  _id: string;
+
+  @Required()
+  @Immutable()
+  @ReadOnly()
+  @Name("created_at")
+  @Groups("!create", "!update")
+  createdAt: number = Date.now();
+
+  @Required()
+  @ReadOnly()
+  @Name("updated_at")
+  @Groups("!create", "!update")
+  updatedAt: number = Date.now();
+}
+
+@Model()
+class DataSourceModel extends BaseModel {
+  @Property()
+  @ReadOnly()
+  get test() {
+    return "test";
+  }
+}
+
+@Injectable()
+class MyService {
+  @Inject(DataSourceModel)
+  model: MongooseModel<DataSourceModel>;
+
+  save() {
+    const instance = new this.model({});
+    return instance.save();
+  }
+}
+
+describe("Mongoose: ReadOnly", () => {
+  beforeEach(TestMongooseContext.create);
+  afterEach(TestMongooseContext.reset);
+  it("should generate json schema", () => {
+    const jsonSchema = getJsonSchema(DataSourceModel);
+
+    expect(jsonSchema).toMatchSnapshot();
+  });
+
+  it("should generate model", async () => {
+    const service = PlatformTest.get<MyService>(MyService);
+    await service.save();
+
+    const list = await service.model.find().exec();
+    expect(list.length).toEqual(1);
+    expect(list[0].toObject().test).toBeUndefined();
+    expect(list[0].toClass().test).toEqual("test");
+  });
+});
---
+++ packages/specs/json-mapper/test/integration/writeonly.integration.spec.ts
@@ -0,0 +1,42 @@
+import {deserialize, serialize} from "@tsed/json-mapper";
+import {Groups, Property, Required, WriteOnly} from "@tsed/schema";
+
+class DataSourceModel {
+  @Required()
+  @Groups("!create")
+  _id: string;
+
+  #test: string;
+
+  @Property()
+  @WriteOnly()
+  set test(test: string) {
+    this.#test = test;
+  }
+
+  getTest() {
+    return this.#test;
+  }
+}
+
+describe("JsonMapper: WriteOnly", () => {
+  it("should serialize object", () => {
+    const instance = new DataSourceModel();
+    instance._id = "id";
+    instance.test = "hello";
+
+    const obj = serialize(instance, {type: DataSourceModel});
+
+    expect(obj).toEqual({_id: "id"});
+  });
+
+  it("should deserialize object", () => {
+    const instance = new DataSourceModel();
+    instance._id = "id";
+
+    const obj = deserialize({test: "new", _id: "id"}, {type: DataSourceModel});
+
+    expect(obj).toEqual({_id: "id"});
+    expect(obj.getTest()).toEqual("new");
+  });
+});
