b23cddf38802123998210689f9f68229e0c20831
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-12 10:49:59 +0200

    chore: use command options to give all options

--- benchmarks/bin/benchmark-bench.js
+++ benchmarks/bin/benchmark-bench.js
@@ -2,8 +2,25 @@
 import inquirer from "inquirer";
 import bench from "../src/bench.js";
 import {choices, list} from "../src/packages.js";
+import {Command} from "commander";
 
-const argv = process.argv.slice(2);
+const program = new Command();
+
+program
+  .option("-a, --all", "run all benchmarks")
+  .option("-c, --connections <connections>", "Set the connections")
+  .option("-p, --pipelining <pipelining>", "Set the pipelining")
+  .option("-d, --duration <duration>", "Set the duration")
+  .parse();
+
+const opts = program.opts();
+const options = {
+  all: false,
+  ...opts,
+  connections: opts.connections ? +opts.connections : 100,
+  pipelining: opts.pipelining ? +opts.pipelining : 10,
+  duration: opts.duration ? +opts.duration : 40
+};
 
 run().catch((err) => {
   console.error(err);
@@ -11,61 +28,8 @@ run().catch((err) => {
 });
 
 async function run() {
-  const options = await getBenchmarkOptions();
   const modules = options.all ? choices : await select(list);
   return bench(options, modules);
-}
-
-async function getBenchmarkOptions() {
-  if (argv.length) return parseArgv();
-  return inquirer.prompt([
-    {
-      type: "confirm",
-      name: "all",
-      message: "Do you want to run all benchmark tests?",
-      default: false
-    },
-    {
-      type: "input",
-      name: "connections",
-      message: "How many connections do you need?",
-      default: 100,
-      validate(value) {
-        return !Number.isNaN(parseFloat(value)) || "Please enter a number";
-      },
-      filter: Number
-    },
-    {
-      type: "input",
-      name: "pipelining",
-      message: "How many pipelines do you need?",
-      default: 10,
-      validate(value) {
-        return !Number.isNaN(parseFloat(value)) || "Please enter a number";
-      },
-      filter: Number
-    },
-    {
-      type: "input",
-      name: "duration",
-      message: "How long should it take?",
-      default: 40,
-      validate(value) {
-        return !Number.isNaN(parseFloat(value)) || "Please enter a number";
-      },
-      filter: Number
-    }
-  ]);
-}
-
-function parseArgv() {
-  const [all, connections, pipelining, duration] = argv;
-  return {
-    all: all === "y",
-    connections: +connections,
-    pipelining: +pipelining,
-    duration: +duration
-  };
 }
 
 async function select() {
