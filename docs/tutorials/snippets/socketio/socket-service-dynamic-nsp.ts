589bbae61add7ae0f4a67590282464cbfcae4c87
Joey Vos <contact@jojoxd.nl>
2022-08-29 19:53:27 +0200

    docs(socketio): update documentation for dynamic namespaces

---
+++ docs/tutorials/snippets/socketio/socket-service-dynamic-nsp.ts
@@ -0,0 +1,13 @@
+import {Nsp, SocketNsp, Input, SocketService} from "@tsed/socketio";
+import * as SocketIO from "socket.io";
+
+@SocketService(/my-namespace-.+/)
+export class MySocketService {
+  // This will be the parent namespace.
+  @Nsp nsp: SocketIO.Namespace;
+
+  @Input("eventName")
+  async myMethod(@SocketNsp nsp: SocketNsp) {
+    // nsp is the actual namespace of this call.
+  }
+}
--- packages/third-parties/socketio/src/decorators/socketNsp.ts
+++ packages/third-parties/socketio/src/decorators/socketNsp.ts
@@ -4,6 +4,29 @@ import {SocketFilter} from "./socketFilter";
 
 export type SocketNsp = Namespace;
 
+/**
+ * Inject the [SocketIO.Namespace](https://socket.io/docs/rooms-and-namespaces/#namespaces) instance in the decorated parameter.
+ * This namespace may differ from the default namespace when using dynamic namespaces.
+ *
+ * ### Example
+ *
+ * ```typescript
+ * @SocketService(/nsp-.+/)
+ * export class MyWS {
+ *
+ *   @Input("event")
+ *   myMethod(@SocketNsp nsp: SocketNsp) {
+ *
+ *   }
+ * }
+ * ```
+ *
+ * @experimental
+ * @param target
+ * @param {string} propertyKey
+ * @param {number} index
+ * @decorator
+ */
 export function SocketNsp(target: any, propertyKey: string, index: number): any {
   return SocketFilter(SocketFilters.SOCKET_NSP)(target, propertyKey, index);
 }
