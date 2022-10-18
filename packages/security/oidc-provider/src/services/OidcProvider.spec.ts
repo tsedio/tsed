6581df85c7710d41282a61525860c35e92fd6d5b
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-10-18 17:20:30 +0200

    fix(oidc-provider): fix prompt mapping between methods and decorators parameters

--- packages/security/oidc-provider/src/services/OidcProvider.spec.ts
+++ packages/security/oidc-provider/src/services/OidcProvider.spec.ts
@@ -23,4 +23,50 @@ describe("OidcProvider", () => {
       expect(oidcProvider.getInteractionsUrl()({}, {uid: "uid"})).toEqual("/interaction/uid");
     });
   });
+  describe("createPrompt()", () => {
+    beforeEach(() =>
+      PlatformTest.create({
+        env: Env.PROD,
+        oidc: {
+          issuer: "http://localhost:8081",
+          secureKey: ["secureKey"]
+        }
+      })
+    );
+    afterEach(() => PlatformTest.reset());
+    it("should bind options to prompt instance", () => {
+      const oidcProvider = PlatformTest.get<OidcProvider>(OidcProvider);
+      const instance = {};
+      const options = {
+        name: "name",
+        requestable: true,
+        details: jest.fn(),
+        checks: []
+      };
+
+      const prompt = oidcProvider.createPrompt(instance, options);
+
+      expect(prompt.details).toEqual(options.details);
+      expect(prompt.name).toEqual(options.name);
+      expect(prompt.requestable).toEqual(options.requestable);
+    });
+
+    it("should bind methods from instance to prompt instance", () => {
+      const oidcProvider = PlatformTest.get<OidcProvider>(OidcProvider);
+      const instance = {
+        details: jest.fn(),
+        checks: jest.fn().mockReturnValue([])
+      };
+      const options = {
+        name: "name",
+        requestable: true
+      };
+
+      const prompt = oidcProvider.createPrompt(instance, options);
+
+      expect(prompt.details).toBeDefined();
+      expect(prompt.name).toEqual(options.name);
+      expect(prompt.requestable).toEqual(options.requestable);
+    });
+  });
 });
