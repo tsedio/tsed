468a1b888d0a72e354cdda0b8c5426bf7ae46db9
Oliver Christen <christen@app-logik.de>
2022-11-17 11:19:09 +0100

    refactor(agenda): remove redundant configuration

--- packages/third-parties/agenda/src/decorators/agenda.ts
+++ packages/third-parties/agenda/src/decorators/agenda.ts
@@ -1,18 +1,6 @@
 import {StoreMerge, useDecorators} from "@tsed/core";
 import {Injectable} from "@tsed/di";
-import {AgendaConfig} from "agenda";
 import {PROVIDER_TYPE_AGENDA} from "../constants/constants";
-
-declare global {
-  namespace TsED {
-    interface Configuration {
-      agenda?: {
-        /** * Enable Agenda jobs. Default false. */
-        enabled?: boolean;
-      } & AgendaConfig;
-    }
-  }
-}
 
 interface AgendaOptions {
   namespace?: string;
