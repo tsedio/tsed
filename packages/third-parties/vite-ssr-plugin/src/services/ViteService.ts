a9099e1bac95166bb158f3030da3a446e3427190
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-02-28 14:18:55 +0100

    fix(vite-ssr-plugin): fix typings issue with httpResponse

--- packages/third-parties/vite-ssr-plugin/src/services/ViteService.ts
+++ packages/third-parties/vite-ssr-plugin/src/services/ViteService.ts
@@ -1,7 +1,7 @@
-import {Constant, PlatformContext, PlatformRequest, PlatformResponse} from "@tsed/common";
+import {Constant, PlatformContext} from "@tsed/common";
 import {Injectable} from "@tsed/di";
+import {Writable} from "stream";
 import {renderPage} from "vite-plugin-ssr";
-
 import {ViteConfig} from "../interfaces/ViteConfig";
 import {ViteRenderContext} from "../interfaces/ViteRenderContext";
 
@@ -13,23 +13,7 @@ export class ViteService {
   @Constant("vite.enableStream", false)
   private enableStream: boolean;
 
-  async render(viewPath: string, $ctx: PlatformContext) {
-    const pageContext = await this.renderPage(viewPath, $ctx);
-
-    if (pageContext.httpResponse) {
-      const {httpResponse} = pageContext;
-
-      $ctx.response.contentType(httpResponse.contentType).status(httpResponse.statusCode);
-
-      if (this.enableStream) {
-        return httpResponse;
-      }
-
-      return httpResponse.body;
-    }
-  }
-
-  async renderPage(viewPath: string, $ctx: PlatformContext<PlatformRequest, PlatformResponse>) {
+  async render(viewPath: string, $ctx: PlatformContext): Promise<string | {pipe(stream: Writable): void} | undefined> {
     const urlOriginal = $ctx.request.url;
 
     const {data} = $ctx;
@@ -67,6 +51,16 @@ export class ViteService {
       });
     }
 
-    return pageContext;
+    if (pageContext.httpResponse) {
+      const {httpResponse} = pageContext;
+
+      $ctx.response.contentType(httpResponse.contentType).status(httpResponse.statusCode);
+
+      if (this.enableStream) {
+        return httpResponse;
+      }
+
+      return httpResponse.body;
+    }
   }
 }
