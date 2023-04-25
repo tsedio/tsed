7ae2d6a129de03e9625b2bc79f4ae7e5d1fd79d8
Camille Foubert <cfoubert@prophessi.com>
2023-04-25 14:23:17 +0200

    fix(oidc): export oidc-provider ClientCredentials type

--- packages/security/oidc-provider/src/domain/interfaces.ts
+++ packages/security/oidc-provider/src/domain/interfaces.ts
@@ -6,6 +6,7 @@ export type OidcClient = InstanceType<Provider["Client"]>;
 export type DefaultPolicy = interactionPolicy.DefaultPolicy;
 export type AuthorizationCode = InstanceType<Provider["AuthorizationCode"]>;
 export type AccessToken = InstanceType<Provider["AccessToken"]>;
+export type ClientCredentials = InstanceType<Provider["ClientCredentials"]>;
 export type DeviceCode = InstanceType<Provider["DeviceCode"]>;
 export type RefreshToken = InstanceType<Provider["RefreshToken"]>;
 export type BackchannelAuthenticationRequest = InstanceType<Provider["BackchannelAuthenticationRequest"]>;
