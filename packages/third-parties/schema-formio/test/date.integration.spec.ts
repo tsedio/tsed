5ea3082c4436b3b049384bd2ee6c95c93b0f6947
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-04-25 17:02:10 +0200

    fix(schema-formio): allow adding additional properties for date/time formio component

--- packages/third-parties/schema-formio/test/date.integration.spec.ts
+++ packages/third-parties/schema-formio/test/date.integration.spec.ts
@@ -1,5 +1,5 @@
 import {DateFormat, DateTime} from "@tsed/schema";
-import {getFormioSchema} from "../src";
+import {Component, getFormioSchema} from "../src";
 
 describe("Date integration", () => {
   it("should generate date-time field", async () => {
@@ -7,6 +7,7 @@ describe("Date integration", () => {
       @DateTime()
       test: Date;
     }
+
     const form = await getFormioSchema(Model);
     expect(form).toEqual({
       components: [
@@ -50,6 +51,7 @@ describe("Date integration", () => {
       @DateFormat()
       test: Date;
     }
+
     const form = await getFormioSchema(Model);
     expect(form).toEqual({
       components: [
@@ -57,6 +59,67 @@ describe("Date integration", () => {
           datePicker: {
             disableWeekdays: false,
             disableWeekends: false
+          },
+          disabled: false,
+          enableMaxDateInput: false,
+          enableMinDateInput: false,
+          enableTime: false,
+          format: "yyyy-MM-dd",
+          input: true,
+          key: "test",
+          label: "Test",
+          type: "datetime",
+          validate: {
+            required: false
+          },
+          widget: {
+            allowInput: true,
+            disableWeekdays: false,
+            disableWeekends: false,
+            displayInTimezone: "viewer",
+            enableTime: false,
+            format: "yyyy-MM-dd",
+            hourIncrement: 1,
+            locale: "en",
+            maxDate: null,
+            minDate: null,
+            minuteIncrement: 1,
+            mode: "single",
+            noCalendar: false,
+            type: "calendar",
+            useLocaleSettings: false
+          }
+        }
+      ],
+      display: "form",
+      machineName: "model",
+      name: "model",
+      title: "Model",
+      type: "form",
+      submissionAccess: [],
+      access: [],
+      tags: []
+    });
+  });
+  it("should generate date field with additional props", async () => {
+    class Model {
+      @DateFormat()
+      @Component({
+        datePicker: {
+          disableWeekends: true,
+          disableWeekdays: true
+        }
+      })
+      test: Date;
+    }
+
+    const form = await getFormioSchema(Model);
+    expect(form).toEqual({
+      components: [
+        {
+          datePicker: {
+            disableWeekdays: true,
+            disableWeekends: true
           },
           disabled: false,
           enableMaxDateInput: false,
