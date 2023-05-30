e8c2f68150bb627f02a2daac935d09e6f4f17182
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-05-30 11:51:12 +0200

    docs: add vite plugin ssr documentation

--- docs/.vuepress/scripts/build.sh
+++ docs/.vuepress/scripts/build.sh
@@ -1,4 +1,5 @@
 export NODE_ENV=production
+export NODE_OPTIONS=--openssl-legacy-provider
 cp .vuepress/scripts/updateBuildScript.js node_modules/@vuepress/core/lib/node/build/index.js
 cp .vuepress/scripts/addWorkerScript.js node_modules/@vuepress/core/lib/node/build/worker.js
 vuepress build
