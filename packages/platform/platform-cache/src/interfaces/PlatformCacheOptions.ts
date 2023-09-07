21d07a767a84bbcf65d5edbd165912ee7dcfa13b
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-09-07 11:36:55 +0200

    feat(platform-cache): add prefix option on useCache and add method to get keys related to Class/method

--- packages/platform/platform-cache/src/interfaces/PlatformCacheOptions.ts
+++ packages/platform/platform-cache/src/interfaces/PlatformCacheOptions.ts
@@ -3,8 +3,24 @@ import {BaseContext} from "@tsed/di";
 import type {Ttl} from "../services/PlatformCache";
 
 export interface PlatformCacheOptions extends MetadataTypes {
+  /**
+   * key expiration ttl in seconds.
+   */
   ttl?: Ttl;
+  /**
+   * By default, the key is generated from the method name and the arguments passed to the method.
+   */
+  prefix?: string;
+  /**
+   * Algorithm used to generate the key. By default, the key is generated from the method name and the arguments passed to the method.
+   */
   key?: string | ((args: any[], ctx?: BaseContext) => string);
+  /**
+   * threshold to refresh the cache.
+   */
   refreshThreshold?: number;
+  /**
+   * The function determine if the result must be cached or not.
+   */
   canCache?: ((item: any) => boolean) | "no-nullish";
 }
