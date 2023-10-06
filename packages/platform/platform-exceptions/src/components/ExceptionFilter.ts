c9b6bb21536d9197b167239fb7a5f581556ba8f9
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-06 10:36:01 +0200

    fix(platform-exceptions): remove unnecessary logs

--- packages/platform/platform-exceptions/src/components/ExceptionFilter.ts
+++ packages/platform/platform-exceptions/src/components/ExceptionFilter.ts
@@ -6,12 +6,8 @@ import {ErrorFilter} from "./ErrorFilter";
 @Catch(Exception)
 export class ExceptionFilter extends ErrorFilter {
   catch(error: Exception, ctx: BaseContext) {
-    const {response, logger, env} = ctx;
+    const {response, env} = ctx;
     const err = this.mapError(error, env);
-    logger.error({
-      error: err,
-      stack: error.stack
-    });
 
     response.setHeaders(this.getHeaders(error)).contentType("application/json").status(error.status).body(err);
   }
