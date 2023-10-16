d8bd471706514e0d3aa1227b22c422496afd603e
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-16 09:32:19 +0200

    feat(temporal): add runInContext on worker processor handler and fix coverage

--- packages/third-parties/bullmq/src/BullMQModule.ts
+++ packages/third-parties/bullmq/src/BullMQModule.ts
@@ -1,38 +1,27 @@
+import {BeforeInit, DIContext, runInContext} from "@tsed/common";
 import {Constant, InjectorService, Module} from "@tsed/di";
-import {JobDispatcher} from "./dispatchers";
-import {BeforeInit} from "@tsed/common";
-import {JobMethods} from "./contracts";
 import {getComputedType} from "@tsed/schema";
+import {Job, Queue, Worker} from "bullmq";
+import {v4} from "uuid";
 import {BullMQConfig} from "./config/config";
-import {Queue, Worker} from "bullmq";
-import ProcessorFactory from "./processors/ProcessorFactory";
+import {JobMethods} from "./contracts";
+import {JobDispatcher} from "./dispatchers";
 
 @Module()
 export class BullMQModule implements BeforeInit {
   @Constant("bullmq")
   private readonly bullmq: BullMQConfig;
 
-  constructor(
-    private readonly injector: InjectorService,
-    private readonly dispatcher: JobDispatcher,
-    private readonly factory: ProcessorFactory
-  ) {}
+  constructor(private readonly injector: InjectorService, private readonly dispatcher: JobDispatcher) {}
 
   $beforeInit() {
-    (this.bullmq.workerQueues ?? this.bullmq.queues).forEach((queue) => {
-      this.injector
-        .add(`bullmq.worker.${queue}`, {
-          type: "bullmq:worker",
-          useValue: new Worker(queue, this.factory.build(), {
-            connection: this.bullmq.connection
-          }),
-          hooks: {
-            $onDestroy: (worker) => worker.close()
-          }
-        })
-        .invoke<Worker>(`bullmq.worker.${queue}`);
-    });
+    this.buildWorkers();
+    this.buildQueues();
 
+    this.injector.getMany<JobMethods>("bullmq:cron").map((job) => this.dispatcher.dispatch(getComputedType(job)));
+  }
+
+  private buildQueues() {
     this.bullmq.queues.forEach((queue) => {
       this.injector
         .add(`bullmq.queue.${queue}`, {
@@ -46,7 +35,58 @@ export class BullMQModule implements BeforeInit {
         })
         .invoke<Queue>(`bullmq.queue.${queue}`);
     });
+  }
 
-    this.injector.getMany<JobMethods>("bullmq:cron").map((job) => this.dispatcher.dispatch(getComputedType(job)));
+  private buildWorkers() {
+    (this.bullmq.workerQueues ?? this.bullmq.queues).forEach((queue) => {
+      this.injector
+        .add(`bullmq.worker.${queue}`, {
+          type: "bullmq:worker",
+          useValue: new Worker(queue, this.onProcess.bind(this), {
+            connection: this.bullmq.connection
+          }),
+          hooks: {
+            $onDestroy: (worker) => worker.close()
+          }
+        })
+        .invoke<Worker>(`bullmq.worker.${queue}`);
+    });
+  }
+
+  private getJob(name: string, queueName: string) {
+    return this.injector.get(`bullmq.job.${queueName}.${name}`);
+  }
+
+  private async onProcess(job: Job) {
+    const jobService = this.getJob(job.name, job.queueName);
+
+    if (!jobService) {
+      this.injector.logger.warn({
+        event: "BULLMQ_JOB_NOT_FOUND",
+        message: `Job ${job.name} ${job.queueName} not found`
+      });
+      return;
+    }
+
+    const $ctx = new DIContext({
+      injector: this.injector,
+      logger: this.injector.logger,
+      id: job.id || v4().split("-").join("")
+    });
+
+    $ctx.set("BULLMQ_JOB", job);
+
+    try {
+      return await runInContext($ctx, () => jobService.handle(job.data, job));
+    } catch (er) {
+      $ctx.logger.error({
+        event: "BULLMQ_JOB_ERROR",
+        message: er.message,
+        stack: er.stack
+      });
+      throw er;
+    } finally {
+      await $ctx.destroy();
+    }
   }
 }
