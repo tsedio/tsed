9f93348ca4ce12f55f4512e070d5f7d8a029ea74
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-25 09:14:52 +0200

    fix(core): make isBuffer browser compliant
    Closes: #2480

--- packages/core/src/utils/objects/isBuffer.spec.ts
+++ packages/core/src/utils/objects/isBuffer.spec.ts
@@ -1,9 +1,11 @@
-import {isBuffer} from "./isBuffer";
+import {isBuffer, isUint8Array} from "./isBuffer";
 
 describe("isBuffer", () => {
   it("should return true", () => {
     expect(isBuffer(Buffer.from(""))).toBeTruthy();
     expect(isBuffer(Buffer)).toBeTruthy();
+    expect(isUint8Array(Uint8Array)).toBeTruthy();
+    expect(isUint8Array(Uint8Array.from([0, 1]))).toBeTruthy();
   });
   it("should return false", () => {
     expect(isBuffer({})).toBeFalsy();
