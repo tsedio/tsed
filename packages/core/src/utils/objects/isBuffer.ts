fb1409fe1c9603990c893e7af1d5ea0c18eb99b5
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-10-25 09:14:52 +0200

    fix(core): make isBuffer browser compliant
    Closes: #2480

--- packages/core/src/utils/objects/isBuffer.ts
+++ packages/core/src/utils/objects/isBuffer.ts
@@ -1,8 +1,19 @@
+import {nameOf} from "./nameOf";
+
 /**
  * Tests to see if the object is Buffer
  * @param target
  * @returns {boolean}
  */
 export function isBuffer(target: any): target is Buffer {
-  return !!(target && (target === Buffer || target instanceof Buffer));
+  // is Class
+  if (target && "isBuffer" in target && typeof target.isBuffer === "function") {
+    return true;
+  }
+
+  return isUint8Array(target);
+}
+
+export function isUint8Array(target: any): target is Uint8Array {
+  return !!(target && (target === Uint8Array || target instanceof Uint8Array));
 }
