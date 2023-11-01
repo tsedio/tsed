17db6d6682b3a141ac2dc26427cce846f28f0e9d
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-11-01 10:07:40 +0100

    feat(apollo): add $alterApolloServerPlugins hook to register additional plugins

--- packages/graphql/apollo/src/interfaces/ApolloSettings.ts
+++ packages/graphql/apollo/src/interfaces/ApolloSettings.ts
@@ -1,11 +1,17 @@
-import type {ApolloServerBase, Config, ApolloServerPluginLandingPageGraphQLPlaygroundOptions} from "apollo-server-core";
+import type {ApolloServerBase, ApolloServerPluginLandingPageGraphQLPlaygroundOptions, Config} from "apollo-server-core";
 
 export type ApolloMiddlewareOptions = Record<string, any>;
 export type ApolloServer = ApolloServerBase & {getMiddleware(settings: ApolloMiddlewareOptions): any};
 export type ApolloCustomServerCB = (config: ApolloConfig) => ApolloServer;
 export type ApolloConfig = Config;
 
-export interface ApolloSettings extends ApolloConfig {
+declare global {
+  namespace TsED {
+    interface ApolloSettings {}
+  }
+}
+
+export interface ApolloSettings extends ApolloConfig, TsED.ApolloSettings {
   // Basic options
   path: string;
   server?: ApolloCustomServerCB;
