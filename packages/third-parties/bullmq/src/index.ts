ef6f7a2b9ae096d57a170dab8bf538451b213eee
EinfachHans <hanskrywaa@web.de>
2023-11-20 14:50:04 +0100

    chore: barrels

--- packages/third-parties/bullmq/src/index.ts
+++ packages/third-parties/bullmq/src/index.ts
@@ -7,4 +7,5 @@ export * from "./config/config";
 export * from "./contracts/JobMethods";
 export * from "./contracts/JobStore";
 export * from "./decorators/JobController";
+export * from "./dispatchers/JobDispatcher.options";
 export * from "./dispatchers/JobDispatcher";
