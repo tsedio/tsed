a97530b0515c0dc714e8b3dcdeda8630037ad3f4
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-02-29 09:07:38 +0100

    test: update swagger module test
    Signed-off-by: Romain Lenzotti <rom.lenzotti@gmail.com>

--- packages/specs/swagger/src/middlewares/redirectMiddleware.ts
+++ packages/specs/swagger/src/middlewares/redirectMiddleware.ts
@@ -6,7 +6,7 @@ import {PlatformContext} from "@tsed/common";
  */
 export function redirectMiddleware(path: string) {
   return (ctx: PlatformContext) => {
-    if (ctx.request.method === "GET" && ctx.request.url === path && !ctx.request.url.match(/\/$/)) {
+    if (ctx.request.method?.toUpperCase() === "GET" && ctx.request.url === path && !ctx.request.url.match(/\/$/)) {
       ctx.response.redirect(302, `${path}/`);
     }
   };
