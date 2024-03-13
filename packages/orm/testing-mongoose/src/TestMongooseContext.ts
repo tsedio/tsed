5802c756156e75612cea3149aca8055e45305ec9
Abenet Tamiru <me@abenet.email>
2024-03-13 08:15:46 +0100

    fix(testing-mongoose): remove artificial delay

--- packages/orm/testing-mongoose/src/TestMongooseContext.ts
+++ packages/orm/testing-mongoose/src/TestMongooseContext.ts
@@ -54,7 +54,6 @@ export class TestMongooseContext extends PlatformTest {
    * Resets the test injector of the test context, so it won't pollute your next test. Call this in your `tearDown` logic.
    */
   static async reset() {
-    await new Promise((resolve) => setTimeout(resolve, 100));
     await PlatformTest.reset();
     await TestMongooseContext.getMongo().stop();
   }
