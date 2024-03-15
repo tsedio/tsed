4664742a18429207ece7244208e1f757853d7259
Camille Foubert <cfoubert@prophessi.com>
2024-03-15 10:54:14 +0100

    fix(oidc-provider-plugin-wildcard-redirect-uri): handle wildcard for post_logout_redirect_uris

--- packages/security/oidc-provider-plugin-wildcard-redirect-uri/src/utils/wildcardRedirectUriAllowed.spec.ts
+++ packages/security/oidc-provider-plugin-wildcard-redirect-uri/src/utils/wildcardRedirectUriAllowed.spec.ts
@@ -3,7 +3,7 @@ import {wildcardRedirectUriAllowed} from "./wildcardRedirectUriAllowed";
 describe("wildcardRedirectUriAllowed()", () => {
   it("should call the original function if there is no wildcard in client redirect_uris", () => {
     const originalRedirectUriAllowed = jest.fn();
-    wildcardRedirectUriAllowed(originalRedirectUriAllowed).call(
+    wildcardRedirectUriAllowed(originalRedirectUriAllowed,"redirectUris").call(
       {
         redirectUris: ["https://redirect.test.com"]
       },
@@ -14,7 +14,7 @@ describe("wildcardRedirectUriAllowed()", () => {
 
   it("should return original function result if client has wildcard uris but not used", () => {
     const originalRedirectUriAllowed = jest.fn().mockReturnValue(true);
-    const result = wildcardRedirectUriAllowed(originalRedirectUriAllowed).call(
+    const result = wildcardRedirectUriAllowed(originalRedirectUriAllowed,"redirectUris").call(
       {
         redirectUris: ["https://test.com/titi", "https://*.test.com/toto"]
       },
@@ -25,9 +25,20 @@ describe("wildcardRedirectUriAllowed()", () => {
   });
   it("should return true if the redirect_uri match client redirect_uri with wildcard", () => {
     const originalRedirectUriAllowed = jest.fn();
-    const result = wildcardRedirectUriAllowed(originalRedirectUriAllowed).call(
+    const result = wildcardRedirectUriAllowed(originalRedirectUriAllowed,"redirectUris").call(
       {
         redirectUris: ["https://*.test.com/"]
+      },
+      "https://toto.test.com/"
+    );
+    expect(originalRedirectUriAllowed).not.toHaveBeenCalledWith();
+    expect(result).toEqual(true);
+  });
+  it("should return true if the post_logout_redirect_uri match client redirect_uri with wildcard", () => {
+    const originalRedirectUriAllowed = jest.fn();
+    const result = wildcardRedirectUriAllowed(originalRedirectUriAllowed,"postLogoutRedirectUris").call(
+      {
+        postLogoutRedirectUris: ["https://*.test.com/"]
       },
       "https://toto.test.com/"
     );
