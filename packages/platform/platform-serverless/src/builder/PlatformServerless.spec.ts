2b353d7b99e9dd9a7e666a6f297c0e8902952a8d
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-03-23 07:01:55 +0100

    feat(platform-serverless): add PlatformServerless.callback method

--- packages/platform/platform-serverless/src/builder/PlatformServerless.spec.ts
+++ packages/platform/platform-serverless/src/builder/PlatformServerless.spec.ts
@@ -56,149 +56,152 @@ class TimeslotsLambdaController {
 }
 
 describe("PlatformServerless", () => {
-  beforeEach(
-    PlatformServerlessTest.bootstrap(PlatformServerless, {
-      lambda: [TimeslotsLambdaController]
-    })
-  );
-  afterEach(() => PlatformServerlessTest.reset());
-
-  it("should load lambda", () => {
-    const handlers = PlatformServerlessTest.callbacks;
-
-    expect(handlers).toHaveProperty("get");
-    expect(handlers).toHaveProperty("byID");
-  });
-
-  it("should call lambda", async () => {
-    const response = await PlatformServerlessTest.request.call("get").query({
-      start_date: new Date("2020-01-01"),
-      end_date: new Date("2020-01-10")
-    });
-
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "requestId",
-      "content-type": "application/json"
-    });
-    expect(JSON.parse(response.body)).toEqual({
-      endDate: "2020-01-10T00:00:00.000Z",
-      startDate: "2020-01-01T00:00:00.000Z",
-      value: "test"
-    });
-  });
-
-  it("should call lambda with custom x-request-id", async () => {
-    const response = await PlatformServerlessTest.request
-      .call("get")
-      .headers({
-        "x-request-id": "request-id"
+  describe("bootstrap()", () => {
+    beforeEach(
+      PlatformServerlessTest.bootstrap(PlatformServerless, {
+        lambda: [TimeslotsLambdaController]
       })
-      .query({
+    );
+    afterEach(() => PlatformServerlessTest.reset());
+
+    it("should load lambda", () => {
+      const handlers = PlatformServerlessTest.callbacks;
+
+      expect(handlers).toHaveProperty("get");
+      expect(handlers).toHaveProperty("byID");
+    });
+    it("should call lambda", async () => {
+      const response = await PlatformServerlessTest.request.call("get").query({
         start_date: new Date("2020-01-01"),
         end_date: new Date("2020-01-10")
       });
 
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "request-id",
-      "content-type": "application/json"
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "requestId",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        endDate: "2020-01-10T00:00:00.000Z",
+        startDate: "2020-01-01T00:00:00.000Z",
+        value: "test"
+      });
     });
-    expect(JSON.parse(response.body)).toEqual({
-      endDate: "2020-01-10T00:00:00.000Z",
-      startDate: "2020-01-01T00:00:00.000Z",
-      value: "test"
+    it("should call lambda with custom x-request-id", async () => {
+      const response = await PlatformServerlessTest.request
+        .call("get")
+        .headers({
+          "x-request-id": "request-id"
+        })
+        .query({
+          start_date: new Date("2020-01-01"),
+          end_date: new Date("2020-01-10")
+        });
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "request-id",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        endDate: "2020-01-10T00:00:00.000Z",
+        startDate: "2020-01-01T00:00:00.000Z",
+        value: "test"
+      });
+    });
+    it("should call lambda from handler()", async () => {
+      const response = await PlatformServerlessTest.request.get("/").query({
+        start_date: new Date("2020-01-01"),
+        end_date: new Date("2020-01-10")
+      });
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "requestId",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        endDate: "2020-01-10T00:00:00.000Z",
+        startDate: "2020-01-01T00:00:00.000Z",
+        value: "test"
+      });
+    });
+    it("should call lambda from handler() with params", async () => {
+      const response = await PlatformServerlessTest.request.get("/1");
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "requestId",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        id: "1"
+      });
+    });
+    it("should call lambda from handler() (post)", async () => {
+      const response = await PlatformServerlessTest.request.post("/").body({
+        label: "label"
+      });
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "requestId",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        label: "label"
+      });
+    });
+    it("should call lambda from handler() (put)", async () => {
+      const response = await PlatformServerlessTest.request.put("/1").body({
+        label: "label"
+      });
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "requestId",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        label: "label"
+      });
+    });
+    it("should call lambda from handler() (patch)", async () => {
+      const response = await PlatformServerlessTest.request.patch("/1").body({
+        label: "label"
+      });
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "requestId",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        label: "label"
+      });
+    });
+    it("should call lambda from handler() (delete)", async () => {
+      const response = await PlatformServerlessTest.request.delete("/1").body({
+        label: "label"
+      });
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.headers).toEqual({
+        "x-request-id": "requestId",
+        "content-type": "application/json"
+      });
+      expect(JSON.parse(response.body)).toEqual({
+        label: "label"
+      });
     });
   });
+  describe("callback()", () => {
+    it("should create handler from given parameters", () => {
+      const handler = PlatformServerless.callback(TimeslotsLambdaController, "get", {
+        // settings
+      });
 
-  it("should call lambda from handler()", async () => {
-    const response = await PlatformServerlessTest.request.get("/").query({
-      start_date: new Date("2020-01-01"),
-      end_date: new Date("2020-01-10")
-    });
-
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "requestId",
-      "content-type": "application/json"
-    });
-    expect(JSON.parse(response.body)).toEqual({
-      endDate: "2020-01-10T00:00:00.000Z",
-      startDate: "2020-01-01T00:00:00.000Z",
-      value: "test"
-    });
-  });
-
-  it("should call lambda from handler() with params", async () => {
-    const response = await PlatformServerlessTest.request.get("/1");
-
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "requestId",
-      "content-type": "application/json"
-    });
-    expect(JSON.parse(response.body)).toEqual({
-      id: "1"
-    });
-  });
-
-  it("should call lambda from handler() (post)", async () => {
-    const response = await PlatformServerlessTest.request.post("/").body({
-      label: "label"
-    });
-
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "requestId",
-      "content-type": "application/json"
-    });
-    expect(JSON.parse(response.body)).toEqual({
-      label: "label"
-    });
-  });
-
-  it("should call lambda from handler() (put)", async () => {
-    const response = await PlatformServerlessTest.request.put("/1").body({
-      label: "label"
-    });
-
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "requestId",
-      "content-type": "application/json"
-    });
-    expect(JSON.parse(response.body)).toEqual({
-      label: "label"
-    });
-  });
-
-  it("should call lambda from handler() (patch)", async () => {
-    const response = await PlatformServerlessTest.request.patch("/1").body({
-      label: "label"
-    });
-
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "requestId",
-      "content-type": "application/json"
-    });
-    expect(JSON.parse(response.body)).toEqual({
-      label: "label"
-    });
-  });
-
-  it("should call lambda from handler() (delete)", async () => {
-    const response = await PlatformServerlessTest.request.delete("/1").body({
-      label: "label"
-    });
-
-    expect(response.statusCode).toEqual(200);
-    expect(response.headers).toEqual({
-      "x-request-id": "requestId",
-      "content-type": "application/json"
-    });
-    expect(JSON.parse(response.body)).toEqual({
-      label: "label"
+      expect(handler).toBeInstanceOf(Function);
     });
   });
 });
