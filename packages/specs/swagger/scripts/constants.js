a507869b9061bbe60168ea867edd978cbdae12a0
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-04-27 18:40:11 +0200

    fix(swagger): fix resolved swagger ui asset path for ESM version
    Closes: #2683

--- packages/specs/swagger/scripts/constants.js
+++ packages/specs/swagger/scripts/constants.js
@@ -2,6 +2,6 @@ import {dirname} from "node:path";
 import {fileURLToPath} from "node:url";
 
 // @ts-ignore
-export const SWAGGER_UI_DIST = dirname(import.meta.resolve("swagger-ui-dist"));
+export const SWAGGER_UI_DIST = dirname(import.meta.resolve("swagger-ui-dist")).replace("file://", "");
 // @ts-expect-error
 export const ROOT_DIR = dirname(fileURLToPath(import.meta.url));
