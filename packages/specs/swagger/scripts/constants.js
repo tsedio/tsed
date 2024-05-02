799e5639fb829f20a87d04cf444acc666134a319
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-05-02 18:26:08 +0200

    fix(swagger): fix resolved path in windows OS context
    Closes: #2692

--- packages/specs/swagger/scripts/constants.js
+++ packages/specs/swagger/scripts/constants.js
@@ -2,6 +2,6 @@ import {dirname} from "node:path";
 import {fileURLToPath} from "node:url";
 
 // @ts-ignore
-export const SWAGGER_UI_DIST = dirname(import.meta.resolve("swagger-ui-dist")).replace("file://", "");
+export const SWAGGER_UI_DIST = dirname(fileURLToPath(import.meta.resolve("swagger-ui-dist")));
 // @ts-expect-error
 export const ROOT_DIR = dirname(fileURLToPath(import.meta.url));
