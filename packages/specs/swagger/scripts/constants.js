864d16f10525744261b4769178f268706ed78b6b
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-05-07 10:16:08 +0200

    fix(swagger): use getAbsolutePath from swagger-ui-dist/get-absolute-path.js
    Closes: #2695

--- packages/specs/swagger/scripts/constants.js
+++ packages/specs/swagger/scripts/constants.js
@@ -1,7 +1,8 @@
 import {dirname} from "node:path";
 import {fileURLToPath} from "node:url";
+import * as SwaggerUIDist from "swagger-ui-dist/absolute-path.js";
 
 // @ts-ignore
-export const SWAGGER_UI_DIST = dirname(fileURLToPath(import.meta.resolve("swagger-ui-dist")));
+export const SWAGGER_UI_DIST = await (SwaggerUIDist.default || SwaggerUIDist)();
 // @ts-expect-error
 export const ROOT_DIR = dirname(fileURLToPath(import.meta.url));
