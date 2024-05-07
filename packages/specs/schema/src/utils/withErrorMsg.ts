1666c91048fe7b4aa73bc3e9308a41d2cd8df441
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-05-07 11:29:46 +0200

    fix(schema): fix custom error message set through .Error() nested decorator
    Closes: #2696

--- packages/specs/schema/src/utils/withErrorMsg.ts
+++ packages/specs/schema/src/utils/withErrorMsg.ts
@@ -1,5 +1,5 @@
-import {useDecorators} from "@tsed/core";
-import {ErrorMsg} from "../decorators/common/errorMsg";
+import {deepMerge, useDecorators} from "@tsed/core";
+import {JsonEntityFn} from "../decorators/common/jsonEntityFn";
 
 export interface ErrorChainedMethods<T> {
   <T>(target: Object, propertyKey?: string | symbol | undefined, descriptor?: TypedPropertyDescriptor<T> | number): any;
@@ -21,7 +21,36 @@ export function withErrorMsg<Decorator extends (...args: any[]) => any>(
     const decorator = useDecorators(originalDecorator(...decoratorOptions));
     (decorator as any).Error = (message: string) => {
       schema.message = message;
-      return useDecorators(decorator, schema.message && ErrorMsg({[errorKey]: message}));
+
+      return useDecorators(
+        decorator,
+        schema.message &&
+          JsonEntityFn((store) => {
+            // since errorMessage is a custom key, it is prefixed with a # to avoid conflict with JSON Schema keywords
+            const errorMessage = store.parentSchema.get("#errorMessage") || {};
+
+            if (errorKey === "required") {
+              store.parentSchema.customKey(
+                "errorMessage",
+                deepMerge(errorMessage, {
+                  [errorKey]: {
+                    [store.propertyName]: message
+                  }
+                })
+              );
+            } else {
+              // if (store.isCollection) {
+              //
+              // }
+              store.itemSchema.customKey(
+                "errorMessage",
+                deepMerge(errorMessage, {
+                  [errorKey]: message
+                })
+              );
+            }
+          })
+      );
     };
 
     return decorator;
