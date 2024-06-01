a7387e7c60bc3e44bb9a6df4cdf8d56049c0e894
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-06-01 12:19:41 +0200

    fix(schema): add support JsonSchema to In.Schema decorator

--- packages/specs/schema/src/decorators/operations/in.spec.ts
+++ packages/specs/schema/src/decorators/operations/in.spec.ts
@@ -1,4 +1,4 @@
-import {execMapper, getSpec, In, JsonEntityStore, Name, OperationPath, Path, SpecTypes} from "../../index";
+import {execMapper, getSpec, In, JsonEntityStore, Name, number, OperationPath, Path, SpecTypes} from "../../index";
 
 describe("In", () => {
   it("should declare all schema correctly (param)", () => {
@@ -120,7 +120,7 @@ describe("In", () => {
       }
     });
   });
-  it("should extra schema", () => {
+  it("should declare a pattern", () => {
     // WHEN
     @Path("/:parentId")
     @In("path")
@@ -186,6 +186,96 @@ describe("In", () => {
                 schema: {
                   type: "string",
                   pattern: "^[0-9a-fA-F]{24}$"
+                }
+              },
+              {
+                in: "path",
+                name: "path",
+                required: true,
+                schema: {
+                  type: "string"
+                }
+              }
+            ],
+            responses: {
+              "200": {
+                description: "Success"
+              }
+            },
+            tags: ["Controller"]
+          }
+        }
+      },
+      tags: [
+        {
+          name: "Controller"
+        }
+      ]
+    });
+  });
+  it("should declare schema", () => {
+    // WHEN
+    @Path("/:parentId")
+    @In("path").Name("parentId").Required().Description("description").Schema(number().integer().minimum(2))
+    class Controller {
+      @OperationPath("GET", "/:path")
+      method(@In("path") @Name("basic") basic: string) {}
+    }
+
+    // THEN
+    const spec = getSpec(Controller, {
+      specType: SpecTypes.OPENAPI
+    });
+
+    const paramSchema = JsonEntityStore.from(Controller, "method", 0);
+    const methodSchema = paramSchema.parent;
+    const operation = execMapper("operation", [methodSchema.operation!], {
+      specType: SpecTypes.OPENAPI
+    });
+
+    expect(operation).toEqual({
+      parameters: [
+        {
+          in: "path",
+          name: "basic",
+          required: true,
+          schema: {
+            type: "string"
+          }
+        },
+        {
+          description: "description",
+          in: "path",
+          name: "parentId",
+          required: true,
+          schema: {
+            minimum: 2,
+            multipleOf: 1,
+            type: "integer"
+          }
+        }
+      ],
+      responses: {
+        "200": {
+          description: "Success"
+        }
+      }
+    });
+    expect(spec).toEqual({
+      paths: {
+        "/{parentId}/{path}": {
+          get: {
+            operationId: "controllerMethod",
+            parameters: [
+              {
+                description: "description",
+                in: "path",
+                name: "parentId",
+                required: true,
+                schema: {
+                  minimum: 2,
+                  multipleOf: 1,
+                  type: "integer"
                 }
               },
               {
