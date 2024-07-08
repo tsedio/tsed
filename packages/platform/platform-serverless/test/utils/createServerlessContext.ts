2262e26c4101370a488f900b900fbb90fa408ee9
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-07-08 17:06:24 +0200

    fix(platform-serverless): fix .headers undefined error when event is a SQS event
    Closes: #2746

--- packages/platform/platform-serverless/test/utils/createServerlessContext.ts
+++ packages/platform/platform-serverless/test/utils/createServerlessContext.ts
@@ -1,11 +1,19 @@
 import {JsonEntityStore} from "@tsed/schema";
-import {createFakeContext, createFakeEvent, PlatformServerlessTest} from "@tsed/platform-serverless-testing";
+import {createFakeEvent, createFakeContext, PlatformServerlessTest} from "@tsed/platform-serverless-testing";
 import {Logger} from "@tsed/logger";
 import {ServerlessContext} from "../../src/domain/ServerlessContext.js";
 
-export function createServerlessContext({endpoint}: {endpoint: JsonEntityStore}) {
-  const context: any = createFakeContext();
-  const event: any = createFakeEvent();
+export function createServerlessContext({
+  endpoint,
+  event: initialEvent,
+  context: initialContext
+}: {
+  endpoint: JsonEntityStore;
+  event?: Parameters<typeof createFakeEvent>[0];
+  context?: Parameters<typeof createFakeContext>[0];
+}) {
+  const context: any = createFakeContext(initialContext);
+  const event: any = createFakeEvent(initialEvent);
 
   return new ServerlessContext({
     event,
