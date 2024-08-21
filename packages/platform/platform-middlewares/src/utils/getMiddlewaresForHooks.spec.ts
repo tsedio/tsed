66c83975945d588c3557e7f7c6861b196b5a4cd2
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-08-21 08:12:57 +0200

    chore: migrate platform-middlewares package to vitest

--- packages/platform/platform-middlewares/src/utils/getMiddlewaresForHooks.spec.ts
+++ packages/platform/platform-middlewares/src/utils/getMiddlewaresForHooks.spec.ts
@@ -5,8 +5,8 @@ describe("getMiddlewaresForHooks", () => {
   it("should return the middlewares for given hook (without default hook)", () => {
     const settings = new DIConfiguration();
     settings.set("middlewares", [
-      {env: "production", use: jest.fn(), hook: "hook"},
-      {use: jest.fn(), hook: "hook"}
+      {env: "production", use: vi.fn(), hook: "hook"},
+      {use: vi.fn(), hook: "hook"}
     ]);
 
     const middlewares = getMiddlewaresForHook("hook", settings);
