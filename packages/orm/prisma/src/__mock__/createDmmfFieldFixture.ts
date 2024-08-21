1091bf5c51c2e83f5dd82c5d656ebfe131fed066
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-08-21 09:18:46 +0200

    chore: migrate prisma package to vitest

--- packages/orm/prisma/src/__mock__/createDmmfFieldFixture.ts
+++ packages/orm/prisma/src/__mock__/createDmmfFieldFixture.ts
@@ -25,7 +25,7 @@ export function createDmmfFieldFixture(options: Partial<FieldFixtureOptions> = {
     },
     model: {
       isInputType: options.isInputType,
-      addImportDeclaration: jest.fn()
+      addImportDeclaration: vi.fn()
     }
   });
 }
