be3b97a9b3e403c06bac3fdeba27822b4094d497
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-09-08 19:31:32 +0200

    fix: remove @tsed/typeorm package

--- packages/orm/typeorm/test/helpers/entity/User.ts
+++
@@ -1,9 +0,0 @@
-import {Property} from "@tsed/schema";
-import {Entity, PrimaryGeneratedColumn} from "typeorm";
-
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  @Property()
-  id: number;
-}
--- packages/orm/typeorm/test/helpers/repository/UserRepository.ts
+++
@@ -1,5 +0,0 @@
-import {EntityRepository, Repository} from "typeorm";
-import {User} from "../entity/User.js";
-
-@EntityRepository(User)
-export class UserRepository extends Repository<User> {}
