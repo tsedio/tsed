1df12f7d56e5b97f8e859b81aee6b07f86b14db1
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-09-09 09:14:01 +0200

    refactor(schema): remove Route alias decorator
    BREAKING CHANGE: remove Route alias decorator. Use Operation instead

--- packages/platform/platform-serverless/src/exports.ts
+++ packages/platform/platform-serverless/src/exports.ts
@@ -1,3 +1,3 @@
 export * from "@tsed/di";
-export {Get, Post, Put, Delete, Head, Patch, Options, Route as Lambda} from "@tsed/schema";
+export {Get, Post, Put, Delete, Head, Patch, Options, Operation, Operation as Lambda} from "@tsed/schema";
 export * from "@tsed/platform-params";
