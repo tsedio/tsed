c89aa169f662b5e2fb052d2b88960b485a62b3b2
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-10-05 09:05:30 +0200

    fix(prisma): update prisma to v5

--- packages/orm/prisma/src/generator/utils/generateRepositories.ts
+++ packages/orm/prisma/src/generator/utils/generateRepositories.ts
@@ -41,7 +41,7 @@ function addDelegatedMethod({name, hasQuestionToken, repository, model, returnTy
 }
 
 export function generateRepositories(dmmf: DMMF.Document, project: Project, baseDirPath: string) {
-  const modelsMap = toMap<string, DMMF.Model>(dmmf.datamodel.models, "name");
+  const modelsMap = toMap<string, DMMF.Model>(dmmf.datamodel.models as any, "name");
   const models = DmmfModel.getModels(dmmf, modelsMap);
   const repoDirPath = path.resolve(baseDirPath, "repositories");
   const repoDirectory = project.createDirectory(repoDirPath);
