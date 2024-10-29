9efa0179affb84f9115eab38c9e3c788efb489e4
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-10-29 20:48:52 +0100

    docs: update theme version

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,32 @@
 // https://vitepress.dev/guide/custom-theme
 import {DefaultTheme} from "@tsed/vitepress-theme";
 import type {Theme} from "vitepress";
+import {h} from "vue";
+import HomeBanner from "@tsed/vitepress-theme/organisms/home/HomeBanner.vue";
+import HomePartners from "@tsed/vitepress-theme/organisms/home/HomePartners.vue";
+import HomeBody from "@tsed/vitepress-theme/organisms/home/HomeBody.vue";
+import HomeTabsTerminal from "@tsed/vitepress-theme/organisms/home/terminal/HomeTabsTerminal.vue";
+import HomeTabTerminalNpm from "@tsed/vitepress-theme/organisms/home/terminal/HomeTabTerminalNpm.vue";
+import HomeTabTerminalYarn from "@tsed/vitepress-theme/organisms/home/terminal/HomeTabTerminalYarn.vue";
+import HomeTabTerminalPnpm from "@tsed/vitepress-theme/organisms/home/terminal/HomeTabTerminalPnpm.vue";
+import HomeTabTerminalBun from "@tsed/vitepress-theme/organisms/home/terminal/HomeTabTerminalBun.vue";
 
 export default {
   extends: DefaultTheme,
-  enhanceApp() {}
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      "home-hero-image": () =>
+        h(HomeBanner, null, {
+          default: () =>
+            h(HomeTabsTerminal, null, {
+              npm: () => h(HomeTabTerminalNpm),
+              yarn: () => h(HomeTabTerminalYarn),
+              pnpm: () => h(HomeTabTerminalPnpm),
+              bun: () => h(HomeTabTerminalBun)
+            })
+        }),
+      "home-features-before": () => h(HomePartners),
+      "home-features-after": () => h(HomeBody)
+    });
+  }
 } satisfies Theme;
