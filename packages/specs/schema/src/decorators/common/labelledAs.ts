2062ef230354e53e5c17d5c3b4897fb5052ec0bc
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-12-05 11:16:09 +0100

    chore: review

--- packages/specs/schema/src/decorators/common/labelledAs.ts
+++ packages/specs/schema/src/decorators/common/labelledAs.ts
@@ -2,16 +2,33 @@ import {JsonEntityFn} from "./jsonEntityFn.js";
 
 /**
  * Use label to create ref on the current decorated property.
- * @param name The name of the label
+ *
+ * ### Example
+ *
+ * ```ts
+ * class Model {
+ *   // For single property
+ *   ＠LabelledAs("UserSchema")
+ *   name: string;
+ *
+ *   // For collection
+ *   ＠LabelledAs("UserCollectionSchema", "collection")
+ *   users: User[];
+ * }
+ * ```
+ *
+ * @param label The name of the label
  * @param includeCollection Add the label to the collection. By default, the label is added to the item of the collection it the property is a collection.
+ * @returns PropertyDecorator
+ *
  * @decorator
  */
-export function LabelledAs(name: string, includeCollection: false | "collection" = false): PropertyDecorator {
+export function LabelledAs(label: string, includeCollection: false | "collection" = false): PropertyDecorator {
   return JsonEntityFn((entity) => {
     if (includeCollection) {
-      entity.schema.label(name);
+      entity.schema.label(label);
     } else {
-      entity.itemSchema.label(name);
+      entity.itemSchema.label(label);
     }
   });
 }
