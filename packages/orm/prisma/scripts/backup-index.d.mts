c6f5a8f00afe8f8a0be3033ea5d08170f16df2e2
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-12-09 08:12:44 +0100

    fix(prisma): change usr/bin/env ts-node by node
    Closes: #2927

--- packages/orm/prisma/scripts/backup-index.d.mts
+++ packages/orm/prisma/scripts/backup-index.d.mts
@@ -1,1 +1,1 @@
-export * from "../.schema";
+export * from "../.schema/index.js";
--- packages/orm/prisma/src/generator.ts
+++ packages/orm/prisma/src/generator.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env node
 import helpers from "@prisma/generator-helper";
 import {join} from "path";
 
