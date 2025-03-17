a027c6eceb24533d6ae40b496776e27632aa00d5
Romain Lenzotti <rom.lenzotti@gmail.com>
2025-03-17 11:21:09 +0100

    fix(di): context.logger.level can be changed during request

--- packages/di/src/node/domain/ContextLogger.spec.ts
+++ packages/di/src/node/domain/ContextLogger.spec.ts
@@ -1,5 +1,6 @@
 import {InjectorService} from "../../common/index.js";
 import {ContextLogger} from "./ContextLogger.js";
+import {levels, Logger} from "@tsed/logger";
 
 function getIgnoreLogFixture(ignore: string[], url: string) {
   const ignoreReg = ignore.map((pattern: string | RegExp) => (typeof pattern === "string" ? new RegExp(pattern, "gi") : pattern));
@@ -99,6 +100,129 @@ describe("ContextLogger", () => {
     expect(logger.trace).toBeCalledWith({
       complete: "complete",
       duration: 1,
+      reqId: "id",
+      test: "test",
+      time: expect.any(Date)
+    });
+  });
+  it("should create a new Context with initial log level (error) and change the request log level (debug)", () => {
+    const logger = new Logger();
+    logger.level = "error";
+
+    vi.spyOn(logger, "error").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "debug").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "info").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "warn").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "trace").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "fatal").mockReturnValue(undefined as never);
+
+    const contextLogger = new ContextLogger({
+      event: {
+        request: {},
+        response: {}
+      },
+      logger,
+      id: "id",
+      dateStart: new Date("2019-01-01"),
+      injector: new InjectorService()
+    });
+
+    expect(logger.level).toEqual("ERROR");
+    expect(contextLogger.level).toEqual(levels().ERROR);
+
+    // WHEN
+    contextLogger.level = levels().DEBUG;
+
+    contextLogger.debug({test: "test"});
+    contextLogger.info({test: "test"});
+    contextLogger.warn({test: "test"});
+    contextLogger.error({test: "test"});
+    contextLogger.fatal({test: "test"});
+    contextLogger.trace({test: "test"});
+
+    contextLogger.flush();
+
+    // THEN
+    expect(logger.level).toEqual("ERROR");
+
+    expect(logger.info).toBeCalledWith({
+      duration: expect.any(Number),
+      reqId: "id",
+      test: "test",
+      time: expect.any(Date)
+    });
+
+    expect(logger.debug).toBeCalledWith({
+      duration: expect.any(Number),
+      reqId: "id",
+      test: "test",
+      time: expect.any(Date)
+    });
+    expect(logger.warn).toBeCalledWith({
+      duration: expect.any(Number),
+      reqId: "id",
+      test: "test",
+      time: expect.any(Date)
+    });
+    expect(logger.error).toBeCalledWith({
+      duration: expect.any(Number),
+      reqId: "id",
+      test: "test",
+      time: expect.any(Date)
+    });
+    expect(logger.fatal).toBeCalledWith({
+      duration: expect.any(Number),
+      reqId: "id",
+      test: "test",
+      time: expect.any(Date)
+    });
+  });
+  it("should create a new Context with initial log level (error) and not change the request log level", () => {
+    const logger = new Logger();
+    logger.level = "error";
+
+    vi.spyOn(logger, "error").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "debug").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "info").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "warn").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "trace").mockReturnValue(undefined as never);
+    vi.spyOn(logger, "fatal").mockReturnValue(undefined as never);
+
+    const contextLogger = new ContextLogger({
+      event: {
+        request: {},
+        response: {}
+      },
+      logger,
+      id: "id",
+      dateStart: new Date("2019-01-01"),
+      injector: new InjectorService()
+    });
+
+    expect(contextLogger.level).toEqual(levels().ERROR);
+
+    // WHEN
+    contextLogger.debug({test: "test"});
+    contextLogger.info({test: "test"});
+    contextLogger.warn({test: "test"});
+    contextLogger.error({test: "test"});
+    contextLogger.fatal({test: "test"});
+    contextLogger.trace({test: "test"});
+
+    contextLogger.flush();
+
+    // THEN
+    expect(logger.debug).not.toBeCalled();
+    expect(logger.trace).not.toBeCalled();
+    expect(logger.info).not.toBeCalled();
+    expect(logger.error).toBeCalledWith({
+      duration: expect.any(Number),
+      reqId: "id",
+      test: "test",
+      time: expect.any(Date)
+    });
+    expect(logger.fatal).toBeCalledWith({
+      duration: expect.any(Number),
       reqId: "id",
       test: "test",
       time: expect.any(Date)
